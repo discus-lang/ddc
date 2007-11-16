d4975b7aa911f7970992065e33b5248f5baa622a
Ben.Lippmeier <Ben.Lippmeier@anu.edu.au>
2007-11-16 03:02:57 +0000

    Remove some old junk source files.

--- src/Type/Util.hs
+++ src/Type/Util.hs
@@ -4,11 +4,9 @@ module Type.Util
 	, module Type.Util.Instantiate
 	, module Type.Util.Elaborate
 	, module Type.Util.Kind
-	, module Type.Util.Label
 	, module Type.Util.Normalise
 	, module Type.Util.Pack
 	, module Type.Util.StripFetters
-	, module Type.Util.Erase
 	, module Type.Util.Unify )
 	
 where
@@ -18,9 +16,7 @@ import Type.Util.Generalise
 import Type.Util.Instantiate
 import Type.Util.Elaborate
 import Type.Util.Kind
-import Type.Util.Label
 import Type.Util.Normalise
 import Type.Util.Pack
 import Type.Util.StripFetters
-import Type.Util.Erase		
 import Type.Util.Unify	
