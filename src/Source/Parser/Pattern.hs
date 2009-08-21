3ebf172ee5b5b882534f464ede5abfc395c5e45c
Erik de Castro Lopo <erikd@mega-nerd.com>
2009-08-21 12:51:00 +0000

    Parser : Refactor pattern parser (now Parsec.try free).

--- src/Source/Parser/Pattern.hs
+++ src/Source/Parser/Pattern.hs
@@ -20,26 +20,25 @@ import qualified Text.ParserCombinators.Parsec.Prim		as Parsec
 -- Patterns ----------------------------------------------------------------------------------------
 pPat :: Parser (Pat SP)
 pPat
- = 	(Parsec.try $ do
- 		p1	<- pPat2
-		pTok K.Colon
-		p2	<- pPat
-		return	$ WCons (spW p1) p1 p2)
+ = 	do	p1	<- pPat2
+		(	do	pTok K.Colon
+				p2	<- pPat
+				return	$ WCons (spW p1) p1 p2
 
-  <|>	pPat2
+		  <|>	do	return	p1)
+
   <?>   "pPat"
 
 pPat2 :: Parser (Pat SP)
 pPat2
- =
-   	-- CON { .l1 = PAT1 .. }
-  	-- overlaps with CON pats..
-  	(Parsec.try $ pPat_conLabel)
-
+ =	-- CON { label = PAT .. }
 	-- CON pats..
-  <|>	(do	con		<- pCon
-		pats		<- Parsec.many pPat1
-		return	$ WCon (spV con) (vNameV con) pats)
+	do	con	<- pCon
+		(	do	lb	<- pCParen $ Parsec.sepBy pLabelBind (pTok K.Comma)
+                		return	$ WConLabel (spV con) (vNameV con) lb
+
+		  <|>	do	pats	<- Parsec.many pPat1
+				return	$ WCon (spV con) (vNameV con) pats)
 
   <|>	pPat1
   <?>   "pPat2"
@@ -73,68 +72,49 @@ pPat1
 			then return	$ WCon (spTP tok) Var.primNil []
 			else return	$ WList (spTP tok) ps
 
-  <|>	-- CON { .l1 = PAT1 .. }
-  	-- overlaps with CON
-  	(Parsec.try $ do
-		pPat_conLabel)
+  <|>	-- CON { label = PAT .. }
+  	-- CON
+	do	con	<- pCon
+		(	do	lb	<- pCParen $ Parsec.sepBy pLabelBind (pTok K.Comma)
+                		return	$ WConLabel (spV con) (vNameV con) lb
 
-  <|>	-- CON
-	do	con		<- pCon
-		return	$ WCon (spV con) (vNameV con) []
+		  <|>	do	return	$ WCon (spV con) (vNameV con) [])
 
   <|>  	-- VAR @ PAT
- 	-- overlaps with VAR
-	(Parsec.try $ do
- 		var		<- pVar
-		pTok K.At
-		pat		<- pPat1
-		return	$ WAt (spV var) (vNameV var) pat)
+	-- VAR
+        do	var		<- pVar
+        	(	do	pTok K.At
+				pat	<- pPat1
+				return	$ WAt (spV var) (vNameV var) pat
 
-  <|> 	-- VAR
-  	do	var		<- pVar
-		return	$ WVar (spV var) (vNameV var)
+		  <|>	do	return	$ WVar (spV var) (vNameV var))
 
   <|>  	-- (PAT, PAT .. )
-	-- overlaps with ( PAT )
-	(Parsec.try $ do
-		pTok K.RBra
-		p1		<- pPat
-		pTok K.Comma
-		ps		<- Parsec.sepBy1 pPat (pTok K.Comma)
-		pTok K.RKet
+	-- ( PAT )
+	do	tok	<- pTok K.RBra
+		ps	<- Parsec.sepBy1 pPat (pTok K.Comma)
+                pTok K.RKet
+		if length ps == 1
+		 then return	$ head ps
+		 else return	$ WTuple (spTP tok) ps
 
-		return	$ WTuple (spW p1) (p1 : ps))
-
-  <|>	-- ( PAT )
-  	do	pat		<- pRParen pPat
-		return	pat
   <?> "pPat1"
 
- -- CON { label = PAT ... }
-pPat_conLabel :: Parser (Pat SP)
-pPat_conLabel
- = do	con		<- pCon
-	pTok K.CBra
 
-	let pLabelBind = do
-		label	<- pDotLabel
+pLabelBind :: Parser (Label SP, Pat SP)
+pLabelBind
+ =	do	label	<- pDotLabel
 		pTok K.Equals
 		pat	<- pPat
 		return	(label, pat)
-
-	labelBinds	<- Parsec.sepBy pLabelBind (pTok K.Comma)
-	pTok K.CKet
-
-	return	$ WConLabel (spV con) (vNameV con) labelBinds
-
 
 -- | Parse a label.
 pDotLabel :: Parser (Label SP)
 pDotLabel
  = 	do	var	<- pVar
- 		return	$ LVar (spV var) (vNameF var)
+		return	$ LVar (spV var) (vNameF var)
 
   <|>	do	pTok K.Dot
-  		(int, sp)	<- pIntSP
-  		return	$ LIndex sp int
+		(int, sp)	<- pIntSP
+		return	$ LIndex sp int
   <?>   "pDotLabel"
