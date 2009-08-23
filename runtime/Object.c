0463a8307316981e31f8b05f4e3bb283c65c46b3
Ben.Lippmeier <Ben.Lippmeier@anu.edu.au>
2009-08-23 06:40:44 +0000

    Adjust coding style and add comments

--- runtime/Object.c
+++ runtime/Object.c
@@ -88,111 +88,152 @@ bool	_objIsAnchored (Obj* obj)
 //------------------------------------------------------------------------------
 // Functions for debugging the runtime.
 
+
+// Dump data to this file in hex format.
 static void
-hexdump (FILE *f, unsigned char * ptr, int len)
+hexdump (FILE *f, Word8 *ptr, int len)
 {
-	fprintf (f, "[ ") ;
-	for (int k = 0 ; k < len ;  k++)
-		fprintf (f, "%02x ", ptr [k]) ;
-	fprintf (f, "]\n") ;
+	fprintf (f, "[ ");
+
+	for (int k = 0; k < len;  k++)
+		fprintf (f, "%02x ", ptr [k]);
+
+	fprintf (f, "]\n");
 }
 
+
+// Dump printable characters from buffer into file.
 static void
-strdump (FILE *f, unsigned char * ptr, int len)
+strdump (FILE *f, Word8 *ptr, int len)
 {
-	putchar ('"') ;
-	for (int k = 0 ; k < len ;  k++)
-		putchar (isprint (ptr [k]) ? ptr [k] : '.') ;
-	fprintf (f, "\"\n") ;
+	putchar ('"');
+	for (int k = 0; k < len;  k++)
+		putchar (isprint (ptr [k]) ? ptr [k] : '.');
+
+	fprintf (f, "\"\n");
 }
 
-static void
-innerDumpObj (FILE *f, int level, Obj* obj, bool recurse)
-{
-	int indent = 2 * level + (level == 0 ? 1 : 3) ;
-	int tag, id, size ;
 
-	size = _objSize (obj) ;
-	tag = _getObjTag (obj) ;
-	id = _getObjId (obj) ;
+// Dump out a single object / or all objects reachable from this one.
+static void
+innerDumpObj 
+	( FILE *f		// file to dump o
+	, int level		// indent level
+	, Obj* obj		// root object
+	, bool recurse)		// whether to recurse into other objects.
+{
+	int indent = 2 * level + (level == 0 ? 1 : 3);
+	int tag, id, size;
+
+	size	= _objSize   (obj);
+	tag	= _getObjTag (obj);
+	id	= _getObjId  (obj);
 
 	switch (id & 0xF7) {
 	 case 0x11 :
-		fprintf (f, "%*cThunk @ %p (%d bytes)\n", indent, ' ', obj, size) ;
-		fprintf (f, "%*c  , tag   = 0x%06x\n", indent, ' ', tag) ;
-		fprintf (f, "%*c  , func  = %p\n", indent, ' ', ((Thunk*)obj)->func) ;
-		fprintf (f, "%*c  , arity = %d\n", indent, ' ', ((Thunk*)obj)->arity) ;
-		fprintf (f, "%*c  , args  = %d\n", indent, ' ', ((Thunk*)obj)->args) ;
+		fprintf (f, "%*cThunk @ %p (%d bytes)\n", indent, ' ', obj, size);
+		fprintf (f, "%*c  , tag   = 0x%06x\n",	  indent, ' ', tag);
+		fprintf (f, "%*c  , func  = %p\n",	  indent, ' ', ((Thunk*)obj)->func);
+		fprintf (f, "%*c  , arity = %d\n",	  indent, ' ', ((Thunk*)obj)->arity);
+		fprintf (f, "%*c  , args  = %d\n",	  indent, ' ', ((Thunk*)obj)->args);
+
 		for (int k = 0 ; k < ((Thunk*)obj)->args ;  k++) {
 			if (recurse)
-				innerDumpObj (f, level + 1, ((Thunk*)obj)->a [k], recurse) ;
+				innerDumpObj (f, level + 1, ((Thunk*)obj)->a [k], recurse);
 			else
-				fprintf (f, "%*c  , a [%3d] = %p\n", indent, ' ', k, ((Thunk*)obj)->a [k]) ;
+				fprintf (f, "%*c  , a [%3d] = %p\n", indent, ' ', k, ((Thunk*)obj)->a [k]);
 		}
 		fprintf (f, "%*c  }\n", indent, ' ') ;
-		break ;
+
+		break;
 
 	 case 0x21 :
-		fprintf (f, "%*cData @ %p (%d bytes) {\n", indent, ' ', obj, size) ;
-		fprintf (f, "%*c  , tag     = 0x%06x\n", indent, ' ', tag) ;
-		fprintf (f, "%*c  , arity   = %d\n", indent, ' ', ((Data*)obj)->arity) ;
+		fprintf (f, "%*cData @ %p (%d bytes) {\n", indent, ' ', obj, size);
+		fprintf (f, "%*c  , tag     = 0x%06x\n",   indent, ' ', tag);
+		fprintf (f, "%*c  , arity   = %d\n",       indent, ' ', ((Data*)obj)->arity);
+
 		for (int k = 0 ; k < ((Data*)obj)->arity ;  k++) {
 			if (recurse)
 				innerDumpObj (f, level + 1, ((Data*)obj)->a [k], recurse) ;
 			else
 				fprintf (f, "%*c  , a [%3d] = %p\n", indent, ' ', k, ((Data*)obj)->a [k]) ;
 		}
+
 		fprintf (f, "%*c  }\n", indent, ' ') ;
 		break ;
 
 	 case 0x31 :
-		fprintf (f, "%*cDataR @ %p (%d bytes) {\n", indent, ' ', obj, size) ;
-		fprintf (f, "%*c  , tag     = 0x%06x\n", indent, ' ', tag) ;
-		fprintf (f, "%*c  , size    = %d\n", indent, ' ', ((DataR*)obj)->size) ;
-		fprintf (f, "%*c  , payload = ", indent, ' ') ;
-		hexdump (f, ((DataR*)obj)->payload, ((DataR*)obj)->size - offsetof (DataR, payload)) ;
+		fprintf (f, "%*cDataR @ %p (%d bytes) {\n", indent, ' ', obj, size);
+		fprintf (f, "%*c  , tag     = 0x%06x\n",    indent, ' ', tag);
+		fprintf (f, "%*c  , size    = %d\n",        indent, ' ', ((DataR*)obj)->size);
+		fprintf (f, "%*c  , payload = ",            indent, ' ');
+
+		hexdump (f, 	((DataR*)obj)->payload, 
+				((DataR*)obj)->size - offsetof (DataR, payload));
+
 		fprintf (f, "%*c                  -> ", indent, ' ') ;
-		strdump (f, ((DataR*)obj)->payload, ((DataR*)obj)->size - offsetof (DataR, payload)) ;
+
+		strdump (f, 	((DataR*)obj)->payload, 
+				((DataR*)obj)->size - offsetof (DataR, payload));
+
 		fprintf (f, "%*c  }\n", indent, ' ') ;
 		break ;
 
 	 case 0x41 :
-		fprintf (f, "%*cDataM @ %p (%d bytes) {\n", indent, ' ', obj, size) ;
-		fprintf (f, "%*c  , tag      = 0x%06x\n", indent, ' ', tag) ;
-		fprintf (f, "%*c  , size     = %d\n", indent, ' ', ((DataM*)obj)->size) ;
-		fprintf (f, "%*c  , ptrCount = %d\n", indent, ' ', ((DataM*)obj)->ptrCount) ;
-		fprintf (f, "%*c  , payload  = ", indent, ' ') ;
-		hexdump (f, ((DataM*)obj)->payload, ((DataM*)obj)->size - offsetof (DataM, payload)) ;
-		fprintf (f, "%*c                  -> ", indent, ' ') ;
-		strdump (f, ((DataM*)obj)->payload, ((DataM*)obj)->size - offsetof (DataM, payload)) ;
-		fprintf (f, "%*c  }\n", indent, ' ') ;
-		break ;
+		fprintf (f, "%*cDataM @ %p (%d bytes) {\n", indent, ' ', obj, size);
+		fprintf (f, "%*c  , tag      = 0x%06x\n",   indent, ' ', tag);
+		fprintf (f, "%*c  , size     = %d\n",       indent, ' ', ((DataM*)obj)->size);
+		fprintf (f, "%*c  , ptrCount = %d\n",       indent, ' ', ((DataM*)obj)->ptrCount);
+		fprintf (f, "%*c  , payload  = ",           indent, ' ');
+
+		hexdump (f,	((DataM*)obj)->payload, 
+			    	((DataM*)obj)->size - offsetof (DataM, payload));
+
+		fprintf (f, "%*c                  -> ",     indent, ' ');
+
+		strdump (f, 	((DataM*)obj)->payload,
+				((DataM*)obj)->size - offsetof (DataM, payload));
+
+		fprintf (f, "%*c  }\n", indent, ' ');
+		break;
 
 	 case 0x51 :
-		fprintf (f, "%*cSuspIndir @ %p (%d bytes) {\n", indent, ' ', obj, size) ;
-		fprintf (f, "%*c  , tag   = 0x%06x\n", indent, ' ', tag) ;
-		innerDumpObj (f, level + 1, ((SuspIndir*)obj)->obj, recurse) ;
-		fprintf (f, "%*c    arity = %d\n", indent, ' ', ((SuspIndir*)obj)->arity) ;
-		for (int k = 0 ; k < ((SuspIndir*)obj)->arity ;  k++)
-			innerDumpObj (f, level + 1, ((SuspIndir*)obj)->a [k], recurse) ;
+		fprintf (f, "%*cSuspIndir @ %p (%d bytes) {\n", indent, ' ', obj, size);
+		fprintf (f, "%*c  , tag   = 0x%06x\n",          indent, ' ', tag);
+
+		innerDumpObj (f, level + 1, ((SuspIndir*)obj)->obj, recurse);
+
+		fprintf (f, "%*c    arity = %d\n", indent, ' ', ((SuspIndir*)obj)->arity);
+
+		for (int k = 0; k < ((SuspIndir*)obj)->arity;  k++)
+			innerDumpObj (f, level + 1, ((SuspIndir*)obj)->a [k], recurse);
+
 		fprintf (f, "%*c  }\n", indent, ' ') ;
+
 		break ;
 
 	 default :
-		fprintf (f, "%*cUnknown @ %p (%d bytes) {\n", indent, ' ', obj, size) ;
-		fprintf (f, "%*c  , tag = 0x%06x\n", indent, ' ', tag) ;
-		fprintf (f, "%*c  , id  : 0x%x : ", indent, ' ', id) ;
-		hexdump (f, (unsigned char *) obj, size) ;
-		fprintf (f, "%*c  }\n", indent, ' ') ;
+		fprintf (f, "%*cUnknown @ %p (%d bytes) {\n",   indent, ' ', obj, size);
+		fprintf (f, "%*c  , tag = 0x%06x\n",            indent, ' ', tag);
+		fprintf (f, "%*c  , id  : 0x%x : ",             indent, ' ', id);
+
+		hexdump (f, (unsigned char *) obj, size);
+
+		fprintf (f, "%*c  }\n", indent, ' ');
 	}
 }
 
+
+// Dump a single object to a file.
 void	objDump (FILE *f, Obj* obj)
 {
-	innerDumpObj (f, 0, obj, false) ;
+	innerDumpObj (f, 0, obj, false);
 }
 
+
+// Dump all objects reachable from this one to a file.
 void	objDumpGraph (FILE *f, Obj* obj)
 {
-	innerDumpObj (f, 0, obj, true) ;
+	innerDumpObj (f, 0, obj, true);
 }
+
