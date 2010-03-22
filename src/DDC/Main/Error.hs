bb69bcfa9a8aaa8db4623f2201d1f4616b31e5d0
benl <benl@ouroborus.net>
2010-03-22 10:25:12 +0000

    Fixup docs

--- src/DDC/Main/Error.hs
+++ src/DDC/Main/Error.hs
@@ -1,4 +1,5 @@
 
+-- | Top-level error handing functions.
 module DDC.Main.Error
 	( panic
 	, freakout
@@ -14,27 +15,29 @@ import qualified DDC.Config.Version	as Version
 
 -- | The walls are crashing down.
 --	Print a last, dying message and bail out.
-panic 	:: Pretty msg PMode
-	=> String 		-- ^ Name of module the panic was in.
+panic 	:: forall msg a
+	.  Pretty msg PMode
+	=> String 		-- ^ Name of the DDC module the panic was in.
 	-> msg 			-- ^ Message to display.
-	-> a
-
+	-> a 
+	
 panic  stage msg
-	=  error
-		( pprStrPlain
-		$ "PANIC in " % stage % "\n" 
-		%> (msg  % "\nPlease report this bug to the maintainers at:\n"
-			 % Version.maintainers))
+	= error
+	$ pprStrPlain
+	$ "PANIC in " % stage % "\n" 
+	%> (msg  % "\nPlease report this bug to the maintainers at:\n"
+		 % Version.maintainers)
 
 
 -- | Something bad has happened, and it's likely to be terminal.
 --	Hopefully we can contine on for a bit longer until some other error occurs
 --	that gives information about what caused this problem.
 freakout 
-	:: Pretty msg PMode
-	=> String 		-- ^ Bame of module the freakout was in.
+	:: forall msg a
+	.  Pretty msg PMode
+	=> String 		-- ^ Name of the DDC module the freakout was in.
 	-> msg 			-- ^ Message to display.
-	-> a 			-- ^ Value to return
+	-> a 			-- ^ Value to return.
 	-> a
 
 freakout stage msg a
@@ -46,29 +49,31 @@ freakout stage msg a
 		a
 
 
--- | A compile time error in the user program. 
---	Report the errors and bail out. Using this version doesn't give the
---	compiler the chance catch the error and write it to a file, it just
---	compilains to stderr and dies. Using `exitWithUserError` is preferable
---	for a clean shutdown.
+-- | Compilain about a compile time error in the user program, then bail out.
+--	Using this version doesn't give the compiler the chance to honor
+--	`StopErrors` by catching the error message and writing it to a file.
+--	It just complains to stderr and dies. 
+--	Using `exitWithUserError` is preferable for a clean shutdown.
 dieWithUserError 
-	:: Pretty err PMode
-	=> [err] 		-- ^ errors
+	:: forall err a
+	.  Pretty err PMode
+	=> [err] 		-- ^ Errors to display.
 	-> a
 
 dieWithUserError  errs
 	= error	(pprStrPlain $ "ERROR\n" % (punc "\n" errs))
 
 	
--- | A compile time error in the user program.
+-- | Handle a compile time error in the user program in an orderly way.
 --	If the args have StopErrors set, then write the errors to a file, 
 --	otherwise write them to stderr. A clean exit. Using this is
 --	preferable to using `dieWithUserError`.
 exitWithUserError
-	:: Pretty a PMode
-	=> [Arg] 
-	-> [a] 
-	-> IO b
+	:: forall err a
+	.  Pretty err PMode
+	=> [Arg] 		-- ^ Command line args, possibly including `StopErrors`.
+	-> [err] 		-- ^ Errors to display.
+	-> IO a
 		
 exitWithUserError args errs
  = case [e | e@StopErrors{} <- args] of
