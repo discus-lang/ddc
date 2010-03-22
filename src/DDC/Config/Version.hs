5222c3a41e82fb378245183ca7d38d532c6b90f2
benl <benl@ouroborus.net>
2010-03-22 05:04:09 +0000

    Comments only

--- src/DDC/Config/Version.hs
+++ src/DDC/Config/Version.hs
@@ -1,16 +1,25 @@
 
+-- | DDC id and version strings.
 module DDC.Config.Version
 	( ddcName
 	, version
 	, maintainers )
 where
 	
+
 -- | The long name is added as a tag line to generated C files,
 --	and used in the command line interface.
+ddcName :: String
 ddcName	= "The Disciplined Disciple Compiler, version " ++ version
-version	= "Alpha 1.2"
+
+
+-- | The version string.
+version :: String
+version	= "Alpha 0.1.2"
+
 
 -- | Maintainer contact string to put in panic messages.
+maintainers :: String
 maintainers	
 	=  "    http://trac.haskell.org/ddc\n"
 	++ "    or by emailing disciple-cafe@googlegroups.com\n"
