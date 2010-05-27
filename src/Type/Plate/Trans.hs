55881da60399ab8568ee2adb63d88e7d5999dbb4
benl <benl@ouroborus.net>
2010-05-27 07:48:45 +0000

    Simplify and comment type boilerplate

--- src/Type/Plate/Trans.hs
+++ src/Type/Plate/Trans.hs
@@ -1,9 +1,11 @@
 {-# OPTIONS -fwarn-incomplete-patterns #-}
 
 -- | Generic transformations on type expressions.
+--   This isn't particuarly fast. Depending on what you're doing, it may be better to write
+--   traversal code specific to the transform you're implementing.
 module Type.Plate.Trans
-	( TransM(..)
-	, TransTable (..)
+	( TransM	(..)
+	, TransTable	(..)
 	, transTableId
 	, transZ
 	, transformT
@@ -20,103 +22,117 @@ import qualified Data.Set	as Set
 import qualified Data.Map	as Map
 
 
------
+-- | Monadic transforms.
 class Monad m => TransM m a 
  where	transZM :: TransTable m -> a -> m a
 
------
-transZ 
-	:: TransM (State ()) a
-	=> TransTable (State ())
-	-> a -> a
+
+-- | Apply a transform in the identity monad.
+transZ 	:: TransM (State ()) a
+	=> TransTable (State ()) -> a -> a
 	
 transZ	table x 
 	= evalState (transZM table x) ()
 
 
------
+-- | Table containing fns to apply at each node in the AST.
 data TransTable m
 	= TransTable
-	{ transV	:: Var  	-> m Var
+	{ -- | Transform to apply to variables.
+	  transV	:: Var  	-> m Var
+
+	  -- | Transform to apply to classids.
 	, transCid	:: ClassId	-> m ClassId
 
-	-- type
-	, transT	:: TransTable m -> Type   	-> m Type
-	, transT_follow	:: TransTable m -> Type		-> m Type
-	, transT_enter	:: Type		-> m Type
-	, transT_leave	:: Type		-> m Type
-
+	  -- | Transform to apply to type constructors.
 	, transC	:: TyCon	-> m TyCon
-	, transJ	:: TProj 	-> m TProj
-	, transF	:: Fetter	-> m Fetter
-	, transK	:: Kind		-> m Kind }
 
------
-transTableId 
-	:: Monad m
-	=> TransTable m
-	
+	  -- | Transform to apply to TProj nodes in a type.
+	, transJ	:: TProj 	-> m TProj
+
+	  -- | Transform to apply to fetters in a type.
+	, transF	:: Fetter	-> m Fetter
+
+	  -- | Transform to apply to kinds in a type.
+	, transK	:: Kind		-> m Kind
+
+	  -- | Transform to apply to types.
+	  --   The default one of these is to invoke the enter, follow, and leave fns below.
+	, transT	:: TransTable m -> Type -> m Type
+
+	  -- | Transform to apply to types in a top-down manner.
+	, transT_enter	:: Type		-> m Type
+
+ 	  -- | Transform that applies itself to children of a node in the AST.
+	, transT_follow	:: TransTable m -> Type	-> m Type
+
+	  -- | Transform to apply to types in a bottom-up manner.
+	, transT_leave	:: Type		-> m Type }
+
+
+-- | Identity transform that just returns the original type.
+transTableId :: Monad m => TransTable m
 transTableId
 	= TransTable
 	{ transV	= \x -> return x
 	, transCid	= \x -> return x
-
-	-- type
-	, transT	= transT_default
-	, transT_follow	= followT
-	, transT_enter	= \t -> return t
-	, transT_leave	= \t -> return t
-
 	, transC	= \x -> return x
 	, transJ	= \x -> return x
 	, transF	= \x -> return x
-	, transK	= \x -> return x }
+	, transK	= \x -> return x 
+	, transT	= transT_default
+	, transT_follow	= followT
+	, transT_enter	= \t -> return t
+	, transT_leave	= \t -> return t }
 
------
-transformT f t		= transZ  transTableId { transT_leave = \x -> return $ f x } t
-transformV f t		= transZ  transTableId { transV = \x -> return $ f x } t
-transformCid f t	= transZ  transTableId { transCid = \x -> return $ f x } t
-		
-transformTM f t	= transZM transTableId { transT_leave = f } t
 
------
+-- Common Transforms  ----------------------------------------------------------------------------
+-- | Apply a transform to all type expressions in a type.
+transformT   f t	= transZ  transTableId { transT_leave 	= \x -> return $ f x } t
+
+-- | Apply a monadic transform to all type expressions in a type.
+transformTM  f t	= transZM transTableId { transT_leave	= f } t
+
+-- | Apply a transform to all variables in a type.
+transformV   f t	= transZ  transTableId { transV 	= \x -> return $ f x } t
+
+-- | Apply a transform to all classids in a type.
+transformCid f t	= transZ  transTableId { transCid	= \x -> return $ f x } t
+
+
+-- Basic Types ------------------------------------------------------------------------------------
 instance (Monad m, TransM m a) 
-		=> TransM m [a]
- where	transZM table xx	
- 	 = 	mapM (transZM table) xx
+	=> TransM m [a] where
+ transZM table xx	
+ 	 = mapM (transZM table) xx
  
+
 instance (Monad m, TransM m a)
-		=> TransM m (Bag a) where
+	=> TransM m (Bag a) where
  transZM table xx
  	= liftM Bag.fromList
 	$ (transZM table)
 	$ Bag.toList xx
 
+
 instance (Monad m, TransM m a, Ord a)
-		=> TransM m (Set a) where
-		
+	=> TransM m (Set a) where
  transZM table xx
  	= liftM Set.fromList
 	$ transZM table
 	$ Set.toList xx
 
+
 instance (Monad m, TransM m a, TransM m b) 
-	 	=> TransM m (a, b) where
+ 	=> TransM m (a, b) where
  transZM table (a, b)
-  = do
-	a'	<- transZM table a
-	b'	<- transZM table b
-	return	(a', b')
+  = liftM2 (,) (transZM table a) (transZM table b)
 
 
 instance (Monad m, TransM m a, TransM m b, TransM m c)
-		=> TransM m (a, b, c) where
+	=> TransM m (a, b, c) where
  transZM table (a, b, c)
-  = do	
- 	a' 	<- transZM table a
-	b'	<- transZM table b
-	c'	<- transZM table c
-	return	(a', b', c')
+  = liftM3 (,,) (transZM table a) (transZM table b) (transZM table c)
 
  
 -- Var / ClassId -----------------------------------------------------------------------------------
@@ -127,21 +143,24 @@ instance Monad m => TransM m ClassId
  where	transZM = transCid
 
 
+-- Kind --------------------------------------------------------------------------------------------
+instance Monad m => TransM m Kind where
+  transZM table kk
+   = case kk of
+ 	KFun k1 k2	
+ 	 ->  liftM2 KFun (transZM table k1) (transZM table k2)
+	 >>= transK table 
+
+	_ -> transK table kk
+	
+
 -- Type --------------------------------------------------------------------------------------------
 instance Monad m => TransM m Bind where
  transZM table tt
   = case tt of
-	BNil ->	return BNil
-
-  	BVar v
-	 -> do	v'	<- transZM table v
-	 	return	$ BVar v'
-		
-	BMore v t
-	 -> do	v'	<- transZM table v
-	 	t'	<- transZM table t
-		return	$ BMore v' t'
-
+	BNil 		-> return BNil
+  	BVar v		-> liftM  BVar (transZM table v)
+	BMore v t	-> liftM2 BMore (transZM table v) (transZM table t)
 
 instance Monad m => TransM m Type where
  transZM table tt 
@@ -155,72 +174,29 @@ transT_default table tt
 
 followT table tt
    = case tt of
-	TNil
-	 -> do	return	$ TNil
-
-	TForall b k t
-	 -> do	b'	<- transZM table b
-	 	k'	<- transZM table k
-	 	t'	<- transZM table t
-		return	$ TForall b' k' t'
-
-	TFetters t fs
-	 -> do	t'	<- transZM table t
-	 	fs'	<- transZM table fs
-		return	$ TFetters t' fs'
+	TNil		-> return tt
+	TForall b k t	-> liftM3 TForall (transZM table b) (transZM table k) (transZM table t)
+	TFetters t fs	-> liftM2 TFetters (transZM table t) (transZM table fs)
 
 	TConstrain t cs@Constraints { crsEq, crsMore, crsOther }
 	 -> do	t'		<- transZM table t
 		crsEq'		<- liftM Map.fromList $ transZM table $ Map.toList crsEq
 		crsMore'	<- liftM Map.fromList $ transZM table $ Map.toList crsMore
 		crsOther'	<- transZM table crsOther
-		
 		return	$ TConstrain t' (Constraints crsEq' crsMore' crsOther')
 	
-	TSum k ts
-	 -> do	ts'	<- transZM table ts
-	 	return	$ TSum k ts'
-
- 	TVar k v
-	 -> do	v'	<- transZM table v
-	 	return	$ TVar k v'
-
-	TApp t1 t2
-	 -> do	t1'	<- transZM table t1
-	 	t2'	<- transZM table t2
-		return	$ TApp t1' t2'
-		
-	TCon tycon
-	 -> do	tycon'	<- transZM table tycon
-	 	return	$ TCon tycon'
-
-	-- closure
-	TFree v t
-	 -> do	v'	<- transZM table v
-		t'	<- transZM table t
-	 	return	$ TFree v' t'
-
-	TDanger v t
-	 -> do	v'	<- transZM table v
-	 	t'	<- transZM table t
-		return	$ TDanger v' t'
-
-	-- used in solver
-	TClass k cid
-	 -> do	cid'	<- transZM table cid
-	 	return	$ TClass k cid'
-
-	TError k ts
-	 -> do	return tt
-	
-	TVarMore k v t
-	 -> do	k'	<- transZM table k
-	 	v'	<- transZM table v
-		t'	<- transZM table t
-		return	$ TVarMore k' v' t'
-
+	TSum k ts	-> liftM2 TSum    (return k) (transZM table ts)
+ 	TVar k v	-> liftM2 TVar    (return k) (transZM table v)
+	TApp t1 t2	-> liftM2 TApp    (transZM table t1) (transZM table t2)
+	TCon tycon	-> liftM  TCon    (transZM table tycon)
+	TFree v t	-> liftM2 TFree   (transZM table v) (transZM table t)
+	TDanger v t	-> liftM2 TDanger (transZM table v) (transZM table t)
+	TClass k cid	-> liftM2 TClass  (return k)  (transZM table cid)
+	TError k ts	-> return tt
+	TVarMore k v t	-> liftM3 TVarMore (transZM table k) (transZM table v) (transZM table t)
 	TIndex{}	-> return tt
-				
+
+		
 -- TyCon -------------------------------------------------------------------------------------------
 instance Monad m => TransM m TyCon where
  transZM table tt
@@ -249,56 +225,31 @@ instance Monad m => TransM m TyCon where
 instance Monad m => TransM m TProj where 
   transZM table jj
    = case jj of
- 	TJField v
-	 -> do	v'		<- transZM table v
-	 	transJ table	$ TJField v'
-		
-	TJFieldR v
-	 -> do	v'		<- transZM table v
-	 	transJ table	$ TJFieldR v'
-
-	TJIndex _
-	 ->	transJ table	jj
-	 
-	TJIndexR _
-	 ->	transJ table 	jj		
+ 	TJField v	-> liftM TJField (transZM table v) 	>>= transJ table
+	TJFieldR v	-> liftM TJFieldR (transZM table v)	>>= transJ table
+	TJIndex _	-> transJ table jj
+	TJIndexR _	-> transJ table jj
 
 
 -- Fetter ------------------------------------------------------------------------------------------
 instance Monad m => TransM m Fetter where	
   transZM table ff
    = case ff of
-	FConstraint v ts
-	 -> do	v'		<- transZM table v
-	 	ts'		<- transZM table ts
-		transF table	$ FConstraint v' ts'
-		
-	FWhere t1 t2
-	 -> do	t1'		<- transZM table t1
-	 	t2'		<- transZM table t2
-		transF table	$ FWhere t1' t2'
-
+	FConstraint v ts	
+	 ->  liftM2 FConstraint (transZM table v) (transZM table ts)
+	 >>= transF table
+						
+	FWhere t1 t2		
+	 ->  liftM2 FWhere (transZM table t1) (transZM table t2)
+	 >>= transF table
+	
 	FMore t1 t2
-	 -> do	t1'		<- transZM table t1
-	 	t2'		<- transZM table t2
-		transF table	$ FMore t1' t2'
-	
-	FProj pj v tDict tBind
-	 -> do	v'		<- transZM table v
-	 	tDict'		<- transZM table tDict
-		tBind'		<- transZM table tBind
-		transF table	$ FProj pj v' tDict' tBind'
-	
-	
--- Kind --------------------------------------------------------------------------------------------
-instance Monad m => TransM m Kind where
-  transZM table kk
-   = case kk of
- 	KFun k1 k2
-	 -> do	k1'		<- transZM table k1
-	 	k2'		<- transZM table k2
-		transK table	$ KFun k1' k2'
+	 -> liftM2 FMore (transZM table t1) (transZM table t2)
+	 >>= transF table
 		
-	_	-> transK table kk
+	FProj pj v tDict tBind
+	 -> liftM4 FProj (return pj) (transZM table v) 
+			 (transZM table tDict) (transZM table tBind)
+	 >>= transF table
+				
 	
-
