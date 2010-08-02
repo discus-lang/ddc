d32f9b53b9bb193f87a65a737677377ad259b9b8
benl <benl@ouroborus.net>
2010-08-02 11:29:56 +0000

    Clear out some trash

--- src/DDC/Core/Lint/Base.hs
+++ src/DDC/Core/Lint/Base.hs
@@ -8,20 +8,11 @@ module DDC.Core.Lint.Base
 	, lintList
 	, checkList
 	, subSingleton)
---	, slurpClosureToMap
---	, slurpMapToClosure)
 where 
---import DDC.Main.Error
 import DDC.Main.Pretty
 import DDC.Type
--- import DDC.Var
 import DDC.Core.Lint.Env
 import qualified Debug.Trace
---import qualified Data.Map	as Map
---import Data.Map			(Map)
---import Data.List
-
---stage		= "DDC.Core.Lint.Base"
 
 debugExp	= False
 debugType	= False
@@ -55,37 +46,4 @@ subSingleton v t v'
 	
 	| v == v'	= Just t
 	| otherwise	= Nothing
-
-{-
--- TODO: repacking this again and again costs O(n^2) time in size of closure.
-slurpClosureToMap :: Closure -> Map Var Type
-slurpClosureToMap clo
- 	| isTBot clo	= Map.empty
-	
-	| Just (v, t)	<- takeTFree clo
-	= Map.singleton v t
-	
-	| TSum k ts	<- clo
-	, isClosureKind k
-	= foldl' (\m1 m2 
-		   -> Map.unionWithKey
-			(\v c1 c2 -> packType $ makeTSum k [makeTFree v c1, makeTFree v c2])
-			m1 m2)
-		Map.empty
-		$ map slurpClosureToMap ts
-	
-	| otherwise
-	= panic stage $ "slurpClosureToMap: no match for " % clo
-	
--- TODO: It'd be better to keep a 
---	map of in-scope vars -> closures
---      as well a a set of out of closures for top-level / out of scope things.
---
-slurpMapToClosure :: Map Var Type -> Closure
-slurpMapToClosure mm
-	= packType 
-	$ makeTSum kClosure
-	$ map (uncurry makeTFree)
-	$ Map.toList mm
--}
 	
