aa4cdc523d02f5c376c0359062832c4646475296
benl <benl@ouroborus.net>
2010-08-04 03:05:41 +0000

    Linter: Change order of subs to fix { x1 : %r1 ; x2 : $c1 } <: ($c1 :> (x : %r1))

--- src/DDC/Type/Subsumes.hs
+++ src/DDC/Type/Subsumes.hs
@@ -46,48 +46,13 @@ subsumesTT t1 t2
 	| TConstrain t2' _ <- t2
 	= subsumesTT t1 t2'
 
-	| TVar _ b1	<- t1
-	, TVar _ b2	<- t2
-	, takeVarOfBound b1 == takeVarOfBound b2
-	= Subsumes
-
-	| TVar _ (UMore _ t2')	<- t2
-	= subsumesTT t1 t2'
-	
-	
-	-- TODO: Nasty special case for closures.
-	--       Needed for test/15-Typing/InfiniteTypes/Loop1/Loop1.ds
-	--       We'll have to think about this some more.
-	-- (x : $c1) <: $c1
-	| Just (_, TVar k1 b1)	<- takeTFree t1
-	, TVar k2 b2		<- t2
-	, v1			<- takeVarOfBound b1
-	, v2			<- takeVarOfBound b2
-	, isClosureKind k1
-	, isClosureKind k2
-	, v1 == v2
-	= Subsumes
-	
-	| TApp t11 t12	<- t1
-	, TApp t21 t22	<- t2
-	= joinSubsumes (subsumesTT t11 t21) (subsumesTT t12 t22)
-
-	-- Constructors ---------------
-	-- Ignore tag variables when checking whether closure constructors are equivalent.
-	| TCon (TyConClosure tcc1 _)	<- t1
-	, TCon (TyConClosure tcc2 _)	<- t2
-	= case (tcc1, tcc2) of
-		(TyConClosureFreeType   _, TyConClosureFreeType _)	-> Subsumes
-		(TyConClosureFreeRegion _, TyConClosureFreeRegion _)	-> Subsumes
-		(TyConClosureFree _,	   TyConClosureFree _)		-> Subsumes
-		_							-> NoSubsumes t1 t2
-
-	| TCon tc1	<- t1
-	, TCon tc2	<- t2
-	, tc1 == tc2
-	= Subsumes
-
 	-- Sums -----------------------
+	-- NOTE: These have to come before the cases for Vars.
+	--       When checking: { x1 : %r1 ; x2 : $c1 } <: ($c1 :> (x : %r1))
+	--       On the right, we need both the variable $c1 and its more-than bound
+	--       to subsume the sum. However, the rules for TVar split the variable
+	--       from the bound and try the subsumptions one at a time.
+	--      
 	| TSum k1 []		<- t1
 	, TSum k2 []		<- t2
 	, isRegionKind k1 || isEffectKind k1 || isClosureKind k1
@@ -110,7 +75,54 @@ subsumesTT t1 t2
 	, and $ map (\t1i -> isSubsumes $ subsumesTT t1i t2)  ts1
 	= Subsumes
 
-	-- No dice.
+	-- Vars -----------------------
+	-- NOTE: These have to come after the cases for sums, as described above.
+	| TVar _ b1		<- t1
+	, TVar _ b2		<- t2
+	, takeVarOfBound b1 == takeVarOfBound b2
+	= Subsumes
+
+	| TVar k (UMore v _)	<- t2
+	, isSubsumes $ subsumesTT t1 (TVar k (UVar v))
+	= Subsumes
+	
+	| TVar _ (UMore _ t2')	<- t2
+ 	= subsumesTT t1 t2'
+		
+	-- Applications ---------------
+	-- NOTE: This special case only applies to closures.
+	--       Needed for test/15-Typing/InfiniteTypes/Loop1/Loop1.ds
+	-- 	 (x : $c1) <: $c1
+	-- 
+	| Just (_, TVar k1 b1)	<- takeTFree t1
+	, TVar k2 b2		<- t2
+	, v1			<- takeVarOfBound b1
+	, v2			<- takeVarOfBound b2
+	, isClosureKind k1
+	, isClosureKind k2
+	, v1 == v2
+	= Subsumes
+
+	| TApp t11 t12	<- t1
+	, TApp t21 t22	<- t2
+	= joinSubsumes (subsumesTT t11 t21) (subsumesTT t12 t22)
+
+	-- Constructors ---------------
+	-- Ignore tag variables when checking whether closure constructors are equivalent.
+	| TCon (TyConClosure tcc1 _)	<- t1
+	, TCon (TyConClosure tcc2 _)	<- t2
+	= case (tcc1, tcc2) of
+		(TyConClosureFreeType   _, TyConClosureFreeType _)	-> Subsumes
+		(TyConClosureFreeRegion _, TyConClosureFreeRegion _)	-> Subsumes
+		(TyConClosureFree _,	   TyConClosureFree _)		-> Subsumes
+		_							-> NoSubsumes t1 t2
+
+	| TCon tc1	<- t1
+	, TCon tc2	<- t2
+	, tc1 == tc2
+	= Subsumes
+
+	-- No dice --------------------
 	| otherwise
 	= NoSubsumes t1 t2
 
