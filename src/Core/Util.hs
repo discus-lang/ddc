02951f5be3350dffb03e72479b6e6265c073da17
benl <benl@ouroborus.net>
2010-08-08 11:37:18 +0000

    Delete old stripping module

--- src/Core/Util.hs
+++ src/Core/Util.hs
@@ -2,11 +2,9 @@
 module Core.Util
 	( module Core.Util.Bits
 	, module Core.Util.Slurp
-	, module Core.Util.Strip
 	, module Core.Util.Substitute
 	)
 where
 import Core.Util.Bits
 import Core.Util.Slurp
-import Core.Util.Strip
 import Core.Util.Substitute
