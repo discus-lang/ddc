2588d5be320f51b5259711aac3f00e68fb68faea
benl <benl@ouroborus.net>
2010-08-08 08:58:54 +0000

    Delete old core renamer

--- src/Core/Util.hs
+++ src/Core/Util.hs
@@ -1,13 +1,11 @@
 
 module Core.Util
 	( module Core.Util.Bits
-	, module Core.Util.Rename
 	, module Core.Util.Slurp
 	, module Core.Util.Strip
 	, module Core.Util.Substitute)
 where
 import Core.Util.Bits
-import Core.Util.Rename
 import Core.Util.Slurp
 import Core.Util.Strip
 import Core.Util.Substitute
