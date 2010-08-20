36bddc28ce79496efaf5c3f558024224feeea06e
benl <benl@ouroborus.net>
2010-08-20 10:03:14 +0000

    Enable warnings with all build flavours

--- make/build.mk
+++ make/build.mk
@@ -68,7 +68,7 @@ BuildFlavour    := $(strip $(BUILDFLAVOUR))
 
 # -- Distribution compile (fastest at runtime)
 ifeq "$(BuildFlavour)" "distro"
-GHC_FLAGS	:= $(GHC_LANGUAGE) -tmpdir /tmp -O2
+GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -tmpdir /tmp -O2
 GCC_FLAGS	:= -std=c99 -O3 -Wundef
 
 # -- Development Compile (fastest compile)
@@ -78,17 +78,17 @@ GCC_FLAGS	:= -std=c99 -O3 -Wall -Wextra
 
 # -- Debug compile
 else ifeq "$(BuildFlavour)" "devel_debug"
-GHC_FLAGS	:= $(GHC_LANGUAGE) -tmpdir /tmp -O0
+GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -tmpdir /tmp -O0
 GCC_FLAGS	:= -std=c99 -Wundef -g
 
 # -- Profiling compile
 else ifeq "$(BuildFlavour)" "devel_prof"
-GHC_FLAGS	:= $(GHC_LANGUAGE) -tmpdir /tmp -O2 -prof -auto-all
+GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -tmpdir /tmp -O2 -prof -auto-all
 GCC_FLAGS	:= -std=c99 -Wundef -g -pg
 
 # -- For Haskell Program Coverage
 else ifeq "$(BuildFlavour)" "devel_hpc"
-GHC_FLAGS	:= $(GHC_LANGUAGE) -fhpc -tmpdir /tmp 
+GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -fhpc -tmpdir /tmp 
 GCC_FLAGS	:= -std=c99 -O3 -Wundef
 
 else 
