fab1cd24113da05a48f44788aec6c852c5b36542
Erik de Castro Lopo <erikd@mega-nerd.com>
2010-08-29 12:27:10 +0000

    LLVM : Add code to box Bool# variables.

--- src/Llvm/Runtime/Boxing.hs
+++ src/Llvm/Runtime/Boxing.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS -fno-warn-type-defaults #-}
 module Llvm.Runtime.Boxing
 	( boxAny	, unboxAny
+
+	, boxEnum
 	, boxInt32	, unboxInt32
 	, boxInt64
 	, boxFloat32
@@ -24,6 +26,7 @@ stage = "Llvm.Runtime.Boxing"
 boxAny :: LlvmVar -> LlvmM LlvmVar
 boxAny any
  = case getVarType any of
+	LMInt 1		-> boxEnum any
 	LMInt 32	-> boxInt32 any
 	_		-> panic stage $ "boxAny " ++ show (getVarType any)
 
@@ -34,6 +37,7 @@ unboxAny anyType any
 	LMInt 32	-> unboxInt32 any
 	_		-> panic stage $ "unboxAny " ++ show anyType
 
+--------------------------------------------------------------------------------
 
 boxInt32 :: LlvmVar -> LlvmM LlvmVar
 boxInt32 int32
@@ -78,16 +82,42 @@ unboxInt32 objptr
  | otherwise
  =	panic stage $ "unboxInt32 (" ++ show objptr ++ ")"
 
+--------------------------------------------------------------------------------
 
-boxInt64 :: LlvmVar -> LlvmM ()
+boxEnum :: LlvmVar -> LlvmM LlvmVar
+boxEnum v@(LMLocalVar u (LMInt 1))
+ = do	int32	<- lift $ newUniqueNamedReg "int32" i32
+	shifted	<- lift $ newUniqueNamedReg "shifted" i32
+	tag	<- lift $ newUniqueNamedReg "tag" i32
+	iptr0	<- lift $ newUniqueNamedReg "iptr0" (pLift i32)
+	iptr1	<- lift $ newUniqueNamedReg "iptr1" (pLift i32)
+	objptr	<- allocate 8 "boxed"
+	addBlock
+		[ Comment [ "boxEnum (" ++ show v ++ ")" ]
+		, Assignment int32 (Cast LM_Zext v i32)
+		, Assignment shifted (LlvmOp LM_MO_Shl int32 (i32LitVar 8))
+		, Assignment tag (LlvmOp LM_MO_Or shifted tagData)
+		, Assignment iptr0 (Cast LM_Bitcast objptr (pLift i32))
+		, Store tag iptr0
+		, Assignment iptr1 (GetElemPtr True iptr0 [llvmWordLitVar 1])
+		, Store (i32LitVar 0) iptr1
+		]
+	return	objptr
+
+boxEnum any
+ = panic stage $ "boxEnumX " ++ show any
+
+--------------------------------------------------------------------------------
+
+boxInt64 :: LlvmVar -> LlvmM LlvmVar
 boxInt64 i
  = panic stage "unimplemented"
 
-boxFloat32 :: LlvmVar -> LlvmM ()
+boxFloat32 :: LlvmVar -> LlvmM LlvmVar
 boxFloat32 f
  = panic stage "unimplemented"
 
-boxFloat64 :: LlvmVar -> LlvmM ()
+boxFloat64 :: LlvmVar -> LlvmM LlvmVar
 boxFloat64 f
  = panic stage "unimplemented"
 
