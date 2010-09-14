a6e8e71ba778ba584bf4ab0d89604a82af059d09
benl <benl@ouroborus.net>
2010-09-14 07:16:44 +0000

    Don't call back on Data.String when constructing exceptions in the RTS

--- runtime/Error.c
+++ runtime/Error.c
@@ -2,6 +2,7 @@
 
 #include <stddef.h>
 #include <inttypes.h>
+#include <string.h>
 
 #include "Error.h"
 #include "Prim.h"
@@ -15,16 +16,30 @@
 
 // This needs to be defined Control.Exception
 Obj*	Base_ExceptionCaseNoMatch (Obj* moduleName, Obj* line, Obj* column);
-Obj*	Data_String_boxString	  (String str);
 
 // Die due to non-matching pattern
 void	_deathCase (const char* moduleName, Int32 line, Int32 column)
 {
+	_ENTER(1);
+
+	// TODO: This is a hacky construction of a FlatString to hold the module name.
+	//       When we can put unboxed values in algebraic data we'll want to pass
+	//       The string pointer directly.
+	_S(0)		= _allocDataR (0, 8 + 256);
+	DataR* strObj	= (DataR*)_S(0);
+ 	uint32_t* buf32	= (uint32_t*) ( &(strObj ->payload) );
+	buf32[0]	= 256;
+	buf32[1]	= strlen(moduleName);
+	char* str	= (char*)(buf32 + 2);
+	strncpy (str, moduleName, 256);
+
 	primException_throw
 		(Base_ExceptionCaseNoMatch
-			(  Data_String_boxString ((Char8*)moduleName)
+			( _S(0)
 			, _boxInt32	(line)
 			, _boxInt32	(column)));
+			
+	_LEAVE(1);
 	abort();
 }
 
