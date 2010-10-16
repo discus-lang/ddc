5167a5a9f7154ff7052b0f39a5e87393a53eb285
benl <benl@ouroborus.net>
2010-10-16 07:02:23 +0000

    Beautify the rest of the inferencer errors

--- src/DDC/Solve/Error/Transform.hs
+++ src/DDC/Solve/Error/Transform.hs
@@ -43,6 +43,9 @@ instance Monad m => TransM m Type where
 instance Monad m => TransM m Kind where
  transZM table kk	= transK table kk
 
+instance Monad m => TransM m Fetter where
+ transZM table kk	= transF table kk
+
 
 -- Structure
 instance (Monad m, TransM m a, TransM m b) => TransM m (a, b) where
@@ -68,6 +71,9 @@ instance Monad m => TransM m SourcePos where
 instance Monad m => TransM m TypeSource where
  transZM _ xx	= return xx
 
+instance Monad m => TransM m TProj where
+ transZM _ xx	= return xx
+
 instance Monad m => TransM m SigMode where
  transZM _ xx	= return xx
 
@@ -78,14 +84,55 @@ instance Monad m => TransM m Error where
   = case rr of
 	ErrorUnifyCtorMismatch x1 x2 x3 x4
 	 -> liftM4 ErrorUnifyCtorMismatch
-			(transZM table x1) (transZM table x2) (transZM table x3) 
-			(transZM table x4)
+			(transZM table x1) (transZM table x2) (transZM table x3) (transZM table x4)
+
+	ErrorUnifyKindMismatch x1 x2 x3 x4
+	 -> liftM4 ErrorUnifyKindMismatch
+			(transZM table x1) (transZM table x2) (transZM table x3) (transZM table x4)
+
+	ErrorInfiniteType x1 x2
+	 -> liftM2 ErrorInfiniteType
+			(transZM table x1) (transZM table x2)
+
+	ErrorNoInstance x1 x2 x3
+	 -> liftM3 ErrorNoInstance
+			(transZM table x1) (transZM table x2) (transZM table x3)
+
+	ErrorNoProjections x1 x2
+	 -> liftM2 ErrorNoProjections
+			(transZM table x1) (transZM table x2)
+
+	ErrorFieldNotPresent x1 x2 x3
+	 -> liftM3 ErrorFieldNotPresent
+			(transZM table x1) (transZM table x2) (transZM table x3)
+
+	ErrorCannotPurify x1 x2 x3 x4
+	 -> liftM4 ErrorCannotPurify
+			(transZM table x1) (transZM table x2) (transZM table x3) (transZM table x4)
+
+	ErrorRegionConstraint x1 x2 x3 x4
+	 -> liftM4 ErrorRegionConstraint
+			(transZM table x1) (transZM table x2) (transZM table x3) (transZM table x4)
+
+	ErrorLateConstraint x1 x2 x3 x4 x5
+	 -> liftM5 ErrorLateConstraint
+			(transZM table x1) (transZM table x2) (transZM table x3) (transZM table x4)
+			(transZM table x5)
+
+	ErrorWrongMainType x1
+	 -> liftM  ErrorWrongMainType
+			(transZM table x1)
+
+	ErrorEffectfulCAF x1 x2
+	 -> liftM2 ErrorEffectfulCAF
+			(transZM table x1) (transZM table x2)
 		
 	ErrorSigMismatch x1 x2 x3 x4 x5 x6
 	 -> liftM6 ErrorSigMismatch 
 			(transZM table x1) (transZM table x2) (transZM table x3)
 			(transZM table x4) (transZM table x5) (transZM table x6)
 	
+	-- The other errors don't have types, kinds or fetters in them.
 	_	-> return rr
 	
 	
