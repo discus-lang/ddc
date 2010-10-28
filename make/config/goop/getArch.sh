0008f4ee8534f0bbe59a37ad4fdae01074b20e31
benl <benl@ouroborus.net>
2010-10-28 02:30:30 +0000

    Teach getArch.sh about cygwin

--- make/config/goop/getArch.sh
+++ make/config/goop/getArch.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Get the architecture of the machine.
-# Must be run from the root of the ddc build tree.
-
+# ZOMG, pain. We have to trim new lines on Cygwin for this to work. 
 ghc --make make/config/goop/getBits.hs -o make/config/goop/getBits > /dev/null 2>&1
-bits=`make/config/goop/getBits`
+bits=`make/config/goop/getBits | sed -e 's/ *$//'`
+
+# Get the name of the cpu.
 cpu=`uname -m`
 
+# Normalise the name of the architecture we're on.
 case "$cpu-$bits" in
 	i?86-32)
 		echo "x86"
