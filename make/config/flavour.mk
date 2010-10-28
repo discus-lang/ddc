d936486883ec266f6952297dbe49acf4f3fcca40
benl <benl@ouroborus.net>
2010-10-28 02:29:28 +0000

    Don't use tmpdir, cygwin doesn't like it

--- make/config/flavour.mk
+++ make/config/flavour.mk
@@ -6,27 +6,27 @@ BuildFlavour    := $(strip $(BUILDFLAVOUR))
 
 # -- Distribution compile (fastest at runtime)
 ifeq "$(BuildFlavour)" "distro"
-GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -tmpdir /tmp -O2
+GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -O2
 GCC_FLAGS	:= -std=c99 -O3 -Wundef
 
 # -- Development Compile (fastest compile)
 else ifeq "$(BuildFlavour)" "devel"
-GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -tmpdir /tmp -O0
+GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -O0
 GCC_FLAGS	:= -std=c99 -O3 -Wall -Wextra
 
 # -- Debug compile
 else ifeq "$(BuildFlavour)" "devel_debug"
-GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -tmpdir /tmp -O0
+GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -O0
 GCC_FLAGS	:= -std=c99 -Wundef -g
 
 # -- Profiling compile
 else ifeq "$(BuildFlavour)" "devel_prof"
-GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -tmpdir /tmp -O2 -prof -auto-all
+GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -O2 -prof -auto-all
 GCC_FLAGS	:= -std=c99 -Wundef -g -pg
 
 # -- For Haskell Program Coverage
 else ifeq "$(BuildFlavour)" "devel_hpc"
-GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -fhpc -tmpdir /tmp 
+GHC_FLAGS	:= $(GHC_WARNINGS) $(GHC_LANGUAGE) -fhpc
 GCC_FLAGS	:= -std=c99 -O3 -Wundef
 
 else 
