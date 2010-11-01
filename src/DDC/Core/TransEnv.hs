934ec43ae872bac5d53cf7328efe77f2f711159a
benl <benl@ouroborus.net>
2010-11-01 07:04:55 +0000

    Finish TransEnv for core expressions

--- src/DDC/Core/TransEnv.hs
+++ src/DDC/Core/TransEnv.hs
@@ -14,50 +14,51 @@ import Data.Maybe
 -- | Table containing functions we can apply to the various nodes of the tree.
 data TransEnvCore m env
 	= TransEnvCore
-	{ transEnvT	:: Maybe (TransEnvType m env)	
+	{ transEnvCoreT		:: Maybe (TransEnvType m env)	
 		
-	, transEnvP	:: TransEnvUp   m env Top
-	, transEnvA	:: TransEnvUp   m env Alt
-	, transEnvG	:: TransEnvUp   m env Guard
-	, transEnvW	:: TransEnvUp   m env Pat 
-	, transEnvS	:: TransEnvUp   m env Stmt
+	, transEnvCoreP		:: TransEnvUp   m env Top
+	, transEnvCoreA		:: TransEnvUp   m env Alt
+	, transEnvCoreG		:: TransEnvUp   m env Guard
+	, transEnvCoreW		:: TransEnvUp   m env Pat 
+	, transEnvCoreS		:: TransEnvUp   m env Stmt
 
-	, transEnvX_down	:: TransEnvDown m env Exp
-	, transEnvX_up	:: TransEnvUp   m env Exp
+	, transEnvCoreX_down	:: TransEnvDown m env Exp
+	, transEnvCoreX_up	:: TransEnvUp   m env Exp
 	
-	, transEnvV	:: TransEnvUp   m env Var }
-
-
+	, transEnvCoreV		:: TransEnvUp   m env Var }
 
 
 -- Instances --------------------------------------------------------------------------------------
+
+-- Pass of type stuff to the transEnvT table.
 instance Monad m => TransEnv TransEnvCore m env Type where
- transEnv table env tt	= transEnv (fromMaybe transEnvTypeId (transEnvT table)) env tt
+ transEnv table env tt	= transEnv (fromMaybe transEnvTypeId (transEnvCoreT table)) env tt
 
 instance Monad m => TransEnv TransEnvCore m env Kind where
- transEnv table env tt	= transEnv (fromMaybe transEnvTypeId (transEnvT table)) env tt
+ transEnv table env tt	= transEnv (fromMaybe transEnvTypeId (transEnvCoreT table)) env tt
 
 instance Monad m => TransEnv TransEnvCore m env Bind where
- transEnv table env tt	= transEnv (fromMaybe transEnvTypeId (transEnvT table)) env tt
+ transEnv table env tt	= transEnv (fromMaybe transEnvTypeId (transEnvCoreT table)) env tt
 
 instance Monad m => TransEnv TransEnvCore m env Bound where
- transEnv table env tt	= transEnv (fromMaybe transEnvTypeId (transEnvT table)) env tt
+ transEnv table env tt	= transEnv (fromMaybe transEnvTypeId (transEnvCoreT table)) env tt
 
 
+-- Exp
 instance Monad m => TransEnv TransEnvCore m env Exp where
  transEnv table env xx
-  = do	(xx', env')	<- transEnvX_down table env xx
+  = do	(xx', env')	<- transEnvCoreX_down table env xx
 	let down :: TransEnv TransEnvCore m env a => a -> m a
 	    down =  transEnv table env' 
 
-	transEnvX_up table env' 
+	transEnvCoreX_up table env' 
 	 =<< case xx' of
 		XNil			-> return xx'
-		XVar   v  t		-> liftM2 XVar      (return v) (down t)
+		XVar   v  t		-> liftM2 XVar      (down v)   (down t)
 		XLit{}			-> return xx'
 		XLAM   b k x		-> liftM3 XLAM      (down b)   (down k)  (down x)
 		XAPP   x t		-> liftM2 XAPP      (down x)   (down t)
-		XLam   v t x e c	-> liftM5 XLam      (return v) (down t)  (down x) (down e) (down c)
+		XLam   v t x e c	-> liftM5 XLam      (down v)   (down t)  (down x) (down e) (down c)
 		XApp   x1 x2		-> liftM2 XApp      (down x1)  (down x2)
 		XDo    ss		-> liftM  XDo       (down ss)
 		XMatch alts		-> liftM  XMatch    (down alts)
@@ -66,20 +67,56 @@ instance Monad m => TransEnv TransEnvCore m env Exp where
 		XPrimType t      	-> liftM  XPrimType (down t)
 		XTau   t  x		-> liftM2 XTau	    (down t)   (down x)
 
-
-instance Monad m => TransEnv TransEnvCore m env Alt where
- transEnv = undefined
-
-instance Monad m => TransEnv TransEnvCore m env Guard where
- transEnv = undefined
-
-instance Monad m => TransEnv TransEnvCore m env Pat where
- transEnv = undefined
-
+-- Stmt
 instance Monad m => TransEnv TransEnvCore m env Stmt where
- transEnv = undefined
+ transEnv table env ss
+  = do 	let down :: TransEnv TransEnvCore m env a => a -> m a
+	    down =  transEnv table env
 
+	case ss of
+		SBind mv x	-> liftM2 SBind (down mv) (down x)
+
+
+-- Alt
+instance Monad m => TransEnv TransEnvCore m env Alt where
+ transEnv table env aa
+  = do	let down :: TransEnv TransEnvCore m env a => a -> m a
+	    down =  transEnv table env
+
+	case aa of
+		AAlt gs x	-> liftM2 AAlt (down gs) (down x)
+		
+-- Guard
+instance Monad m => TransEnv TransEnvCore m env Guard where
+ transEnv table env gg
+  = do	let down :: TransEnv TransEnvCore m env a => a -> m a
+	    down =  transEnv table env
+
+	case gg of
+		GExp ws x	-> liftM2 GExp (down ws) (down x)
+
+-- Pat
+instance Monad m => TransEnv TransEnvCore m env Pat where
+ transEnv table env ww
+  = do	let down :: TransEnv TransEnvCore m env a => a -> m a
+	    down =  transEnv table env
+
+	case ww of
+		WVar v		-> liftM WVar (down v)
+		WLit{}		-> return ww
+		WCon sp v lvts	-> liftM3 WCon (return sp) (down v) (down lvts)
+
+-- Label
+instance Monad m => TransEnv TransEnvCore m env Label where
+ transEnv table env ll
+  = do	let down :: TransEnv TransEnvCore m env a => a -> m a
+	    down =  transEnv table env
+
+	case ll of
+		LIndex{}	-> return ll
+		LVar v		-> liftM LVar (down v)
+		
+-- Var
 instance Monad m => TransEnv TransEnvCore m env Var where
- transEnv = undefined
-
+ transEnv table env vv	= transEnvCoreV table env vv
 
--- src/DDC/Util/Plate/TransEnv.hs
+++ src/DDC/Util/Plate/TransEnv.hs
@@ -4,22 +4,27 @@ module DDC.Util.Plate.TransEnv
 	, TransEnvDown
 	, TransEnvUp)
 where
+import Control.Monad
+
 
 -- | Class of things that can be transformed while keeping track of an environment
 --   which grows in a top-down manner.
 class Monad m => TransEnv t m env a where
 	transEnv :: t m env -> env -> a -> m a
 	
--- | A transformation we can apply to a particular node of the tree.
+-- | Transform that is applied to a node of the tree on the way down.
 --   It is given an environment that can be extended in a top-down manner.
 type TransEnvDown m env a	
 	= env -> a -> m (a, env)
 
+-- | Tranform that is applied to a node of the tree on the way up.
 type TransEnvUp   m env a
 	= env -> a -> m a 
 	
 
 -- Instances ------------------------------------------------------------------
+
+-- (,)
 instance (  Monad m
 	 ,  TransEnv t m env a
 	 ,  TransEnv t m env b) 
@@ -30,10 +35,36 @@ instance (  Monad m
 	y'	<- transEnv table env y
 	return	(x', y')
 
-	
+-- (,,)
+instance (  Monad m
+	 ,  TransEnv t m env a
+	 ,  TransEnv t m env b
+	 ,  TransEnv t m env c)
+ 	 => TransEnv t m env (a, b, c) 
+ where
+ transEnv table env (x, y, z)
+  = do	x'	<- transEnv table env x
+	y'	<- transEnv table env y
+	z'	<- transEnv table env z
+	return	(x', y', z')
+
+
+-- []	
 instance (  Monad m
 	 ,  TransEnv t m env a) 
 	 => TransEnv t m env [a] 
  where
  transEnv table env xx	= mapM (transEnv table env) xx
+
+
+-- Maybe
+instance (  Monad m
+	 ,  TransEnv t m env a) 
+	 => TransEnv t m env (Maybe a)
+ where
+ transEnv table env xx
+  = case xx of
+	Nothing	-> return Nothing
+	Just x	-> liftM Just (transEnv table env x)
+	
 
