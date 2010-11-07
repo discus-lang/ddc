c0ba3dd129320bce221db8f1a2268b135b21b076
benl <benl@ouroborus.net>
2010-11-07 05:16:41 +0000

    Prettier pretty printing for constraints

--- src/DDC/Constraint/Pretty.hs
+++ src/DDC/Constraint/Pretty.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS -fwarn-incomplete-patterns -fwarn-unused-matches -fwarn-name-shadowing #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 -- | Pretty printing of type constraints.
 module DDC.Constraint.Pretty
@@ -9,65 +10,73 @@ import DDC.Type
 import DDC.Main.Pretty
 import qualified Data.Map	as Map
 
+padVar = padL 20
 
 -- CTree ------------------------------------------------------------------------------------------
 instance Pretty CTree PMode where
  ppr c  
   = case c of
  	CTreeNil
-	 -> ppr "@CTreeNil"
+	 -> ppr "NIL"
 
-	CBranch{}
-	 -> "@CBranch ("  % branchBind c % ")\n"
+	CBranch BNothing subs
+	 -> "BRANCH\n"
 	  % "{\n"
-		%> ("\n" %!% branchSub c % "\n")
-	  % "}\n"
+		%> (vcat (map ppr subs))
+	  % "}"
+
+	CBranch binds subs
+	 -> "BRANCH ("  % binds % ")\n"
+	  % "{\n"
+		%> (vcat (map ppr subs))
+	  % "}"
+
 
 	CSig	_ v t
-	 -> "@CSig  " % v % "\n" %> prettyTypeSplit t % ";\n"
+	 -> "SIG  " % v % "\n" %> prettyTypeSplit t
 
 	CEq 	_ v t	
-	 -> "@CEq   " % padL 15 v % " " % prettyTypeSplit t % ";"
+	 -> padVar v 	%% "= " %% prettyTypeSplit t
 
 	CEqs 	_ ts
-	 -> "@CEqs  " % ts
-
+	 -> punc " =  " $ map padVar ts
+	
 	CClass	_ v ts
-	 -> "@CClass " % v % " " % ts
+	 -> "CLASS " % v %% ts
 
 	CProject _ tp vInst tDict tBind
-	 -> "@CProject " % tp % " " % vInst % " " % tDict % " " % tBind % ";\n"
+	 -> "PROJECT " % tp % " " % vInst % " " % tDict %% tBind
 
 	CDictProject _ t vs
-	 -> "@CDictProject    " % t % "\n"
+	 -> "DICTPROJECT    " % t % "\n"
 	 	% "{\n"
 		%> ("\n" %!% map (\(v1, v2) -> v1 %>> " = " % v2 % ";") (Map.toList vs) % "\n")
-		% "};\n\n"
+		% "}"
 	
 	CClassInst _ v ts
-	 -> "@ClassInst " % v % " " % ts % ";"
+	 -> "CLASSINST " % v % " " % ts
 
 	CInst _ v1 v2
-	 -> "@CInst " % padL 15 v1 % " " % v2 % ";"
+	 -> padVar v1	%% "<- INST" %% v2
 	 
 	CGen _ v1
-	 -> "@CGen  " % v1 % ";"
+	 -> "GEN  " % v1
 	 
 	-- internal
 	CLeave v
-	 -> "@CLeave " % v % ";"
+	 -> "LEAVE " % v
  
  	CGrind
-	 -> ppr "@CGrind"
+	 -> ppr "GRIND"
 
 	CInstLambda _ v1 v2
-	 -> "@InstLambda " % v1 % " " % v2
+	 -> "INSTLAMBDA " % v1 %% v2
 
 	CInstLet _ v1 v2
-	 -> "@InstLet " % v1 % " " % v2
+	 -> "INSTLET " 	  % v1 %% v2
 
 	CInstLetRec _ v1 v2
-	 -> "@InstLetRec " % v1 % " " % v2
+	 -> "INSTLETREC " % v1 %% v2
 	 
 	 
 
@@ -76,10 +85,10 @@ instance Pretty CTree PMode where
 instance Pretty CBind PMode where
  ppr bb	
   = case bb of
-	BNothing	-> ppr "@BNothing"
-  	BLet 	v	-> "@BLet "		% v
-	BLetGroup vs	-> "@BLetGroup "	% vs
-	BLambda	v 	-> "@BLambda "		% v
-  	BDecon	v  	-> "@BDecon "		% v
+	BNothing	-> ppr "NOTHING"
+  	BLet  	  vs	-> "LET"	%% punc " " vs
+	BLetGroup vs	-> "LETGROUP"	%% punc " " vs
+	BLambda	  vs 	-> "LAMBDA"	%% punc " " vs
+  	BDecon 	  vs  	-> "DECON"	%% punc " " vs
 
 
