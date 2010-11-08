3971f963e8b330339570a1a13418a61b01c236a9
Erik de Castro Lopo <erikd@mega-nerd.com>
2010-11-08 09:59:21 +0000

    LLVM : Remove redundant readSlotVar function.

--- src/Llvm/Runtime/Slot.hs
+++ src/Llvm/Runtime/Slot.hs
@@ -1,7 +1,6 @@
 {-# OPTIONS -Wall -Werror -fwarn-unused-imports #-}
 module Llvm.Runtime.Slot
 	( readSlot
-	, readSlotVar
 	, writeSlot )
 where
 
@@ -19,16 +18,13 @@ stage = "LLvm.Runtime.Slot"
 -- | Store the given variable on the specified GC slot number.
 writeSlot :: LlvmVar -> Int -> LlvmM ()
 writeSlot var 0
- = addBlock	[ Comment [ "slot [0] = " ++ show var ]
-		, Store var localSlotBase ]
+ = addBlock	[ Store var localSlotBase ]
 
 writeSlot var n
  | n > 0
- = do	dst	<- newUniqueNamedReg ("slot" ++ show n) pObj
-	addBlock
-		[ Comment [ "slot [" ++ show n ++ "] = " ++ show var ]
-		, Assignment dst (GetElemPtr True localSlotBase [llvmWordLitVar n])
-		, Store var (pVarLift dst) ]
+ = do	dst		<- newUniqueNamedReg ("slot" ++ show n) pObj
+	addBlock	[ Assignment dst (GetElemPtr True localSlotBase [llvmWordLitVar n])
+			, Store var (pVarLift dst) ]
 
 writeSlot _ n = panic stage $ "writeSlot with slot == " ++ show n
 
@@ -39,35 +35,16 @@ writeSlot _ n = panic stage $ "writeSlot with slot == " ++ show n
 readSlot :: Int -> LlvmM LlvmVar
 readSlot 0
  = do	dstreg		<- newUniqueNamedReg "slot.0" pObj
-	addBlock	[ Comment [ show dstreg ++ " = readSlot 0" ]
-			, Assignment dstreg (Load localSlotBase) ]
+	addBlock	[ Assignment dstreg (Load localSlotBase) ]
 	return		dstreg
 
 readSlot n
  | n > 0
  = do	dstreg		<- newUniqueNamedReg ("slot." ++ show n) pObj
 	r0		<- newUniqueReg pObj
-	addBlock	[ Comment [ show dstreg ++ " = readSlot " ++ show n ]
-			, Assignment r0 (GetElemPtr True localSlotBase [llvmWordLitVar n])
+	addBlock	[ Assignment r0 (GetElemPtr True localSlotBase [llvmWordLitVar n])
 			, Assignment dstreg (loadAddress r0) ]
 	return		dstreg
 
 readSlot n = panic stage $ "readSlot with slot == " ++ show n
-
-
-readSlotVar :: Int -> LlvmVar -> LlvmM ()
-readSlotVar 0 dstreg
- = 	addBlockResult	dstreg
-		[ Comment [ show dstreg ++ " = readSlotVar 0" ]
-		, Assignment dstreg (Load localSlotBase) ]
-
-readSlotVar n dstreg
- | n > 0
- = do	r0		<- newUniqueReg pObj
-	addBlockResult	dstreg
-		[ Comment [ show dstreg ++ " = readSlotVar " ++ show n ]
-		, Assignment r0 (GetElemPtr True localSlotBase [llvmWordLitVar n])
-		, Assignment dstreg (loadAddress r0) ]
-
-readSlotVar n _ = panic stage $ "readSlotVar with slot == " ++ show n
 
