062d8d2f54ffc88878100f383cc9fa07231a9a52
Ben Lippmeier <benl@ouroborus.net>
2010-11-24 11:19:40 +0000

    Wibble on warnings

--- make/config/options.mk
+++ make/config/options.mk
@@ -1,8 +1,6 @@
 # Language features, warning options and packages used by DDC.
-#
 #   We use the same options and package settings for DDC proper as well as its tools
 #   This is so we can keep an eye on what is being used by the whole project.
-# 
 
 # -- Language features --------------------------------------------------------
 GHC_LANGUAGE	:= \
@@ -35,28 +33,41 @@ GHC_WARNINGS	:= \
 	-fwarn-overlapping-patterns \
 	-fwarn-type-defaults \
 	-fwarn-unused-binds \
-	-fno-warn-missing-methods \
-	-fwarn-unused-imports
+	-fwarn-unused-imports \
+	-fno-warn-missing-methods
 
+
+# -- Warnings for GHC 7.0.1
+#    -fspec-constr-count is to try and shut up internal compiler warnings in 7.0.1,
+#       which doesn't seem to work. This should be fixed in 7.0.2, then we can disable it.
+#    -fno-warn-deprecations is because the alex generated files includes -fglagsow-exts
+#
 ifeq "$(GHC_VERSION)" "7"
 GHC_WARNINGS	:= \
 	$(GHC_WARNINGS) \
-	-fwarn-monomorphism-restriction
+	-fwarn-monomorphism-restriction \
+	-fwarn-unrecognised-pragmas \
+	-fspec-constr-count=10
 endif
 
-# Warnings we should probably add.
-#	-fwarn-unused-matches			# we should probably enable this one0
+
+# Warnings that are enabled manually in the DDC tree
+#	-fwarn-unused-matches
+#	-fwarn-incomplete-patterns
+#	-fwarn-name-shadowing 
+
+# Warnings we should probably enable
+#	-fwarn-unused-do-bind
+#	-fwarn-wrong-do-bind
 
 # Warnings I'm not sure about.
 #	-fwarn-orphans  			# not sure if this is a problem
 
 # Warnings we would add if they had a better implementation.
-#	-fwarn-incomplete-patterns		# incomplete pattern warnings are buggy in GHC
 #	-fwarn-incomplete-record-updates 	# this isn't smart enough to be useful
 
 # Warnings that enforce programming styles that we don't use.
 # 	-fwarn-missing-methods			# we don't usually define coarbitrary in the Arbitrary class
-#	-fwarn-name-shadowing 			# too many good names are stolen by the prelude: first, last, max etc.
 #	-fwarn-missing-signatures		# I use this all the time.
 #	-fwarn-simple-patterns			# I use this all the time.
 #	-fwarn-tabs				# heresy!
