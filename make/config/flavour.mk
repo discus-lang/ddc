4138d94ed0b126b6542a8e676857ebe8382e5d7e
Ben Lippmeier <benl@ouroborus.net>
2010-11-26 07:51:14 +0000

    Fix -rtsopts handling conflict

--- make/config/flavour.mk
+++ make/config/flavour.mk
@@ -6,27 +6,27 @@ BuildFlavour    := $(strip $(BUILDFLAVOUR))
 
 # -- Distribution compile (fastest at runtime)
 ifeq "$(BuildFlavour)" "distro"
-GHC_FLAGS	:= -rtsopts -O2 $(GHC_WARNINGS) $(GHC_LANGUAGE)
+GHC_FLAGS	:= -O2 $(GHC_VERSION_FLAGS) $(GHC_WARNINGS) $(GHC_LANGUAGE)
 GCC_FLAGS	:= -std=c99 -O3 -Wundef
 
 # -- Development Compile (fastest compile)
 else ifeq "$(BuildFlavour)" "devel"
-GHC_FLAGS	:= -rtsopts -O0 $(GHC_WARNINGS) $(GHC_LANGUAGE)
+GHC_FLAGS	:= -O0 $(GHC_VERSION_FLAGS) $(GHC_WARNINGS) $(GHC_LANGUAGE)
 GCC_FLAGS	:= -std=c99 -O3 -Wall -Wextra
 
 # -- Debug compile
 else ifeq "$(BuildFlavour)" "devel_debug"
-GHC_FLAGS	:= -rtsopts -O0 $(GHC_WARNINGS) $(GHC_LANGUAGE)
+GHC_FLAGS	:= -O0 $(GHC_VERSION_FLAGS) $(GHC_WARNINGS) $(GHC_LANGUAGE)
 GCC_FLAGS	:= -std=c99 -Wundef -g
 
 # -- Profiling compile
 else ifeq "$(BuildFlavour)" "devel_prof"
-GHC_FLAGS	:= -rtsopts -O2 -prof -auto-all $(GHC_WARNINGS) $(GHC_LANGUAGE)
+GHC_FLAGS	:= -O2 -prof -auto-all $(GHC_VERSION_FLAGS) $(GHC_WARNINGS) $(GHC_LANGUAGE)
 GCC_FLAGS	:= -std=c99 -Wundef -g -pg
 
 # -- For Haskell Program Coverage
 else ifeq "$(BuildFlavour)" "devel_hpc"
-GHC_FLAGS	:= -rtsopts -fhpc $(GHC_WARNINGS) $(GHC_LANGUAGE)
+GHC_FLAGS	:= -fhpc $(GHC_VERSION_FLAGS) $(GHC_WARNINGS) $(GHC_LANGUAGE)
 GCC_FLAGS	:= -std=c99 -O3 -Wundef
 
 else 
