8aba1e366735920d49ba7bfea285370aba416eca
Ben Lippmeier <benl@ouroborus.net>
2010-11-26 10:33:05 +0000

    Comments and formatting to allocators

--- runtime/Alloc.c
+++ runtime/Alloc.c
@@ -12,9 +12,8 @@
 #include <stdio.h>
 
 
-// Initialise the allocation system.
-//	and allocate the heap.
-void	_allocInit	(size_t heapSize)
+// Initialise the allocation system and allocate the heap.
+void	_allocInit (size_t heapSize)
 {
 	_ddcHeapBase		= malloc (heapSize);
 	_ddcHeapPtr		= _ddcHeapBase;
@@ -29,16 +28,16 @@ void	_allocInit	(size_t heapSize)
 // Perform a garbage collection.
 void	_allocCollect
 		(size_t byteCount)	// How much space must be left over after the collection.
-					//	If we don't get at least this much back then the RTS will panic.
+					// If we don't get at least this much back then the RTS will panic.
 {
+	// Check how much data is in the heap before collection.
+	Word64	heapUsageStart	= _ddcHeapPtr - _ddcHeapBase;
+
 #if 	_DDC_PROFILE_GC
 	_ddcProfileMutatorEnd();
 	_ddcProfileCollectorStart();
+	allocBytes	+= heapUsageStart - _ddcProfile ->gc.lastCompactionSize
 #endif
-
-	// Check how much data is in the heap before collection.
-	Word64	heapUsageStart	= _ddcHeapPtr - _ddcHeapBase;
-	_PROFILE_GC (allocBytes += heapUsageStart - _ddcProfile ->gc.lastCompactionSize);
 
 	// Copy out the live data to the new heap.
 	_collectHeap
@@ -47,7 +46,6 @@ void	_allocCollect
 		, _ddcHeapMax
 		, _ddcHeapBackBase
 		, &_ddcHeapBackPtr);
-
 
 #if 	_DDC_PROFILE_GC
 	_ddcProfileCollectorEnd();
@@ -68,15 +66,13 @@ void	_allocCollect
 	_ddcHeapMax		= _ddcHeapBackMax;
 	_ddcHeapBackMax		= tmp;
 
-
 	// Check how much data is in the heap after collection.
 	Word64 heapUsageEnd	= _ddcHeapPtr - _ddcHeapBase;
 	_PROFILE_GC (lastCompactionSize = heapUsageEnd);
 
-
-	// If we haven't recovered enough space,
-	//	then the allocation that triggered this collection won't be able to complete,
-	//	so we're screwed. Emit a RTS panic.
+	// If we haven't recovered enough space, then the allocation that triggered this
+	// 	collection won't be able to complete, so we're screwed.
+	// TODO: instead of just panicing we should grow the heap.
 	if (_ddcHeapPtr + byteCount > _ddcHeapMax)
 		_panicOutOfHeap (byteCount, (UInt64)(_ddcHeapMax - _ddcHeapBase));
 }
