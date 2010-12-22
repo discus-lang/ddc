2df7e5654a0925dcbfd7f5de63fef539e602ebfe
Erik de Castro Lopo <erikd@mega-nerd.com>
2010-12-22 11:29:33 +0000

    Add test/Broken-skip/T210-CtorOrder test.

---
+++ test/Broken-skip/T210-CtorOrder/Main.hs
@@ -0,0 +1,21 @@
+
+import Data.Char
+import Data.List
+import System.Exit
+
+main :: IO ()
+main
+ = do	text <- readFile "test/18-ImportExport/T210-CtorOrder/A.di"
+	case extract text of
+	  "ABC"	-> putStrLn "Ok"
+	  _	-> exitFailure
+
+
+extract :: String -> String
+extract text
+ = filter isAlpha
+	$ dropWhile (/= '=')
+	$ intercalate " "
+	$ filter (\s -> length s > 0)
+	$ filter (\s -> not (isPrefixOf "--" s))
+	$ lines text
