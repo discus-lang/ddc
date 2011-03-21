cca60cdcc99cef7711a6d97996bb33c880e95a63
Ben Lippmeier <benl@ouroborus.net>
2011-03-21 11:01:15 +0000

    Start on coq proof

---
+++ proof/Bool.v
@@ -0,0 +1,56 @@
+
+Module Bool.
+
+
+Inductive bool : Type :=
+ | true  : bool
+ | false : bool.
+
+
+Fixpoint beq_nat (n m : nat) : bool :=
+ match n, m with
+ | O, O       => true
+ | O, S _     => false
+ | S _, O     => false
+ | S n1, S n2 => beq_nat n1 n2
+ end.
+
+
+Theorem beq_nat_refl
+ : forall n : nat
+ , true = beq_nat n n.
+Proof.
+ intros. 
+ induction n.
+  tauto.
+  simpl. tauto. 
+Qed.
+
+
+Theorem beq_nat_sym
+ : forall n1 n2 : nat
+ , beq_nat n1 n2 = beq_nat n2 n1.
+Proof. 
+ induction n1.
+ intro n2. destruct n2.
+  tauto. tauto.
+  intro. destruct n2. tauto.
+  simpl. auto.
+Qed.
+
+
+Theorem beq_nat_eq
+ : forall n1 n2 : nat
+ , true = beq_nat n1 n2 -> n1 = n2.
+Proof.
+ induction n1.
+ destruct n2.
+  simpl. tauto.
+  simpl. intro contra. inversion contra.
+  intros n2. destruct n2.
+   simpl. intro contra. inversion contra.
+   simpl. intro eq. apply IHn1 in eq. rewrite -> eq. tauto.
+Qed.
+
+End Bool.
+
---
+++ proof/Main.v
@@ -0,0 +1,71 @@
+
+Require Export Bool.
+
+
+(* names ******************************)
+Inductive name : Type :=
+ Name : nat -> name.
+
+
+Definition beq_name n1 n2 := 
+ match (n1, n2) with 
+  (Name x1, Name x2) => beq_nat x1 x2
+ end.
+
+
+Theorem beq_name_refl 
+ : forall n
+ , true = beq_name n n.
+Proof.
+ intros. destruct n. apply beq_nat_refl.
+Qed.
+
+
+Theorem beq_name_eq 
+ : forall n1 n2
+ , true = beq_name n1 n2 -> n1 = n2.
+Proof.
+ intros n1 n2. 
+ destruct n1. destruct n2.
+ unfold beq_name.
+ intros eq. apply beq_nat_eq in eq. auto.
+Qed.
+
+
+Theorem beq_name_sym
+ : forall n1 n2
+ , beq_name n1 n2 = beq_name n2 n1.
+Proof.
+ intros n1 n2.
+ destruct n1. destruct n2.
+ unfold beq_name.
+ apply beq_nat_sym.
+Qed.
+
+ 
+
+(* types **************************************************)
+Inductive ty : Type :=
+ | TVar  : name -> ty
+ | TFun  : ty -> ty -> ty.
+
+Notation A := (TVar (Name 0)).
+Notation B := (TVar (Name 1)).
+Notation C := (TVar (Name 2)).
+
+
+(* terms **************************************************)
+Inductive exp : Type :=
+ | XVar  : name -> exp
+ | XApp  : exp  -> exp -> exp
+ | XLam  : name -> ty  -> exp -> exp.
+
+
+
+
+
+
+
+
+
+
