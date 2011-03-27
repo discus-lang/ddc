046dd965fec1f6cf14f21ff8c5afa1be627d89c7
Ben Lippmeier <benl@ouroborus.net>
2011-03-27 09:31:25 +0000

    Prove environment invariance for SystemF

--- proof/SystemF/TyJudge.v
+++ proof/SystemF/TyJudge.v
@@ -17,21 +17,74 @@ Definition kienv := partial_map ki.
    Regular System-F isn't higher kinded, so we just need to check
    that all the type variables are in the correct namespace.
  *)
+Inductive KIND : kienv -> ty -> ki -> Prop :=
+ | KICon 
+   :  forall kenv x
+   ,  KIND kenv (TCon x) KStar
+
+ | KIVar
+   :  forall kenv x K
+   ,  space_of_name x = SType
+   -> kenv x          = some K
+   -> KIND kenv (TVar x) KStar
+
+ | KIForall
+   :  forall kenv a T
+   ,  space_of_name a = SType
+   -> KIND (extend kenv a KStar) T KStar
+   -> KIND kenv (TForall a T) KStar
+
+ | KIFun 
+   :  forall kenv t1 t2
+   ,  KIND kenv t1 KStar
+   -> KIND kenv t2 KStar
+   -> KIND kenv (TFun t1 t2) KStar.
+
+Hint Constructors KIND.
+
+
+(* For well formed types, the only free variables are type variables *)
+Lemma only_type_vars_in_types
+ :  forall a kenv T K
+ ,  freeT a T
+ -> KIND kenv T K
+ -> space_of_name a = SType.
+Proof. 
+ intros.
+ generalize dependent kenv. 
+ induction T.
+ Case "TCon".
+  inversion H.
+ Case "TVar".
+  intros.
+  inversion H0. subst.
+  inversion H.  subst. auto.
+ Case "TForall".
+  intros.
+  eapply IHT. 
+  inversion H.  subst. auto.
+  inversion H0. subst. eauto.
+ Case "TFun".
+  intros. inversion H0. subst.
+  inversion H. subst.
+  eapply IHT1; eauto.
+  eapply IHT2; eauto.
+Qed.
 
 
 (* Check well typeness of terms. *)
 Inductive TYPE : kienv -> tyenv -> exp -> ty -> Prop :=
  | TYVar 
    :  forall kenv tenv x T
-   ,  space_of_name x = SpaceValue
+   ,  space_of_name x = SValue
    -> tenv x = some T
    -> TYPE kenv tenv (XVar x) T
 
  | TYLam 
    :  forall kenv tenv x T11 T12 t12
-   ,  space_of_name x = SpaceValue
+   ,  space_of_name x = SValue
    -> TYPE kenv (extend tenv x T11) t12 T12
-   -> TYPE kenv tenv                (XLam x T11 t12) (TFun T11 T12)
+   -> TYPE kenv tenv (XLam x T11 t12) (TFun T11 T12)
 
  | TYApp 
    :  forall kenv tenv t1 t2 T11 T12
@@ -41,13 +94,14 @@ Inductive TYPE : kienv -> tyenv -> exp -> ty -> Prop :=
 
  | TYLAM
    :  forall kenv tenv a t12 T12
-   ,  space_of_name a = SpaceType
+   ,  space_of_name a = SType
    -> TYPE (extend kenv a KStar) tenv t12 T12 
    -> TYPE kenv tenv (XLAM a t12) (TForall a T12)
 
  | TYAPP
    :  forall kenv tenv a T11 t1 T2
    ,  TYPE kenv tenv t1 (TForall a T11)
+   -> KIND kenv T2 KStar
    -> TYPE kenv tenv (XAPP t1 T2) (substTT a T2 T11). 
 
 Hint Constructors TYPE.
@@ -59,7 +113,7 @@ Hint Constructors TYPE.
 Lemma tyenv_contains_free_valvars
  :  forall x t T kenv tenv
  ,  freeX x t
- -> space_of_name x = SpaceValue
+ -> space_of_name x = SValue
  -> TYPE kenv tenv t T
  -> (exists T', tenv x = some T').
 Proof.
@@ -88,37 +142,101 @@ Proof.
   eapply IHfreeX. auto. eauto.
  Case "XApp/APP2".
   inversion H1. subst.
-  inversion H6. subst.
-  
-  inversion H. subst.
-  
-  ea
+  eapply only_type_vars_in_types with (a:=x) in H8.
+  rewrite H0 in H8. inversion H8.
+  auto.
+Qed.
 
+Definition freeX_type a t
+ := space_of_name a = SType -> freeX a t.
+Hint Unfold freeX_type.
+
+Definition freeX_value x t
+ := space_of_name x = SValue -> freeX x t.
+Hint Unfold freeX_value.
+
+
+(* We can check a type with a larger kind environment, 
+   and get the same kind. *)
+Lemma kienv_invariance
+ :  forall kenv kenv' T K
+ ,  KIND kenv  T K
+ -> (forall a, freeT a T -> kenv a = kenv' a)
+ -> KIND kenv' T K.
+Proof.
+ intros.
+ generalize dependent kenv'.
+ induction H; intros.
+ Case "TCon".
+  auto.
+ Case "TVar".
+  eapply KIVar. auto. rewrite <- H1.
+  eauto. auto.
+ Case "TForall".
+  apply KIForall. auto.
+  apply IHKIND.
+   intros.
+   unfold extend. remember (beq_name a a0) as e. destruct e.
+   auto. apply false_name_neq in Heqe. eauto.
+ Case "TFun".
+  apply KIFun; auto.
 Qed.
 
 
-
-
-(* We can ignore elements of the type environment if that
-   variable is not free in the expresison being checked. 
+(* We can check an exp with larger kind and type environments,
+   and get the same type.
  *)
 Lemma tyenv_invariance 
- : forall env env' t T
- ,  TYPE env t T
- -> (forall x, freeX x t -> env x = env' x)
- -> TYPE env' t T.
+ :  forall tenv tenv' kenv kenv' t T
+ ,  TYPE kenv tenv t T
+ -> (forall a, freeX_type  a t -> kenv a = kenv' a)
+ -> (forall x, freeX_value x t -> tenv x = tenv' x)
+ -> TYPE kenv' tenv' t T.
 Proof.
  intros.
- generalize dependent env'.
+ generalize dependent tenv'.
+ generalize dependent kenv'.
  induction H; intros.
  Case "XVar".
-  apply TYVar. rewrite <- H0; auto.
+  apply TYVar. auto. rewrite <- H0.
+  symmetry. eapply H2. auto.
  Case "XLam".
-  apply TYLam. apply IHTYPE.
-  intros. unfold extend.
-  remember (beq_name x x0) as e. destruct e; auto.
+  apply TYLam. auto.
+  apply IHTYPE.
+   intros. apply H1.
+    unfold freeX_type. intro.
+    apply FreeX_lam. unfold not. intro. subst.
+    rewrite H in H4. inversion H4. auto.
+   intros. unfold extend.
+    remember (beq_name x x0) as e. destruct e; auto.
+    apply H2. unfold freeX_value. intro.
+    apply FreeX_lam. unfold not. intro. subst.
+    apply false_name_neq in Heqe. tauto.
+    auto.
  Case "XApp".
-  eapply TYApp; eauto.
+  eapply TYApp.
+   apply IHTYPE1; auto.
+   apply IHTYPE2; auto.
+ Case "XLAM".
+  eapply TYLAM.
+   auto. apply IHTYPE.
+    intros. unfold extend.
+     remember (beq_name a a0) as e. destruct e; auto.
+     apply H1. unfold freeX_type. intro.
+     apply FreeX_LAM. unfold not. intro. subst.
+     apply false_name_neq in Heqe. tauto.
+     auto.
+    intros. apply H2. unfold freeX_value. intro.
+     apply FreeX_LAM. unfold not. intro. subst.
+     rewrite H in H4. inversion H4. auto.
+ Case "XAPP".
+  apply TYAPP.
+   apply IHTYPE.
+    intros. apply H1. eauto.
+    intros. apply H2. eauto.
+   eapply kienv_invariance.
+    eauto.
+    intros. apply H1. eauto.
 Qed.
 
 
