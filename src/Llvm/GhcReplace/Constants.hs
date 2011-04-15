87506b8a28e0153e1c3d0c5bd91815b6d6052acb
Erik de Castro Lopo <erikd@mega-nerd.com>
2011-04-15 23:49:50 +0000

    LLVM : Make wORD_SIZE equal to SIZEOF_VOID_P.

--- src/Llvm/GhcReplace/Constants.hs
+++ src/Llvm/GhcReplace/Constants.hs
@@ -2,9 +2,13 @@
 -- GHC compiler. This is needed to allow the use of David Terei's LLVM output
 -- code that is part of GHC.
 
+{-# OPTIONS -cpp #-}
+
 module Llvm.GhcReplace.Constants where
 
+#include "ghcconfig.h"
+
 wORD_SIZE :: Int
-wORD_SIZE = 8
+wORD_SIZE = SIZEOF_VOID_P
 
 
