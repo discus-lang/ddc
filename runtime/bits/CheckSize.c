0646d1f53b854abd65087bd6760af01a6c8970d4
Erik de Castro Lopo <erikd@mega-nerd.com>
2011-04-22 02:30:06 +0000

    runtime/bits/CheckSize.c : Fix compiler warnings.

--- runtime/bits/CheckSize.c
+++ runtime/bits/CheckSize.c
@@ -6,13 +6,14 @@
 
 int	main (int argc, char** argv)
 {
-	printf ("sizeof unsigned int = %d\n", sizeof (unsigned int));
-	printf ("sizeof long         = %d\n", sizeof (long));
-	printf ("sizeof long long    = %d\n", sizeof (long long));
-	printf ("sizeof char*        = %d\n", sizeof (char*));
-	printf ("sizeof void*        = %d\n", sizeof (void*));
-	printf ("sizeof float        = %d\n", sizeof (float));
-	printf ("sizeof double       = %d\n", sizeof (double));
-	printf ("sizeof char         = %d\n", sizeof (char));
-	printf ("sizeof wchar_t      = %d\n", sizeof (wchar_t));
+	printf ("sizeof unsigned int = %zu\n", sizeof (unsigned int));
+	printf ("sizeof long         = %zu\n", sizeof (long));
+	printf ("sizeof long long    = %zu\n", sizeof (long long));
+	printf ("sizeof char*        = %zu\n", sizeof (char*));
+	printf ("sizeof void*        = %zu\n", sizeof (void*));
+	printf ("sizeof float        = %zu\n", sizeof (float));
+	printf ("sizeof double       = %zu\n", sizeof (double));
+	printf ("sizeof char         = %zu\n", sizeof (char));
+	printf ("sizeof wchar_t      = %zu\n", sizeof (wchar_t));
+	return 0;
 }
