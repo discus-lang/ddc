0a34d7542ee2007dd2c340d4926e2e548d8011ba
Ben Lippmeier <benl@ouroborus.net>
2011-05-31 10:53:12 +0000

    Simple Data: working on relational stuff in type_tyenv_insert

--- proof/Simple_Data/TyJudge.v
+++ proof/Simple_Data/TyJudge.v
@@ -27,8 +27,8 @@ Inductive TYPE : defs -> tyenv -> exp -> ty -> Prop :=
  | TYCon 
    :  forall ds te xs dc tsArgs tResult
    ,  getDataDef dc ds = Some (DefData dc tsArgs tResult)
-   -> (forall xs, Forall2 (TYPE ds te) xs tsArgs)
-  -> TYPE ds te (XCon dc xs) tResult
+   -> Forall2 (TYPE ds te) xs tsArgs
+   -> TYPE ds te (XCon dc xs) tResult
 
  | TYCase
    :  forall ds te xObj alts tPat tResult
@@ -46,21 +46,6 @@ with TYPEA : defs -> tyenv -> alt -> ty -> ty -> Prop :=
 
 Hint Constructors TYPE.
 Hint Constructors TYPEA.
-
-
-Lemma Forall2_exists_left
- : forall (A B: Type) (R: A -> B -> Prop) x xs ys
- ,  In x xs 
- -> Forall2 R xs ys 
- -> (exists y, R x y).
-Proof.
- intros.
- induction H0.
-  false.
-  simpl in H. destruct H.
-   subst. eauto.
-   eapply IHForall2. eauto.
-Qed.
 
 
 (* Well Formedness **************************************************)
@@ -93,7 +78,6 @@ Proof.
   rewrite Forall_forall.
    intros. 
    inverts H0.
-   specialize H8 with xs.
    lets D: Forall2_exists_left H1 H8.
     destruct D. 
     apply H in H0. eauto. eauto.
@@ -119,22 +103,61 @@ Hint Resolve type_wfX.
    the new one.
  *)
 Lemma type_tyenv_insert
- :  forall e ix x t1 t2
- ,  TYPE e x t1
- -> TYPE (insert ix t2 e) (liftX ix x) t1.
+ :  forall ds te ix x t1 t2
+ ,  TYPE ds te x t1
+ -> TYPE ds (insert ix t2 te) (liftX ix x) t1.
 Proof.
- intros. gen ix e t1.
- induction x; intros; simpl; inverts H; eauto.
+ intros.
+ gen ix ds te t1.
+
+ eapply 
+  (exp_mutind 
+    (fun x => forall ix ds te t1
+           ,  TYPE ds te x t1    
+           -> TYPE ds (insert ix t2 te) (liftX ix x) t1) 
+    (fun a => forall ix ds te t3 t4
+           ,  TYPEA ds te a t3 t4 
+           -> TYPEA ds (insert ix t2 te) (liftA ix a) t3 t4))
+  ; intros; simpl.
 
  Case "XVar".
+  inverts H.
   lift_cases; intros; auto.
 
  Case "XLam".
+  inverts H0.
   apply TYLam.
   rewrite insert_rewind. 
-   apply IHx. auto.
-Qed.
+   apply H. auto.
 
+ Case "XApp".
+  inverts H1.
+  eapply TYApp.
+   eauto. eauto.
+
+ Case "XCon".
+  inverts H0.
+  eapply TYCon. 
+   eauto.
+   apply (Forall2_map_left (TYPE ds (insert ix t2 te))).
+   apply (Forall2_impl_In (TYPE ds te)); eauto.
+
+ Case "XCase".
+  inverts H1.
+  eapply TYCase. 
+   eauto.
+   apply Forall_map.
+   eapply (Forall_impl_In (fun a => TYPEA ds te a tPat t1)); eauto.
+
+ Case "XAlt".
+  inverts H0.
+  eapply TYAlt. eauto.
+  assert ( insert ix t2 te ++ envOfList tsArgs 
+         = insert (ix + List.length tsArgs) t2 (te ++ envOfList tsArgs)).
+      admit.
+  rewrite H0.
+  eapply H.
+Qed. 
 
 Lemma type_tyenv_weaken
  :  forall e x t1 t2
