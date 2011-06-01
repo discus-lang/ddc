f76cd4af0da094aaa1656591d68a7550a5496c75
Ben Lippmeier <benl@ouroborus.net>
2011-06-01 14:17:52 +0000

    Simple Data: Do most of Subst Exp/Exp need to generalise liftX to push all ctor args on env at once

--- proof/Simple_Data/SubstExpExp.v
+++ proof/Simple_Data/SubstExpExp.v
@@ -9,37 +9,91 @@ Require Export Exp.
    place at top level.
  *)
 Theorem subst_value_value_ix
- :  forall ix e x1 x2 t1 t2
- ,  get  e ix = Some t2
- -> TYPE e           x1 t1
- -> TYPE (drop ix e) x2 t2
- -> TYPE (drop ix e) (substX ix x2 x1) t1.
+ :  forall ix ds te x1 x2 t1 t2
+ ,  get  te ix = Some t2
+ -> TYPE ds te           x1 t1
+ -> TYPE ds (drop ix te) x2 t2
+ -> TYPE ds (drop ix te) (substX ix x2 x1) t1.
 Proof.
- intros. gen ix e x2 t1.
- induction x1; intros; simpl; inverts H0; eauto.
+ intros. gen ix ds te x2 t1 t2.
+
+ eapply 
+  (exp_mutind 
+    (fun x1 => forall ix ds te x2 t1
+            ,  TYPE ds te           x1 t1
+            -> forall t2
+            ,  get te ix = Some t2
+            -> TYPE ds (drop ix te) x2 t2
+            -> TYPE ds (drop ix te) (substX ix x2 x1) t1)
+
+    (fun a1 => forall ix ds te x2 t11 t12 t2
+            ,  get te ix = Some t2
+            -> TYPEA ds te           a1 t11 t12
+            -> TYPE  ds (drop ix te) x2 t2
+            -> TYPEA ds (drop ix te) (substA ix x2 a1) t11 t12))
+  ; intros; simpl.
 
  Case "XVar".
+  inverts H.
   fbreak_nat_compare.
   SCase "i = ix".
-   rewrite H in H4. inverts H4. auto.
+   rewrite H0 in H5. inverts H5. auto.
 
   SCase "n < ix".
    apply TYVar.
-   rewrite <- H4.
+   rewrite <- H5.
     apply get_drop_above. auto.
 
   SCase "n > ix".
    apply TYVar.
    destruct n.
     false. omega.
-    simpl. nnat. rewrite <- H4.
+    simpl. nnat. rewrite <- H5.
      apply get_drop_below. omega.
 
  Case "XLam".
+  inverts H0.
   apply TYLam.
   rewrite drop_rewind.
-  apply IHx1; auto.
+  eapply H; eauto.
    simpl. apply type_tyenv_weaken. auto.
+
+ Case "XApp". 
+  inverts H1. eauto.
+
+ Case "XCon".
+  inverts H0.
+  eapply TYCon.
+   eauto.
+   rewrite Forall_forall in H.
+   apply (Forall2_map_left (TYPE ds (drop ix te))).
+   apply (Forall2_impl_In  (TYPE ds te)); eauto.
+
+ Case "XCase".
+  inverts H1.
+  eapply TYCase.
+   eauto.
+   rewrite Forall_forall in H0.
+   eapply Forall_map.
+   eapply (Forall_impl_In (fun a => TYPEA ds te a tPat t1)); eauto.
+
+ Case "AAlt".
+  inverts H1.
+  eapply TYAlt. 
+   eauto.
+   assert  ( drop ix  te ++ envOfList ts
+           = drop (ix + length (envOfList ts)) (te ++ envOfList ts)).
+    admit. rewrite H1. clear H1.
+   simpl.
+   (* TODO: Definition of substA is broken.
+            Need to handle all of the ctor args being pushed
+            onto the environment at onceee.
+            Generalise liftX to lift across several new types at once *)
+   eapply H.
+    eauto.
+    eapply get_append_some. eauto.
+    
+
 Qed.
 
 
