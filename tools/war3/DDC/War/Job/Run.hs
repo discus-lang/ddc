e446fa15601fc46a036f5ba2d79bb7158daed731
Ben Lippmeier <benl@ouroborus.net>
2011-07-11 08:55:29 +0000

    war3: whitespace

--- tools/war3/DDC/War/Job/Run.hs
+++ tools/war3/DDC/War/Job/Run.hs
@@ -12,12 +12,12 @@ jobRun :: Job -> Build [Result]
 jobRun (JobRun	testName _wayName _fileName
 		mainBin mainRunOut mainRunErr)
  = do	needs mainBin
-	
+ 
 	-- Run the binary.
 	(time, (code, strOut, strErr))
 	 <- runTimedCommand 
 	 $  systemTee False mainBin ""
-	
+
 	-- Write its output to files.
 	atomicWriteFile mainRunOut strOut
 	atomicWriteFile mainRunErr strErr
