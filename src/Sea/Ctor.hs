fcf8a1faea51a06f2b24aaafc2263e087efed83d
Erik de Castro Lopo <erikd@mega-nerd.com>
2011-07-20 12:24:10 +0000

    Sea.Ctor : Fix calculation of DataM payload size.

--- src/Sea/Ctor.hs
+++ src/Sea/Ctor.hs
@@ -4,8 +4,11 @@ module Sea.Ctor
 	(expandCtorTree)
 where
 import Util
+import DDC.Base.DataFormat
+import DDC.Main.Error
 import DDC.Sea.Exp
 import DDC.Var
+import DDC.Var.PrimId
 import Shared.VarUtil		(VarGenM, newVarN)
 import qualified Shared.Unique	as Unique
 import qualified Config.Config	as Config
@@ -13,6 +16,7 @@ import qualified Data.Map	as Map
 
 type	ExM	= VarGenM
 
+stage = "Sea.Ctor"
 
 -- | Expand the definitions of data constructors in this tree.
 expandCtorTree :: Tree () -> Tree ()
@@ -38,28 +42,27 @@ expandCtor
 	:: CtorDef
 	-> ExM [Top ()]
 
-expandCtor (CtorDef vCtor tCtor arity tag fields types)
+expandCtor ctor@(CtorDef vCtor tCtor arity tag fields types)
  = do	-- var of the constructed object.
 	nObj		<- liftM NAuto $ newVarN NameValue
 
 	-- Determine how the fields are going to be store. Constructors with
 	-- one or more  unboxed fields use a different allocation method and
 	-- layout to those only boxed fields.
-	let (boxed, unboxed)
-			= partition typeIsBoxed types
+	let boxedCount	= length $ filter typeIsBoxed types
 
-	let allocation	= if null unboxed
-			then PAllocData vCtor arity
-			else PAllocDataM vCtor (length boxed)
-					(dataMSize (length boxed) unboxed)
+	let useDataM	= hasUnboxedFields ctor
+
+	let allocation	= if useDataM
+			then PAllocDataM vCtor boxedCount (payloadSize types)
+			else PAllocData vCtor arity
 
 	-- allocate the object
 	let allocS 	= SAssign (XVar nObj tPtrObj) tPtrObj (XPrim (MAlloc allocation) [])
 
-	let expandField = if null unboxed
-			then expandFieldData
-			else expandFieldDataM vCtor
-
+	let expandField = if useDataM
+			then expandFieldDataM vCtor
+			else expandFieldData
 
 	-- Initialise all the fields.
 	(stmtss, mArgVs)
@@ -110,6 +113,38 @@ expandFieldData nObj ixArg tArg
 
 
 
+-- | Calculate the size of the DataM payload
+payloadSize types
+ = let (boxed, unboxed) = partition typeIsBoxed types
 
-dataMSize boxCount unboxed
- = boxCount * Config.pointerBytes + (length unboxed) * 4
+   in	(length boxed) * Config.pointerBytes
+	+ foldl' calculatePaddedLength 0 (map unboxedSize unboxed)
+
+
+calculatePaddedLength :: Int -> Int -> Int
+calculatePaddedLength accum fieldLen
+ = if accum `mod` fieldLen == 0
+	then accum + fieldLen
+	else accum - (accum `div` fieldLen) + 2 * fieldLen
+
+
+unboxedSize :: Type -> Int
+unboxedSize (TCon (TyConUnboxed v))
+ = case varId v of
+	VarIdPrim (TChar (UnboxedBits 32))	-> 4
+
+	VarIdPrim (TFloat (UnboxedBits 32))	-> 4
+	VarIdPrim (TFloat (UnboxedBits 64))	-> 8
+
+	VarIdPrim (TInt (UnboxedBits 8))	-> 1
+	VarIdPrim (TInt (UnboxedBits 16))	-> 2
+	VarIdPrim (TInt (UnboxedBits 32))	-> 4
+	VarIdPrim (TInt (UnboxedBits 64))	-> 8
+
+	VarIdPrim (TWord (UnboxedBits 8))	-> 1
+	VarIdPrim (TWord (UnboxedBits 16))	-> 2
+	VarIdPrim (TWord (UnboxedBits 32))	-> 4
+	VarIdPrim (TWord (UnboxedBits 64))	-> 8
+
+	id -> panic stage $ "unboxedSize : " ++ show id
+
