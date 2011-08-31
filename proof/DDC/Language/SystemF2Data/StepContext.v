a708c8fa91c849f9e09dee66b4cc4516f8fabd3f
Ben Lippmeier <benl@ouroborus.net>
2011-08-31 02:11:50 +0000

    SystemF2Data: Add evaluation contexts

---
+++ proof/DDC/Language/SystemF2Data/StepContext.v
@@ -0,0 +1,48 @@
+
+Require Import DDC.Language.SystemF2Data.TyJudge.
+Require Export DDC.Language.SystemF2Data.Exp.
+Require Export DDC.Language.SystemF2Data.SubstExpExp.
+Require Export DDC.Data.Context.
+
+
+(********************************************************************)
+(*  Evaluation contexts for expressions.
+    This describes a place in the exp AST where the sub-expression
+    there is able to take an evaluation step *)
+Inductive exp_ctx : (exp -> exp) -> Prop :=
+
+ (* The top level context names the entire expression *)
+ | XcTop 
+   : exp_ctx  (fun x => x)
+
+ (* Left of an application *)
+ | XcApp1
+   :  forall x2
+   ,  exp_ctx  (fun xx => XApp xx x2)
+
+ (* The right of an application can step only when the left is
+    already a value. *)
+ | XcApp2 
+   :  forall v1
+   ,  value v1
+   -> exp_ctx  (fun xx => XApp v1 xx)
+
+ | XcAPP
+   :  forall x1 t2
+   ,  exp_ctx  (fun xx => XAPP x1 t2)
+
+ (* As the XCon constructor contains a list of sub-expressions, 
+    we need an additional exps_ctx context to indicate which one 
+    we're talking about. *)
+ | XcCon 
+   :  forall dc ts C
+   ,  exps_ctx wnfX C 
+   -> exp_ctx  (fun xx => XCon dc ts (C xx))
+
+ (* We need to reduce the discriminant of a case to a value. *)
+ | XcCase
+   :  forall alts
+   ,  exp_ctx  (fun xx => XCase xx alts).
+
+Hint Constructors exp_ctx. 
+
