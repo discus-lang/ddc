17c3f2acf2ffef417db6bd7a1d3d68dfdf7592f8
Ben Lippmeier <benl@ouroborus.net>
2011-09-26 12:36:04 +0000

    SystemF2Store: Add def of store model and well typed stores

--- proof/DDC/Language/SystemF2Store/Store.v
+++ proof/DDC/Language/SystemF2Store/Store.v
@@ -2,6 +2,7 @@
 Require Export DDC.Language.SystemF2Store.TyJudge.
 Require Export DDC.Language.SystemF2Store.Exp.
 
+
 (********************************************************************)
 (* Storeable values are the ones that we can keep directly in 
    store bindings *)
@@ -12,6 +13,15 @@ Inductive svalue :=
 Hint Constructors svalue.
 
 
+(* Store binding with a constructor tag and some storeable values *)
+Inductive sbind :=
+ | SObj : datacon -> list svalue -> sbind.
+
+
+Definition store  := list sbind.
+Hint Unfold store.
+
+
 Definition takeSValueOfExp (xx : exp) : option svalue :=
  match xx with
  | XLoc n    => Some (SLoc n)
@@ -20,6 +30,7 @@ Definition takeSValueOfExp (xx : exp) : option svalue :=
  | _         => None
  end.
 
+
 Definition expOfSValue (s: svalue) : exp :=
  match s with
  | SLoc n    => XLoc n
@@ -27,14 +38,28 @@ Definition expOfSValue (s: svalue) : exp :=
  | SLam t x  => XLam t x
  end.
 
+
 Definition svalueOf (xx : exp) (sv : svalue) : Prop
  := takeSValueOfExp xx = Some sv.
 
 
-(* Store binding with a constructor tag and some storeable values *)
-Inductive sbind :=
- | SObj : datacon -> list svalue -> sbind.
+(* Store typing models the store.
+   All types in the store typing have a corresponding binding in the store *)
+Definition SM (ds: defs) (st: stenv) (s: store)
+ := length st = length s.
 
 
-Definition store  := list sbind.
-Hint Unfold store.
+(* Store is well typed, *)
+Definition ST (ds: defs) (st: stenv) (ss: store)
+ := forall i dcObj svFields tcObj tsParam tsFields
+ ,  DEFSOK ds
+ -> get i ss = Some (SObj dcObj svFields)
+ -> get i st = Some (makeTApps (TCon tcObj) tsParam)
+ /\ getDataDef dcObj ds = Some (DefData dcObj tsFields tcObj)
+ /\ Forall2 (TYPE ds nil nil st)
+            (map expOfSValue svFields)
+            (map (substTTs 0 tsParam) tsFields).
+
+
+
+
