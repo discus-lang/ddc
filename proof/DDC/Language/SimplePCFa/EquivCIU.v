c8852c9c277a810b2725356b456eaf36f7330915
Ben Lippmeier <benl@ouroborus.net>
2011-11-28 10:38:51 +0000

    SimplePCFa: hacks

--- proof/DDC/Language/SimplePCFa/EquivCIU.v
+++ proof/DDC/Language/SimplePCFa/EquivCIU.v
@@ -26,7 +26,7 @@ Definition EQCIU (te: tyenv) (x1 x2: exp) (t: ty)
       ,  Forall2 (fun v => TYPEX nil (XVal v)) vs te    
       -> csubstVXs vs x1 x1'
       -> csubstVXs vs x2 x2'
-      -> (TERMF f x1' <-> TERMF f x2')).
+      -> (TERMS f x1' <-> TERMS f x2')).
 
 
 Lemma eqciu_from_eval
@@ -103,57 +103,23 @@ Proof.
  intros. subst. red. intros.
  unfold csubstVXs in H4.
  unfold csubstVXs in H5. rip.
- split.
+ split; intros.
 
- intros.
-  simpl.
-  induction f. admit.
-  simpl in IHf.
-  simpl in H4.
-  eapply IHf.
+ simpl in H4.
 
-simpl in H4.
-  eapply RfLetPush.
-  eapply RfLetPush.
-  inverts H4.
-   admit.       (* fixme *)
-   inverts H5.
+ simpl in *.
+ inverts H4.
 
- intros.
-  simpl. 
-  simpl in H4.
-  inverts H4. 
-   admit.      (* fixme *)
-   inverts H5.
+  admit. (* Finish me! *)
 Qed.
 
-(*
 
-(* TODO: finish me, swap indices 0 1 *)
-Definition swapXX (d: nat) (x: exp) : exp
- := x.
 
-(* Swap two let bindings, 
-   changes the order of operations. *)
-Lemma ecui_if_let_let_swap
- :  forall z1 z2 t1 x1 t2 x2 t3 x3
- ,  z1 = XLet t1 x1             (XLet t2 x2            x3)
- -> z2 = XLet t2 (lowerXX 0 x2) (XLet t1 (liftXX 0 x1) (swapXX 0 x3))
- -> wfX 0 x2
- -> TYPEX nil z1 t3
- -> EQCIU  z1  z2 t3.
-Proof.
- intros. subst.
- red. rip.
-  admit. (* z2 has same type *)
 
-  split.
-   intros.
 
-  
- (* TODO *)
- admit.
- admit.
-Qed.
 
-*)
+
+
+
+
+
