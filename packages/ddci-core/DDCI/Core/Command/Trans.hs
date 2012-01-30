1393496ae2c3db9f14efc2f5043928ebd3c345ac
Ben Lippmeier <benl@ouroborus.net>
2012-01-30 03:33:09 +0000

    ddci-core: add missing Trans module

---
+++ packages/ddci-core/DDCI/Core/Command/Trans.hs
@@ -0,0 +1,55 @@
+
+
+module DDCI.Core.Command.Trans
+        (cmdTrans)
+where
+import DDC.Core.Check
+import DDC.Type.Equiv
+import DDCI.Core.Transform
+import DDCI.Core.State
+import DDCI.Core.Command.Check
+import DDCI.Core.Eval.Env
+import DDC.Base.Pretty
+import qualified DDC.Type.Env   as Env
+
+
+-- | Apply the current transform to an expression.
+cmdTrans :: State -> Int -> String -> IO ()
+cmdTrans state lineStart str
+ = cmdParseCheckExp lineStart str >>= goStore
+ where
+        -- Expression had a parse or type error.
+        goStore Nothing
+         = do   return ()
+
+        -- Expression is well-typed.
+        goStore (Just (x, t1, eff1, clo1))
+         = do   let x'  = applyTransformX (stateTransform state) x
+
+                case checkExp primDataDefs Env.empty x' of
+                  Right (_, t2, eff2, clo2)
+                   |  equivT t1 t2
+                   ,  equivT eff1 eff2
+                   ,  equivT clo1 clo2
+                   -> do putStrLn (pretty $ ppr x')
+                         return ()
+
+                   | otherwise
+                   -> do putStrLn $ pretty $ vcat
+                            [ text "* CRASH AND BURN: Transform is not type preserving."
+                            , ppr x'
+                            , text "::"  <+> ppr t2
+                            , text ":!:" <+> ppr eff2
+                            , text ":$:" <+> ppr clo2 ]
+
+                  Left err
+                   -> do putStrLn $ pretty $ vcat
+                            [ text "* CRASH AND BURN: Type error in transformed program."
+                            , ppr err
+                            , text ""]
+
+                         putStrLn "Transformed expression:"
+                         
+                         putStrLn (pretty $ ppr x')
+                         return ()
+
