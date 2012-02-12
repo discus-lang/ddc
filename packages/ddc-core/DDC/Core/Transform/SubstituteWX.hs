7c5187f0a05cc15ec27d9082ecdb936413895bce
Ben Lippmeier <benl@ouroborus.net>
2012-02-12 11:57:28 +0000

    ddc-core: refactor SubstituteWX to use new subst framework

--- packages/ddc-core/DDC/Core/Transform/SubstituteWX.hs
+++ packages/ddc-core/DDC/Core/Transform/SubstituteWX.hs
@@ -9,39 +9,50 @@ import DDC.Core.Exp
 import DDC.Core.Collect
 import DDC.Core.Transform.LiftW
 import DDC.Type.Compounds
-import DDC.Type.Transform.SubstituteT
+import DDC.Type.Rewrite
 import Data.Maybe
 import qualified DDC.Type.Env   as Env
 import qualified Data.Set       as Set
-import Data.Set                 (Set)
 
 
 -- | Wrapper for `substituteWithW` that determines the set of free names in the
 --   type being substituted, and starts with an empty binder stack.
-substituteWX :: (SubstituteWX c, Ord n) => Bind n -> Witness n -> c n -> c n
-substituteWX b w xx
- | Just u       <- takeSubstBoundOfBind b
- = let  -- Determine the free names in the type we're subsituting.
-        -- We'll need to rename binders with the same names as these
-        fnsX    = Set.fromList
-                $ mapMaybe takeNameOfBound 
-                $ Set.toList 
-                $ freeX Env.empty w
+substituteWX 
+        :: (Ord n, SubstituteWX c) 
+        => Bind n -> Witness n -> c n -> c n
 
-        fnsT    = Set.fromList
+substituteWX b wArg xx
+ | Just u       <- takeSubstBoundOfBind b
+ = substituteWithWX wArg
+        ( Sub 
+        { subBound      = u
+
+          -- Rewrite level-1 binders that have the same name as any
+          -- of the free variables in the expression to substitute.
+        , subConflict1  
+                = Set.fromList
+                $  (mapMaybe takeNameOfBound $ Set.toList $ freeT Env.empty wArg) 
+
+          -- Rewrite level-0 binders that have the same name as any
+          -- of the free variables in the expression to substitute.
+        , subConflict0
+                = Set.fromList
                 $ mapMaybe takeNameOfBound 
                 $ Set.toList 
-                $ freeT Env.empty w
-       
-        stack    = BindStack [] [] 0 0
- 
-  in    substituteWithWX u w fnsT fnsX stack stack xx
+                $ freeX Env.empty wArg
+                
+        , subStack1     = BindStack [] [] 0 0
+        , subStack0     = BindStack [] [] 0 0
+        , subShadow0    = False })
+        xx
 
  | otherwise    = xx
  
 
 -- | Wrapper for `substituteW` to substitute multiple things.
-substituteWXs :: (SubstituteWX c, Ord n) => [(Bind n, Witness n)] -> c n -> c n
+substituteWXs 
+        :: (Ord n, SubstituteWX c) 
+        => [(Bind n, Witness n)] -> c n -> c n
 substituteWXs bts x
         = foldr (uncurry substituteWX) x bts
 
@@ -54,117 +65,115 @@ class SubstituteWX (c :: * -> *) where
  --   avoiding the capture.
  substituteWithWX
         :: forall n. Ord n
-        => Bound n      -- ^ Bound variable that we're subsituting into.
-        -> Witness n    -- ^ Witness to substitute.
-        -> Set n        -- ^ Names of free spec names in the exp to substitute.
-        -> Set n        -- ^ Names of free valwit names in the exp to substitute.
-        -> BindStack n  -- ^ Bind stack for Spec names (level-1).
-        -> BindStack n  -- ^ Bind stack for Value and Witness names (level-0).
-        -> c n -> c n
-
-
--- Instances ------------------------------------------------------------------
-instance SubstituteWX Witness where
- substituteWithWX u w fnsT fnsX stackT stackX ww
-  = let down    = substituteWithWX u w fnsT fnsX stackT stackX
-    in case ww of
-        WVar u'
-         -> case substBound stackX u u' of
-                Left u'' -> WVar u''
-                Right n  -> liftW n w
-
-        WCon{}                  -> ww
-        WApp  w1 w2             -> WApp  (down w1) (down w2)
-        WJoin w1 w2             -> WJoin (down w1) (down w2)
-        WType{}                 -> ww
+        => Witness n -> Sub n -> c n -> c n
 
 
 instance SubstituteWX (Exp a) where 
- substituteWithWX u w fnsT fnsX stackT stackX xx
-  = let down    = substituteWithWX u w fnsT fnsX stackT stackX
+ substituteWithWX wArg sub xx
+  = let down    = substituteWithWX wArg
+        into    = rewriteWith
     in case xx of
-        XVar{}          -> xx
+        XVar a u        -> XVar a (into sub u)
         XCon{}          -> xx
-        XApp  a x1 x2   -> XApp  a   (down x1)  (down x2)
+        XApp a x1 x2    -> XApp a (down sub x1) (down sub x2)
 
-        XLAM  a b xBody
-         -> let (stackT', b')   = pushBind fnsX stackX b
-                xBody'          = substituteWithWX u w fnsT fnsX stackT' stackX xBody
-            in  XLAM  a b' xBody'
+        XLAM a b x
+         -> let (sub1, b')      = bind1 sub b
+                x'              = down  sub1 x
+            in  XLAM a b' x'
 
-        XLam  a b xBody
-         | namedBoundMatchesBind u b -> xx
-         | otherwise
-         -> let (stackX', b')   = pushBind fnsX stackX b
-                xBody'          = substituteWithWX u w fnsT fnsX stackT stackX' xBody
-            in  XLam  a b' xBody'
+        XLam a b x
+         -> let (sub1, b')      = bind0 sub  b
+                x'              = down  sub1 x
+            in  XLam a b' x'
 
-        XLet  a (LLet m b x1) x2
-         | namedBoundMatchesBind u b -> xx
-         | otherwise
-         -> let m'              = down m
-                (stackX', b')   = pushBind fnsX stackX b
-                x1'             = down x1
-                x2'             = substituteWithWX u w fnsT fnsX stackT stackX' x2
+        XLet a (LLet m b x1) x2
+         -> let m'              = down  sub  m
+                x1'             = down  sub  x1
+                (sub1, b')      = bind0 sub  b
+                x2'             = down  sub1 x2
             in  XLet a (LLet m' b' x1') x2'
 
         XLet a (LRec bxs) x2
-         | any (namedBoundMatchesBind u) $ map fst bxs -> xx
-         | otherwise
-         -> let (bs, xs)        = unzip bxs
-                (stackX', bs')  = pushBinds fnsX stackX bs
-                xs'             = map (substituteWithWX u w fnsT fnsX stackT stackX') xs
-                x2'             = substituteWithWX u w fnsT fnsX stackT stackX' x2
+         -> let (bs, xs)        = unzip  bxs
+                (sub1, bs')     = bind0s sub bs
+                xs'             = map (down sub1) xs
+                x2'             = down sub1 x2
             in  XLet a (LRec (zip bs' xs')) x2'
 
         XLet a (LLetRegion b bs) x2
-         | any (namedBoundMatchesBind u) bs -> xx
-         | otherwise
-         -> let (stackT', b')   = pushBind  fnsT stackT b
-                (stackX', bs')  = pushBinds fnsX stackX bs
-                x2'             = substituteWithWX u w fnsT fnsX stackT' stackX' x2
+         -> let (sub1, b')      = bind1  sub  b
+                (sub2, bs')     = bind0s sub1 bs
+                x2'             = down   sub2 x2
             in  XLet a (LLetRegion b' bs') x2'
 
         XLet a (LWithRegion uR) x2
-         -> XLet a (LWithRegion uR) (down x2)
+         -> XLet a (LWithRegion uR) (down sub x2)
 
-        XCase a x alts  -> XCase a  (down x)   (map down alts)
-        XCast a c x     -> XCast a  (down c)   (down x)
-        XType{}         -> xx
-        XWitness w1     -> XWitness (down w1)
+        XCase a x1 alts -> XCase a  (down sub x1) (map (down sub) alts)
+        XCast a cc x1   -> XCast a  (down sub cc) (down sub x1)
+        XType t         -> XType    (into sub t)
+        XWitness w      -> XWitness (down sub w)
+
 
 
 instance SubstituteWX LetMode where
- substituteWithWX u f fnsT fnsX stackT stackX lm
-  = let down = substituteWithWX u f fnsT fnsX stackT stackX
+ substituteWithWX wArg sub lm
+  = let down = substituteWithWX wArg
     in case lm of
         LetStrict        -> lm
         LetLazy Nothing  -> LetLazy Nothing
-        LetLazy (Just w) -> LetLazy (Just (down w))
+        LetLazy (Just w) -> LetLazy (Just (down sub w))
 
 
 instance SubstituteWX (Alt a) where
- substituteWithWX u w fnsT fnsX stackT stackX alt
-  = let down    = substituteWithWX u w fnsT fnsX stackT stackX
-    in case alt of
+ substituteWithWX wArg sub aa
+  = let down = substituteWithWX wArg
+    in case aa of
         AAlt PDefault xBody
-         -> AAlt PDefault $ down xBody
+         -> AAlt PDefault $ down sub xBody
         
-        AAlt (PData uCon bs) xBody
-         | any (namedBoundMatchesBind u) bs -> alt
-         | otherwise
-         -> let (stackX', bs')  = pushBinds fnsX stackX bs
-                xBody'          = substituteWithWX u w fnsT fnsX stackT stackX' xBody
-            in  AAlt (PData uCon bs') xBody'
+        AAlt (PData uCon bs) x
+         -> let (sub1, bs')     = bind0s sub bs
+                x'              = down   sub1 x
+            in  AAlt (PData uCon bs') x'
 
 
 instance SubstituteWX Cast where
- substituteWithWX u w fnsT fnsX stackT stackX cc
-  = let down    = substituteWithWX u w fnsT fnsX stackT stackX 
+ substituteWithWX wArg sub cc
+  = let down    = substituteWithWX wArg
+        into    = rewriteWith
     in case cc of
-        CastWeakenEffect eff    -> CastWeakenEffect  eff
-        CastWeakenClosure clo   -> CastWeakenClosure clo
-        CastPurify w'           -> CastPurify (down w')
-        CastForget w'           -> CastForget (down w')
+        CastWeakenEffect eff    -> CastWeakenEffect  (into sub eff)
+        CastWeakenClosure clo   -> CastWeakenClosure (into sub clo)
+        CastPurify w            -> CastPurify        (down sub w)
+        CastForget w            -> CastForget        (down sub w)
 
+
+instance SubstituteWX Witness where
+ substituteWithWX wArg sub ww
+  = let down    = substituteWithWX wArg
+        into    = rewriteWith
+    in case ww of
+        WVar u
+         -> case substW wArg sub u of
+                Left u'  -> WVar u'
+                Right w  -> w
+
+        WCon{}                  -> ww
+        WApp  w1 w2             -> WApp  (down sub w1) (down sub w2)
+        WJoin w1 w2             -> WJoin (down sub w1) (down sub w2)
+        WType t                 -> WType (into sub t)
+
+
+-- | Rewrite or substitute into an expression variable.
+substW  :: Ord n => Witness n -> Sub n -> Bound n 
+        -> Either (Bound n) (Witness n)
+
+substW wArg sub u
+  = case substBound (subStack0 sub) (subBound sub) u of
+        Left  u'                -> Left (rewriteWith sub u')
+        Right n  
+         | not $ subShadow0 sub -> Right (liftW n wArg)
+         | otherwise            -> Left (rewriteWith sub u)
 
