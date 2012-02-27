6af10c797669e38091806a8718fc2485e86d0e0e
Ben Lippmeier <benl@ouroborus.net>
2012-02-27 09:25:59 +0000

    ddc-core: add core module syntax tree

---
+++ packages/ddc-core/DDC/Core/Module.hs
@@ -0,0 +1,72 @@
+
+module DDC.Core.Module
+        ( ModuleName    (..)
+        , QualName      (..)
+        , Module        (..)
+        , Extern        (..))
+where
+import DDC.Core.Exp
+import Data.Map         (Map)
+
+
+-- | A hierarchical module name.
+data ModuleName
+        = ModuleName [String]
+        deriving (Show, Eq, Ord)
+
+
+-- | A fully qualified name, 
+--   including the name of the module it is from.
+data QualName n
+        = QualName ModuleName n
+
+
+-- | A module can be mutually recursive with other modules.
+data Module a n
+
+        -- | A module containing core bindings.
+        = ModuleCore
+        { -- | Name of this module.
+          moduleName            :: ModuleName
+
+          -- Exports ------------------
+          -- | Kinds of exported type names.
+        , moduleExportKinds     :: Map n (Kind n)
+
+          -- | Types of exported value names.
+        , moduleExportTypes     :: Map n (Type n)
+
+          -- Imports ------------------
+          -- | Map of external value names used in this module,
+          --   to their qualified name and types.
+        , moduleImportKinds     :: Map n (QualName n, Kind n)
+
+          -- | Map of external value names used in this module,
+          --   to their qualified name and types.
+        , moduleImportTypes     :: Map n (QualName n, Type n)
+
+          -- Local --------------------
+          -- | Non-recursive bindings defined in this module, 
+          --   which are initialised on startup.
+        , moduleLets            :: [Lets a n] 
+        }
+
+
+        -- | A module that imports foreign functions.
+        | ModuleForeign
+        { -- | Name of this module.
+          moduleName            :: ModuleName
+
+          -- | External values defined by the module.
+        , moduleExternTypes     :: Map n (Extern n) }
+
+
+-- | Definition of some external thing.
+data Extern n
+        -- | Import a function from Sea land.
+        = ExternSeaFun
+        { -- | Name of the external function.
+          externSeaName         :: String
+
+          -- | Type of the function.
+        , externType            :: Type n }
--- packages/ddc-sea-llvm/DDC/Sea/Type.hs
+++ packages/ddc-sea-llvm/DDC/Sea/Type.hs
@@ -23,13 +23,20 @@ data Type n
 -- | Type constructors,
 --   parameterised by a name type.
 data TyCon n
-        -- | Some anonymous boxed object.
-        --   This might be algabraic data, or a non-algebraic object like
-        --   an array of unboxed ints, a thunk or suspension.
-        = TyConObj
+        -- | A natural number, used as a arguments to functions from the
+        --   runtime tsystem. Not for literal values in the source program.
+        = TyConNat
+
+        -- | The tag of a data object.
+        | TyConTag
 
         -- | An unboxed, primitive type.
         | TyConPrim TyConPrim
+
+        -- | Some anonymous boxed object.
+        --   This might be algabraic data, or a non-algebraic object like
+        --   an array of unboxed ints, a thunk or suspension.
+        | TyConObj
 
         -- | Some abstract type that we don't know anything about.
         --   These are types like FILE which are defined by the system libraries,
