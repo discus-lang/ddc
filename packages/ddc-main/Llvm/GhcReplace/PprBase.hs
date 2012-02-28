ea4fde49019bde5e871d68a1b6ec3b3dcfac3671
Ben Lippmeier <benl@ouroborus.net>
2012-02-28 01:55:18 +0000

    ddc-main: unbreak doubleToBytes in existing LLVM backend

--- packages/ddc-main/Llvm/GhcReplace/PprBase.hs
+++ packages/ddc-main/Llvm/GhcReplace/PprBase.hs
@@ -2,5 +2,27 @@
 -- GHC compiler. The following two functions are a direct steal from GHC.
 
 module Llvm.GhcReplace.PprBase where
+import Data.Array.ST
+import Control.Monad.ST
+import Data.Word
+import qualified Data.Array.Unsafe      as Unsafe
 
+doubleToBytes :: Double -> [Int]
+doubleToBytes d
+   = runST (do
+        arr <- newArray_ ((0::Int),7)
+        writeArray arr 0 d
+        arr <- castDoubleToWord8Array arr
+        i0 <- readArray arr 0
+        i1 <- readArray arr 1
+        i2 <- readArray arr 2
+        i3 <- readArray arr 3
+        i4 <- readArray arr 4
+        i5 <- readArray arr 5
+        i6 <- readArray arr 6
+        i7 <- readArray arr 7
+        return (map fromIntegral [i0,i1,i2,i3,i4,i5,i6,i7])
+     )
 
+castDoubleToWord8Array :: STUArray s Int Double -> ST s (STUArray s Int Word8)
+castDoubleToWord8Array = Unsafe.castSTUArray
--- packages/ddc-main/Llvm/Types.hs
+++ packages/ddc-main/Llvm/Types.hs
@@ -781,3 +781,4 @@ fixEndian = id
 fixEndian = reverse
 #endif
 
+
