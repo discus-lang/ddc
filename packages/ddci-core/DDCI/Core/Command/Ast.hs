4dd9ccf0bea16b062349ded28030946a6e7f029f
Ben Lippmeier <benl@ouroborus.net>
2012-03-03 10:17:39 +0000

    ddci-core: make ast command take arb lang profile

--- packages/ddci-core/DDCI/Core/Command/Ast.hs
+++ packages/ddci-core/DDCI/Core/Command/Ast.hs
@@ -2,7 +2,6 @@
 module DDCI.Core.Command.Ast
         (cmdAst)
 where
-import DDC.Core.Eval.Profile
 import DDCI.Core.Command.Check
 import DDCI.Core.State
 import qualified Language.Haskell.Exts.Parser as H
@@ -12,7 +11,8 @@ import qualified Language.Haskell.Exts.Pretty as H
 -- | Parse, check, and pretty print an expression's internal representation
 cmdAst :: State -> Int -> String -> IO ()
 cmdAst state lineStart str
- = cmdParseCheckExp state evalProfile lineStart str >>= goShow
+ | StateProfile profile <- stateProfile state
+ = cmdParseCheckExp state profile lineStart str >>= goShow
  where
         -- Expression had a parse or type error.
         goShow Nothing
