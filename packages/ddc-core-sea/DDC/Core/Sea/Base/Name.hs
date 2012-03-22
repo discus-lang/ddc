10c22619bb71a4661ede4349fa1d1d7700d57d8a
Ben Lippmeier <benl@ouroborus.net>
2012-03-22 05:21:14 +0000

    ddc-core-sea: Split primops not visible in SeaOutput fragment to new dir

--- packages/ddc-core-sea/DDC/Core/Sea/Base/Name.hs
+++ packages/ddc-core-sea/DDC/Core/Sea/Base/Name.hs
@@ -11,43 +11,6 @@ where
 import DDC.Base.Pretty
 
 
--- Names of things used in the Sea core language profile.
-data Name
-        -- | User defined variables.
-        = NameVar       String
-
-        -- | A user defined constructor.
-        | NameCon       String
-
-        -- | Baked in data type constructors.
-        | NameDataTyCon DataTyCon
-
-        -- | A primitive type constructor.
-        | NamePrimTyCon PrimTyCon
-
-        -- | A primitive data constructor.
-        | NamePrimDaCon PrimDaCon
-
-        -- | A primitive operator.
-        | NamePrimOp    PrimOp
-
-        -- | A integer literal.
-        | NameInt       Integer
-        deriving (Eq, Ord, Show)
-
-
-instance Pretty Name where
- ppr nn
-  = case nn of
-        NameVar  v              -> text v
-        NameCon  c              -> text c
-        NameDataTyCon dc        -> ppr dc
-        NamePrimTyCon tc        -> ppr tc
-        NamePrimDaCon dc        -> ppr dc
-        NamePrimOp op           -> ppr op
-        NameInt i               -> text (show i)
-
-
 -- PrimTyCon -----------------------------------------------------------------
 -- | Primitive type constructors.
 data PrimTyCon
@@ -98,38 +61,6 @@ instance Pretty PrimTyCon where
         PrimTyConFloat  bits    -> text "Float" <> int bits <> text "#"
         PrimTyConString         -> text "String#"
 
-
--- DataTyCon ------------------------------------------------------------------
-data DataTyCon
-        = DataTyConUnit         -- ^ Unit type constructor.
-        | DataTyConPair         -- ^ @Pair@ data constructor.
-        | DataTyConList         -- ^ @List@ type constructor.
-        deriving (Eq, Ord, Show)
-
-
-instance Pretty DataTyCon where
- ppr dc
-  = case dc of
-        DataTyConUnit           -> text "Unit"
-        DataTyConPair           -> text "Pair"
-        DataTyConList           -> text "List"
-
-
--- PrimDaCon ------------------------------------------------------------------
-data PrimDaCon
-        = PrimDaConUnit         -- ^ Unit data constructor (@()@).
-        | PrimDaConPr           -- ^ @Pr@ data construct (pairs).
-        | PrimDaConNil          -- ^ @Nil@ data constructor.
-        | PrimDaConCons         -- ^ @Cons@ data constructor.
-        deriving (Show, Eq, Ord)
-
-instance Pretty PrimDaCon where
- ppr dc
-  = case dc of
-        PrimDaConUnit           -> text "Unit"
-        PrimDaConPr             -> text "Pr"
-        PrimDaConNil            -> text "Nil"
-        PrimDaConCons           -> text "Cons"
 
 
 -- PrimOp ---------------------------------------------------------------------
