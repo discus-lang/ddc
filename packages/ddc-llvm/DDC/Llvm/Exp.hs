db07c7bf9f980d8a45422bfe98f39860a9681111
Ben Lippmeier <benl@ouroborus.net>
2012-03-28 02:54:41 +0000

    ddc-llvm: merge Exp and Lit modules

--- packages/ddc-llvm/DDC/Llvm/Exp.hs
+++ packages/ddc-llvm/DDC/Llvm/Exp.hs
@@ -1,58 +1,28 @@
 
 module DDC.Llvm.Exp
         ( module DDC.Llvm.Type
-        , module DDC.Llvm.Lit
 
-          -- * Names
-        , Name  (..)
+          -- * Expressions
+        , Exp   (..)
+        , pprPlainX
+        , typeOfExp
 
           -- * Variables
         , Var   (..)
         , nameOfVar
         , typeOfVar
 
-          -- * Expressions
-        , Exp   (..)
-        , pprPlainX
-        , typeOfExp)
+          -- * Names
+        , Name  (..)
+
+          -- * Literals
+        , Lit   (..)
+        , pprPlainL
+        , typeOfLit)
 where
 import DDC.Llvm.Attr
 import DDC.Llvm.Type
-import DDC.Llvm.Lit
 import DDC.Base.Pretty
-
-
--- Name -----------------------------------------------------------------------
--- | Names of variables.
-data Name
-        = NameGlobal String
-        | NameLocal  String
-        deriving (Eq, Show)
-
-instance Pretty Name where
- ppr (NameGlobal str)   = text "@" <> text str
- ppr (NameLocal  str)   = text "%" <> text str
-
-
--- Var ------------------------------------------------------------------------
--- | A variable that can be assigned to.
-data Var
-        = Var   Name    Type
-        deriving (Eq, Show)
-
-
--- | Yield the name of a var.
-nameOfVar :: Var -> Name
-nameOfVar (Var n _)     = n
-
-
--- | Yield the type of a var.
-typeOfVar :: Var -> Type
-typeOfVar (Var _ t)     = t
-
-
-instance Pretty Var where
- ppr (Var n t)  = ppr t <+> ppr n
 
 
 -- Exp ------------------------------------------------------------------------
@@ -86,3 +56,85 @@ typeOfExp xx
  = case xx of
         XVar var        -> typeOfVar var
         XLit lit        -> typeOfLit lit
+
+
+-- Var ------------------------------------------------------------------------
+-- | A variable that can be assigned to.
+data Var
+        = Var   Name    Type
+        deriving (Eq, Show)
+
+
+-- | Yield the name of a var.
+nameOfVar :: Var -> Name
+nameOfVar (Var n _)     = n
+
+
+-- | Yield the type of a var.
+typeOfVar :: Var -> Type
+typeOfVar (Var _ t)     = t
+
+
+instance Pretty Var where
+ ppr (Var n t)  = ppr t <+> ppr n
+
+
+-- Name -----------------------------------------------------------------------
+-- | Names of variables.
+data Name
+        = NameGlobal String
+        | NameLocal  String
+        deriving (Eq, Show)
+
+instance Pretty Name where
+ ppr (NameGlobal str)   = text "@" <> text str
+ ppr (NameLocal  str)   = text "%" <> text str
+
+
+-- Lit ------------------------------------------------------------------------
+-- | Literal data.
+data Lit
+        -- | An integer literal
+        = LitInt        Type    Integer
+
+        -- | A floating-point literal.
+        | LitFloat      Type    Double
+
+        -- | A null pointer literal.
+        --   Only applicable to pointer types
+        | LitNull       Type
+
+        -- | A completely undefined value.
+        | LitUndef      Type
+        deriving (Eq, Show)
+
+
+instance Pretty Lit where
+ ppr ll
+  = case ll of
+        LitInt   t i    -> ppr t <+> integer i
+        LitFloat{}      -> error "ppr[Lit]: floats aren't handled yet"
+        LitNull  _      -> text "null"
+        LitUndef _      -> text "undef"
+
+
+-- | Pretty print a literal without its type.
+pprPlainL :: Lit -> Doc
+pprPlainL ll
+ = case ll of
+        LitInt _ i      -> integer i
+        LitFloat{}      -> error "ppr[Lit]: floats aren't handled yet"
+        LitNull  _      -> text "null"
+        LitUndef _      -> text "undef"
+
+
+-- | Yield the `Type` of a `Lit`.
+typeOfLit :: Lit -> Type
+typeOfLit ll
+ = case ll of
+        LitInt    t _   -> t
+        LitFloat  t _   -> t
+        LitNull   t     -> t
+        LitUndef  t     -> t
+
+
