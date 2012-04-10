b75975979d40df0b70129e2f0359e0763f108546
Ben Lippmeier <benl@ouroborus.net>
2012-04-10 11:25:34 +0000

    ddci-core: automatically anormalizse program during conversion with -sea and -llvm

--- packages/ddci-core/DDCI/Core/Command/Llvm.hs
+++ packages/ddci-core/DDCI/Core/Command/Llvm.hs
@@ -6,18 +6,23 @@ import DDCI.Core.Language
 import DDCI.Core.Mode
 import DDCI.Core.Pipeline.Module
 import DDCI.Core.State
-import DDC.Base.Pretty
+import DDC.Core.Simplifier.Recipie      as Simpl
+import qualified DDC.Base.Pretty        as P
+import Data.Monoid
 
 
 -- | Parse, check and convert a Sea module to LLVM.
+---
+--   The Core -> C conversion only accepts A-normalised programs,
+--   so we normalize it along the way.
 cmdLlvmOut :: State -> Source -> String -> IO ()
 cmdLlvmOut state source str
- = do   errs    <- pipeText source str
-                $  PipeTextLoadCore  fragmentSea
-                [  PipeCoreSimplify  fragmentSea (stateSimplifier state)
-                [  PipeCoreCheck     fragmentSea
-                [  PipeCoreAsSea
-                [  PipeSeaToLlvm 
-                [  PipeLlvmPrint SinkStdout ]]]]]
-
-        mapM_ (putStrLn . renderIndent . ppr) errs
+ = (pipeText source str
+        $  PipeTextLoadCore     fragmentSea
+        [  PipeCoreSimplify     fragmentSea 
+                                (stateSimplifier state <> Simpl.anormalize)
+        [  PipeCoreCheck        fragmentSea
+        [  PipeCoreAsSea
+        [  PipeSeaToLlvm 
+        [  PipeLlvmPrint SinkStdout ]]]]])
+ >>= mapM_ (putStrLn . P.renderIndent . P.ppr)
--- packages/ddci-core/DDCI/Core/Command/Sea.hs
+++ packages/ddci-core/DDCI/Core/Command/Sea.hs
@@ -6,20 +6,25 @@ import DDCI.Core.Mode
 import DDCI.Core.Pipeline.Module
 import DDCI.Core.State
 import DDCI.Core.Language
-import DDC.Base.Pretty
+import DDC.Core.Simplifier.Recipie      as Simpl
 import qualified Data.Set               as Set
+import qualified DDC.Base.Pretty        as P
+import Data.Monoid
 
 
 -- | Parse, check, and fully evaluate an expression.
+---
+--   The Core -> C conversion only accepts A-normalised programs,
+--   so we normalize it along the way.
 cmdSeaOut :: State -> Source -> String -> IO ()
 cmdSeaOut state source str
- = do   errs    <- pipeText source str
-                $  PipeTextLoadCore  fragmentSea
-                [  PipeCoreSimplify  fragmentSea (stateSimplifier state)
-                [  PipeCoreCheck     fragmentSea 
-                [  PipeCoreAsSea
-                [  PipeSeaPrint 
-                        (Set.member SeaPrelude (stateModes state))
-                        SinkStdout ]]]]
-
-        mapM_ (putStrLn . renderIndent . ppr) errs
+ = (pipeText source str
+        $  PipeTextLoadCore     fragmentSea
+        [  PipeCoreSimplify     fragmentSea     
+                                (stateSimplifier state <> Simpl.anormalize)
+        [  PipeCoreCheck        fragmentSea 
+        [  PipeCoreAsSea
+        [  PipeSeaPrint 
+                (Set.member SeaPrelude (stateModes state))
+                SinkStdout ]]]])
+ >>= mapM_ (putStrLn . P.renderIndent . P.ppr)
