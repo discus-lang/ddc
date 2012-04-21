42dd7180fce1e0ca58ebaacfb3108fc9904fef62
Ben Lippmeier <benl@ouroborus.net>
2012-04-21 11:14:03 +0000

    ddci-core: better error message if a system command fails

--- packages/ddci-core/DDCI/Core/Build/Builder.hs
+++ packages/ddci-core/DDCI/Core/Build/Builder.hs
@@ -9,8 +9,9 @@ module DDCI.Core.Build.Builder
         , determineDefaultBuilder)
 where
 import DDCI.Core.Build.Platform
-import System.Cmd
 import System.FilePath
+import System.Exit
+import System.Cmd
 import qualified DDC.Core.Llvm.Platform         as Llvm
 
 -- | Configuration information for a builder that is not platform specific.
@@ -38,14 +39,14 @@ data Builder
 
           -- | Invoke the LLVM compiler
           --   to compile a .ll file into a .s file.
-        , buildLlc      :: FilePath -> FilePath -> IO Bool 
+        , buildLlc      :: FilePath -> FilePath -> IO ()
 
           -- | Invoke the system assembler
           --   to assemble a .s file into a .o file.
-        , buildAs       :: FilePath -> FilePath -> IO Bool 
+        , buildAs       :: FilePath -> FilePath -> IO ()
 
           -- | Link an executable.
-        , buildLdExe    :: FilePath -> FilePath -> IO Bool }
+        , buildLdExe    :: FilePath -> FilePath -> IO () }
 
 instance Show Builder where
  show builder
@@ -107,19 +108,22 @@ builder_X8632_Darwin config
 
         , buildLlc    
                 = \llFile sFile
-                -> doCmd $ "llc -O3 -march=x86 " 
+                -> doCmd "LLVM compiler"
+                $ "llc -O3 -march=x86 " 
                 ++ llFile 
                 ++ " -o " ++ sFile
 
         , buildAs
                 = \sFile oFile
-                -> doCmd $  "as -arch i386"  
+                -> doCmd "assembler"
+                $  "as -arch i386"  
                 ++ " -o " ++ oFile
                 ++ " "    ++ sFile  
 
         , buildLdExe
                 = \oFile binFile
-                -> doCmd $  "gcc -m32" 
+                -> doCmd "linker"
+                $  "gcc -m32" 
                 ++ " -o " ++ binFile
                 ++ " "    ++ oFile
                 ++ " "    ++ (builderConfigRuntime config </> "libddc-runtime.dylib")
@@ -136,19 +140,22 @@ builder_X8664_Darwin config
 
         , buildLlc    
                 = \llFile sFile
-                -> doCmd $ "llc -O3 -march=x86-64 " 
+                -> doCmd "LLVM compiler"
+                $ "llc -O3 -march=x86-64 " 
                 ++ llFile 
                 ++ " -o " ++ sFile
 
         , buildAs
                 = \sFile oFile
-                -> doCmd $  "as -arch x86_64"  
+                -> doCmd "assembler"
+                $  "as -arch x86_64"  
                 ++ " -o " ++ oFile
                 ++ " "    ++ sFile  
 
         , buildLdExe  
                 = \oFile binFile
-                -> doCmd $  "gcc -m64" 
+                -> doCmd "linker"
+                $  "gcc -m64" 
                 ++ " -o " ++ binFile
                 ++ " "    ++ oFile
                 ++ " "    ++ (builderConfigRuntime config </> "libddc-runtime.dylib")
@@ -165,19 +172,22 @@ builder_X8632_Linux config
 
         , buildLlc    
                 = \llFile sFile
-                -> doCmd $ "llc -O3 -march=x86 -relocation-model=pic " 
+                -> doCmd "LLVM compiler"
+                $ "llc -O3 -march=x86 -relocation-model=pic " 
                 ++ llFile 
                 ++ " -o " ++ sFile
 
         , buildAs
                 = \sFile oFile
-                -> doCmd $  "as --32"  
+                -> doCmd "assembler"
+                $  "as --32"  
                 ++ " -o " ++ oFile
                 ++ " "    ++ sFile  
 
         , buildLdExe  
                 = \oFile binFile
-                -> doCmd $  "gcc -m32" 
+                -> doCmd "linker"
+                $  "gcc -m32" 
                 ++ " -o " ++ binFile
                 ++ " "    ++ oFile
                 ++ " "    ++ (builderConfigRuntime config </> "libddc-runtime.so")
@@ -194,19 +204,22 @@ builder_X8664_Linux config
 
         , buildLlc    
                 = \llFile sFile
-                -> doCmd $ "llc -O3 -march=x86-64 -relocation-model=pic " 
+                -> doCmd "LLVM compiler" 
+                $ "llc -O3 -march=x86-64 -relocation-model=pic " 
                 ++ llFile 
                 ++ " -o " ++ sFile
 
         , buildAs
                 = \sFile oFile
-                -> doCmd $  "as --64"  
+                -> doCmd "assembler" 
+                $  "as --64"  
                 ++ " -o " ++ oFile
                 ++ " "    ++ sFile  
 
         , buildLdExe  
                 = \oFile binFile
-                -> doCmd $  "gcc -m64" 
+                -> doCmd "linker" 
+                $  "gcc -m64" 
                 ++ " -o " ++ binFile
                 ++ " "    ++ oFile
                 ++ " "    ++ (builderConfigRuntime config </> "libddc-runtime.so")
@@ -245,7 +258,15 @@ builder_X8632_Cygwin config
 
 
 -- Utils ----------------------------------------------------------------------
-doCmd :: String -> IO Bool
-doCmd cmd
- = do   _ <- system cmd
-        return True
+-- | Run a system command, and if it fails quit the program.
+doCmd :: String -> String -> IO ()
+doCmd thing cmd
+ = do   code <- system cmd
+        case code of
+         ExitSuccess    -> return ()
+         ExitFailure _  
+          -> error
+                $ unlines
+                [ "System command failed when invoking external " ++ thing ++ "."
+                , " Command was: " ++ cmd ]
+
