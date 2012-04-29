694b09712d09f05321bb54d2f8b1d85924fae8c4
Ben Lippmeier <benl@ouroborus.net>
2012-04-29 04:12:56 +0000

    Fix race with T191-LocalMake

--- test/ddc-main/10-Driver/T191-LocalMake/Main.sh
+++ test/ddc-main/10-Driver/T191-LocalMake/Main.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 if test $# -eq 1 ; then
-	loc=$1
+	srcDir=$1
+        buildDir=$srcDir
 
 elif test $# -eq 2 ; then
 	loc=$1
+        buildDir=$2
 
 elif test -f Main.ds ; then
 	loc=`pwd`
+        buildDir=`pwd`
 else
 	echo "Need a directory parameter."
 	exit 1
 	fi
 
-(cd $loc && make clean check)
+(cd $loc && make BUILDDIR=$2 clean check)
 
