882ba18bbea8a59324f5cf1803938226318b88fc
Ben Lippmeier <benl@ouroborus.net>
2012-06-14 14:07:30 +0000

    ddc-core: refactor arity analysis

--- packages/ddc-core/DDC/Core/Analysis/Arity.hs
+++ packages/ddc-core/DDC/Core/Analysis/Arity.hs
@@ -1,4 +1,6 @@
 
+-- | Slurp out arities of functions, 
+--   and infer arities for primitives based on their types.
 module DDC.Core.Analysis.Arity
         ( Arities       (..)
         , emptyArities
@@ -19,36 +21,39 @@ import DDC.Type.Compounds
 import qualified Data.Map       as Map
 
 
--- Arities --------------------------------------------------------------------
--- Recording arities of known values
--- So we can try to create apps to fully apply 
-
--- | Arities of known bound variables.
---   We need to track everything even if it's not a function to keep indices
---   correct. Just use zero for unknown/irrelevant
+-- | Arities of named and anonymous functions.
 type Arities n = (Map.Map n Int, [Int])
 
 
--- | Empty arities context
+-- | Empty arities context.
 emptyArities :: Ord n => Arities n
 emptyArities = (Map.empty, [])
 
 
--- | Extend map with multiple bindings and their arities
+-- | Extend map with multiple bindings and their arities.
 extendsArities :: Ord n => Arities n -> [(Bind n, Int)] -> Arities n
 extendsArities arity exts = foldl go arity exts
- where  go (named,anon) (BNone _t,   _) = (named,anon)
-        go (named,anon) (BAnon _t,   a) = (named, a:anon)
-        go (named,anon) (BName n _t, a) = (Map.insert n a named, anon)
+ where  go (named, anon) (BNone _t,   _) = (named,anon)
+        go (named, anon) (BAnon _t,   a) = (named, a:anon)
+        go (named, anon) (BName n _t, a) = (Map.insert n a named, anon)
 
 
--- | Look up a binder's arity
+-- | Look up a binder's arity.
 getArity :: Ord n => Arities n -> Bound n -> Maybe Int
-getArity (_named, anon) (UIx ix _)   = Just (anon !! ix)
-getArity (named, _anon) (UName n _)  = Map.lookup n named
-getArity _              (UHole _)    = Just 0
--- Get a primitive's arity from its type
-getArity (_named,_anon) (UPrim _ t)  = Just $ arityFromType t
+getArity (named, anon) u
+ = case u of
+        -- Get arities of anonymous things from the stack.
+        UIx ix  _       -> Just (anon !! ix)
+
+        -- Lookup arities of named things from the stack.
+        UName n _       -> Map.lookup n named
+
+        -- A hole has no expression, so no arity.
+        UHole   _       -> Nothing
+
+        -- Get a primitive's arity from its type.
+        -- The arities of primitives always match their types, so this is ok.
+        UPrim _ t       -> Just $ arityFromType t
 
 
 -- | Get the arities of a `Lets`
@@ -71,45 +76,45 @@ aritiesOfModule mm
         aritiesImports  = [ (BName n t, arityFromType t)        
                           | (n, (_, t)) <- Map.toList $ moduleImportTypes mm ]
 
-    in emptyArities
-       `extendsArities` aritiesImports
-       `extendsArities` aritiesLets
+    in  emptyArities
+        `extendsArities` aritiesImports
+        `extendsArities` aritiesLets
 
 
 -- | Get the arity of an expression. 
 --   Count lambdas, use type for primitives.
 arityOfExp :: Ord n => Exp a n -> Int
--- Counting all binders, because they all correspond to XApps.
-arityOfExp (XLam _ _ e)
-        = 1 + arityOfExp e
+arityOfExp xx
+ = case xx of
+        -- Counting all binders, because they all correspond to XApps.
+        XLam _ _ e              -> 1 + arityOfExp e
+        XLAM _ _ e              -> 1 + arityOfExp e
 
-arityOfExp (XLAM _ _ e)
-        = 1 + arityOfExp e
+        -- Find primitive's constructor's arities from type,
+        -- we might need to do this for user defined constructors too.
+        XCon _ (UPrim _ t)      -> arityFromType t
 
--- Find primitive's constructor's arities from type,
--- we might need to do this for user defined constructors too.
-arityOfExp (XCon _ (UPrim _ t))
-        = arityFromType t
-
--- Anything else we'll need to apply one at a time
-arityOfExp _
-        = 0
+        -- Anything else we'll need to apply one at a time
+        _                       -> 0
 
 
 -- | Determine the arity of an expression by looking at its type.
 --   Count all the function arrows, and foralls.
 arityFromType :: Ord n => Type n -> Int
-arityFromType (TForall _ t)
- =  1 + arityFromType t
+arityFromType tt
+ = case tt of
+        TForall _ t             
+          -> 1 + arityFromType t
 
-arityFromType t
- = let (args, _) = takeTFunArgResult t
-   in  length args
+        t -> let (args, _) = takeTFunArgResult t
+             in  length args
 
 
 -- | Retrieve binders from case pattern, so we can extend the arity context.
 --   We don't know anything about their values, so record as 0.
 aritiesOfPat :: Ord n => Pat n -> [(Bind n, Int)]
-aritiesOfPat PDefault = []
-aritiesOfPat (PData _b bs) = zip bs (repeat 0)
+aritiesOfPat p
+ = case p of
+        PDefault        -> []
+        (PData _b bs)   -> zip bs (repeat 0)
 
