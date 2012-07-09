522b382f8dc5de2fcdff712deb7389b734d3ec01
Ben Lippmeier <benl@ouroborus.net>
2012-07-09 02:46:50 +0000

    ddci-core: refactor make command

--- packages/ddci-core/DDCI/Core/Command/Make.hs
+++ packages/ddci-core/DDCI/Core/Command/Make.hs
@@ -15,54 +15,44 @@ import Control.Monad
 import Data.Maybe
 import qualified DDC.Core.Pretty        as P
 
-data Frag = FragDCL | FragDCE
 
 cmdMake :: State -> Source -> String -> IO ()
 cmdMake state source str
- = let  filePath = dropWhile isSpace str
-   in   makeFile state source filePath
+ = do
+        -- Read in the source file.
+        let filePath = dropWhile isSpace str
+        exists  <- doesFileExist filePath
+        when (not exists)
+         $      error $ "No such file " ++ show filePath
 
-makeFile state source filePath
-        | isSuffixOf ".dcl" filePath
-        = makeFrag FragDCL state source filePath
+        src     <- readFile filePath
 
-        | isSuffixOf ".dce" filePath
-        = makeFrag FragDCE state source filePath
-
-        | otherwise
-        = error $ "Don't know how to make " ++ filePath
-
-makeFrag :: Frag -> State -> Source -> FilePath -> IO ()
-makeFrag frag state source filePath
- = do   
         -- Determine the builder to use.
         mBuilder        <- determineDefaultBuilder defaultBuilderConfig
         let builder     =  fromMaybe (error "Can not determine host platform")
                                      mBuilder
 
-        -- Read in the source file.
-        exists  <- doesFileExist filePath
-        when (not exists)
-         $ error $ "No such file " ++ show filePath
+        -- Decide what to do based on file extension.
+        let make
+                -- Make a Core Lite module.
+                | isSuffixOf ".dcl" filePath
+                = pipeText (nameOfSource source) (lineStartOfSource source) src
+                $ PipeTextLoadCore  fragmentLite
+                [ stageLiteToSalt   state builder
+                [ stageSaltToLLVM   state builder True 
+                [ stageCompileLLVM  state builder filePath ]]]
 
-        src       <- readFile filePath
+                -- Make a Core Salt module.
+                | isSuffixOf ".dce" filePath
+                = pipeText (nameOfSource source) (lineStartOfSource source) src
+                $ PipeTextLoadCore  fragmentSalt 
+                [ stageSaltToLLVM   state builder False 
+                [ stageCompileLLVM  state builder filePath ]]
 
-        -- Run the pipelines to make the source into a binary.
-        errs
-         <- case frag of
-             FragDCL 
-              -> pipeText (nameOfSource source) (lineStartOfSource source) src
-               $ PipeTextLoadCore  fragmentLite
-               [ stageLiteToSalt   state builder
-               [ stageSaltToLLVM   state builder True 
-               [ stageCompileLLVM  state builder filePath ]]]
-
-             FragDCE
-              -> pipeText (nameOfSource source) (lineStartOfSource source) src
-               $ PipeTextLoadCore  fragmentSalt 
-               [ stageSaltToLLVM   state builder False 
-               [ stageCompileLLVM  state builder filePath ]]
+                -- Unrecognised.
+                | otherwise
+                = error $ "Don't know how to make " ++ filePath
 
         -- Print any errors that arose during compilation.
+        errs    <- make
         mapM_ (putStrLn . P.renderIndent . P.ppr) errs
-
