afeb943e7079f1b7f6f40fac372c0a52aa4374c1
Amos Robinson <amos.robinson@gmail.com>
2012-07-12 12:21:56 +0000

    ddc-core-simpl: rewrite: better type matching

--- packages/ddc-core-simpl/DDC/Core/Transform/Rewrite.hs
+++ packages/ddc-core-simpl/DDC/Core/Transform/Rewrite.hs
@@ -18,9 +18,13 @@ import qualified DDC.Type.Transform.SubstituteT as S
 
 import qualified Data.Map as Map
 import qualified Data.Maybe as Maybe
+import qualified Data.Set as Set
+
+import qualified DDC.Type.Equiv as TE
 
 -- I would eventually like to change this to take Map String (RewriteRule..)
 -- so that we can record how many times each rule is fired?
+-- (no, I think just [(String,Rule)] because don't want to require unique names
 rewrite :: (Show a, Show n, Ord n) => [RewriteRule a n] -> Exp a n -> Exp a n
 rewrite rules x0
  =  down x0 emptyWitness
@@ -97,18 +101,22 @@ rewriteX
     -> WitMap n
     -> Maybe (Exp a n)
 rewriteX (RewriteRule binds constrs lhs rhs eff clo) f args ws
- = do	let m	= Map.empty
+ = do	let m	= emptySubstInfo
 	l:ls   <- return $ flatApps lhs
-	m'     <- constrain m bs l f
+	m'     <- constrain m vs l f
 	go m' ls args
  where
     bs = map snd binds
+    vs = Set.fromList $ map nm bs
+
+    nm (BName name _) = name
+    nm _ = error "no anonymous binders in rewrite rules, please"
 
     go m [] rest
      = do   s <- subst m
 	    return $ mkApps s rest
     go m (l:ls) ((r,_):rs)
-     = do   m' <- constrain m bs l r
+     = do   m' <- constrain m vs l r
 	    go m' ls rs
     go _ _ _ = Nothing
 
@@ -139,34 +147,47 @@ rewriteX (RewriteRule binds constrs lhs rhs eff clo) f args ws
     lookupT (b,XType t) = Just (b,t)
     lookupT _ = Nothing
 
-    lookupz m b@(BName n _)
-     = do   x <- Map.lookup n m
-	    return (b,x)
+    lookupz (xs,_) b@(BName n _)
+     | Just x <- Map.lookup n xs
+     = Just (b,x)
+    lookupz (_,tys) b@(BName n _)
+     | Just t <- Map.lookup n tys
+     = Just (b,XType t)
+
     lookupz _ _ = Nothing
     
-constrain :: (Show a, Show n, Ord n) => Map.Map n (Exp a n) -> [Bind n] -> Exp a n -> Exp a n -> Maybe (Map.Map n (Exp a n))
+type SubstInfo a n = (Map.Map n (Exp a n), Map.Map n (Type n))
+emptySubstInfo = (Map.empty, Map.empty)
+
+lookupx n (xs,_) = Map.lookup n xs
+insertx n x (xs,tys) = (Map.insert n x xs, tys)
+
+constrain :: (Show a, Show n, Ord n)
+	  => SubstInfo a n		-- ^ current substitution
+	  -> Set.Set n			-- ^ variables we're interested in
+	  -> Exp a n			-- ^ template
+	  -> Exp a n			-- ^ target
+	  -> Maybe (SubstInfo a n)
 {-
 constrain _m _bs l r
  | trace ("L:"++show l++"\nR:"++show r) False
  = Nothing
  -}
 constrain m bs (XVar _ (UName n _)) r
- | n `elem` (map nm bs)
- = case Map.lookup n m of
-   Nothing -> return $ Map.insert n r m
-   Just x  -> constrain m [] x r
- where
-    nm (BName name _) = name
-    nm _ = error "no anonymous binders in rewrite rules, please"
+ | n `Set.member` bs
+ = case lookupx n m of
+   Nothing -> return $ insertx n r m
+   Just x  -> 
+	-- constrain, but with no matching: basically check whether they're equal
+	constrain m Set.empty x r
 
+{-
 constrain m bs (XType (TVar (UName n _))) r
- | n `elem` (map nm bs)
- = case Map.lookup n m of
-   Nothing -> return $ Map.insert n r m
+ | n `Set.member` bs
+ = case lookupx n m of
+   Nothing -> return $ insertx n r m
    Just x  -> constrain m [] x r
- where
-    nm (BName name _) = name
-    nm _ = error "no anonymous binders in rewrite rules, please"
+-}
 
 constrain m _ (XVar _ l) (XVar _ r)
  | l == r	= Just m
@@ -181,8 +202,9 @@ constrain m bs (XApp _ lf la) (XApp _ rf ra)
 constrain m bs (XCast _ lc le) (XCast _ rc re)
  | lc == rc	= constrain m bs le re
 
-constrain m _ (XType l) (XType r)
- | l == r	= return m
+constrain (xs,tys) bs (XType l) (XType r)
+ = do	tys' <- TE.matchT bs tys l r
+	return (xs,tys')
 
 constrain m _ (XWitness l) (XWitness r)
  | eqWit l r	= return m
