7af466bb203f9b52a373d4c7a29458b1f24c01f4
Amos Robinson <amos.robinson@gmail.com>
2012-08-02 09:41:58 +0000

    ddc-type: fix bug in some equality where (Bottom == Set[foo]) was crashing

--- packages/ddc-type/DDC/Type/Sum.hs
+++ packages/ddc-type/DDC/Type/Sum.hs
@@ -301,14 +301,19 @@ instance Eq n => Eq (TypeSum n) where
         , TypeSumBot{}  <- normalise ts2
         = typeSumKind ts1 == typeSumKind ts2
 
-        -- If the sum has elements, then compare them directly and ignore the
+        -- If both sums have elements, then compare them directly and ignore the
         -- kind. This allows us to use (tBot sComp) as the typeSumKind field
         -- when we want to compute the real kind based on the elements. 
-        | otherwise
+        | TypeSumSet{} <- ts1
+	, TypeSumSet{} <- ts2
         =  typeSumElems ts1      == typeSumElems ts2
         && typeSumBoundNamed ts1 == typeSumBoundNamed ts2
         && typeSumBoundAnon  ts1 == typeSumBoundAnon ts2
         && typeSumSpill      ts1 == typeSumSpill ts2
+
+	-- One is a set and one is bottom, so they are not equal.
+	| otherwise
+	= False
 
   where normalise ts
          | []   <- toList ts    = empty (typeSumKind ts)
