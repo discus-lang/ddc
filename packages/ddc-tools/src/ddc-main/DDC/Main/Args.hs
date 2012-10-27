eb7e3dabb6952fd423f70267e526b2e467d7f329
Ben Lippmeier <benl@ouroborus.net>
2012-10-27 21:08:13 +0000

    ddc-tools: accept no-flag commands as --make, better arg parsing error messages

--- packages/ddc-tools/src/ddc-main/DDC/Main/Args.hs
+++ packages/ddc-tools/src/ddc-main/DDC/Main/Args.hs
@@ -19,26 +19,26 @@ parseArgs args config
         = return 
         $ config { configMode   = ModeHelp }
 
-        | "-make" : file : rest <- args
+        | "-make"      : file : rest <- args
         = parseArgs rest
-        $ config { configMode   = ModeMake file }
+        $ setMode config $ ModeMake file
 
         -- Compilation --------------------------
         | compile : file : rest <- args
         , elem compile ["-c", "-compile"]
         = parseArgs rest
-        $ config { configMode   = ModeCompile file}
+        $ setMode config $ ModeCompile file
 
         | "-library" : path : rest <- args
         = parseArgs rest
         $ config { configLibraryPath = path }
 
-        | flag : file : rest     <- args
+        | flag         : file : rest     <- args
         , elem flag    ["-o", "-output"]
         = parseArgs rest
         $ config { configOutputFile = Just file }
 
-        | "-fvia-c" : rest      <- args
+        | "-fvia-c"    : rest      <- args
         = parseArgs rest
         $ config { configViaBackend = ViaC }
 
@@ -72,37 +72,90 @@ parseArgs args config
         -- Conversion ---------------------------
         | "-to-salt" : file : rest  <- args
         = parseArgs rest
-        $ config { configMode   = ModeToSalt file }
+        $ setMode config $ ModeToSalt file
 
-        | "-to-c" : file : rest  <- args
+        | "-to-c"    : file : rest  <- args
         = parseArgs rest
-        $ config { configMode   = ModeToC file }
+        $ setMode config $ ModeToC file
 
         | "-to-llvm" : file : rest  <- args
         = parseArgs rest
-        $ config { configMode   = ModeToLLVM file }
+        $ setMode config $ ModeToLLVM file
 
         -- Debugging ----------------------------
-        | "-dump" : rest        <- args
+        | "-dump"   : rest        <- args
         = parseArgs rest
         $ config { configDump   = True }
 
-        | "-load" : file : rest <- args
+        | "-load"   : file : rest <- args
         = parseArgs rest
-        $ config { configMode   = ModeLoad file }
+        $ setMode config $ ModeLoad file
 
-        | "-ast" : file : rest  <- args
+        | "-ast"   : file : rest  <- args
         = parseArgs rest
-        $ config { configMode   = ModeAST file }
+        $ setMode config $ ModeAST file
 
         | "-print-builder" : rest <- args
         = parseArgs rest
-        $ config { configMode   = ModePrintBuilder }        
+        $ setMode config ModePrintBuilder
+
+        -- If we get some other argument starting with '-' then assume it's
+        -- a flag we don't support.
+        | arg : _               <- args
+        , '-' : _               <- arg
+        = error $ "Unrecognised flag: " ++ arg
+
+        -- Otherwise, treat the argument as a source file to make.
+        | arg : rest            <- args
+        = parseArgs rest
+        $ setMode config (ModeMake arg)
 
         | otherwise
         = error $ "Cannot parse arguments " ++ show args
 
 
+-- | Set the major mode of DDC.
+-- 
+--   We can't have two major modes like '-make' and '-compile' set at the same time.
+--   If this happens then `error`.
+setMode :: Config -> Mode -> Config
+setMode config newMode
+ | ModeMake{}    <- configMode config
+ , ModeMake{}    <- newMode
+ = error "Multi-module compilation is not supported yet."
+
+ | ModeCompile{} <- configMode config
+ , ModeCompile{} <- newMode
+ = error "Multi-module compilation is not supported yet."
+
+ | otherwise
+ = case flagOfMode newMode of
+    Nothing     
+     -> error "DDC.Main.Args.setMode: not setting mode to ModeNone"
+
+    Just newFlag
+     -> case flagOfMode $ configMode config of
+         Nothing        -> config { configMode = newMode }
+         Just oldFlag   -> error $ "Cannot use " ++ newFlag ++ " with " ++ oldFlag
+
+
+-- | Get the flag used to set DDC to this mode.
+flagOfMode :: Mode -> Maybe String
+flagOfMode mode
+ = case mode of
+        ModeNone{}              -> Nothing
+        ModeHelp{}              -> Just "-help"
+        ModeLoad{}              -> Just "-load"
+        ModeCompile{}           -> Just "-compile"
+        ModeMake{}              -> Just "-make"
+        ModeAST{}               -> Just "-ast"
+        ModeToSalt{}            -> Just "-to-salt"
+        ModeToC{}               -> Just "-to-c"
+        ModeToLLVM{}            -> Just "-to-llvm"
+        ModePrintBuilder{}      -> Just "-print-builder"
+
+
+-- | The command-line help page.
 help :: String
 help    = unlines
         [ "The Disciplined Disciple Compiler, version 0.3.0"
