dee2a6613bf3b2ca0a6244e1793cc5c35f29cbc8
Ben Lippmeier <benl@ouroborus.net>
2012-11-10 01:39:33 +0000

    ddc-base: add missing Check module

---
+++ packages/ddc-base/DDC/Control/Monad/Check.hs
@@ -0,0 +1,30 @@
+
+-- | A simple exception monad.
+module DDC.Control.Monad.Check
+        ( CheckM (..)
+        , throw
+        , result)
+where
+
+
+data CheckM err a
+        = CheckM (Either err a)
+
+
+instance Monad (CheckM err) where
+ return x   = CheckM (Right x)
+ (>>=) m f  
+  = case m of
+          CheckM (Left err)     -> CheckM (Left err)
+          CheckM (Right x)      -> f x
+
+          
+-- | Throw a type error in the monad.
+throw :: err -> CheckM err a
+throw e       = CheckM $ Left e
+
+
+-- | Take the result from a check monad.
+result :: CheckM err a -> Either err a
+result (CheckM r)       = r
+
