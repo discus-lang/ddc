618be79b84d1567feb52cc040293fc63257ff08d
Ben Lippmeier <benl@ouroborus.net>
2012-11-17 10:11:28 +0000

    ddc-war: pass relative path names to ddc tools

--- packages/ddc-war/DDC/War/Task/Test.hs
+++ packages/ddc-war/DDC/War/Task/Test.hs
@@ -83,7 +83,7 @@ build spec
                 
         -- Canonicalize all the paths and put them in a set (which sorts them)
         testFilesSet   <- io $ liftM (Set.fromList . Seq.toList)
-                        $  Seq.mapM canonicalizePath
+                        $  Seq.mapM (makeRelativeToCurrentDirectory <=< canonicalizePath)
                         $  testFilesRaw
 
         -- Skip over files with 'skip' in the path,
