04506f5dbf342b779dbd3474091ab83c22dac8fa
Ben Lippmeier <benl@ouroborus.net>
2013-05-24 07:47:51 +0000

    ddc-core-flow: fix alloc patcher for nested selector contexts

--- packages/ddc-core-flow/DDC/Core/Flow/Transform/Slurp/Alloc.hs
+++ packages/ddc-core-flow/DDC/Core/Flow/Transform/Slurp/Alloc.hs
@@ -17,15 +17,23 @@ patchAllocRates ops
          = [ (opOutputRate op, opInputRate op)
                 | op@OpPack{}   <- ops ]
 
-        getAllocRate rate
+        -- Fix the number of nested contexts to some finite number so we
+        -- don't end up diverging if there is a loop in the  list of
+        -- operator descriptions.
+        maxNestedContexts = 1000 :: Int
+
+        getAllocRate 0 _rate
+         = error "ddc-core-flow.patchAllocRates: too many nested contexts"
+
+        getAllocRate n rate
          = case lookup rate packRates of
-                Just inRate     -> inRate
+                Just inRate     -> getAllocRate (n - 1) inRate
                 _               -> rate
 
+        patchOperator op@OpCreate{}
+         = op { opAllocRate = Just $ getAllocRate maxNestedContexts (opInputRate op) }
+
         patchOperator op
-         = case op of
-                OpCreate{}
-                  -> op { opAllocRate = Just $ getAllocRate (opInputRate op) }
-                _ -> op
+         = op
 
    in   map patchOperator ops
