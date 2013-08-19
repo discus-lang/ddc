4303b5de0a4d48d8de7e9851110d4bd4704afdd5
Ben Lippmeier <benl@ouroborus.net>
2013-08-19 11:39:48 +0000

    source tetra: split up names file

--- packages/ddc-source-tetra/DDC/Source/Tetra/Name.hs
+++ packages/ddc-source-tetra/DDC/Source/Tetra/Name.hs
@@ -7,8 +7,8 @@ module DDC.Source.Tetra.Name
         , readName)
 where
 import DDC.Source.Tetra.Lexer.Lit
+import DDC.Source.Tetra.Name.Base
 import DDC.Base.Pretty
-import Control.DeepSeq
 import Data.Char
 
 import DDC.Core.Tetra    
@@ -20,63 +20,6 @@ import DDC.Core.Tetra.Prim
         ( readTyConPrim
         , readPrimArith
         , readPrimRef)
-
-
--- | Names of things used in Disciple Source Tetra.
-data Name
-        -- | A user defined variable.
-        = NameVar               String
-
-        -- | A user defined constructor.
-        | NameCon               String
-
-        -- Machine primitives -------------------
-        -- | Primitive type cosntructors.
-        | NameTyConPrim         TyConPrim        
-
-        -- | Primitive arithmetic, logic and comparison.
-        | NamePrimArith         PrimArith
-
-        -- | Mutable references.
-        | NamePrimRef           PrimRef
-
-
-        -- Literals -----------------------------
-        -- | A boolean literal.
-        | NameLitBool           Bool
-
-        -- | A natural literal.
-        | NameLitNat            Integer
-
-        -- | An integer literal.
-        | NameLitInt            Integer
-
-        -- | A word literal.
-        | NameLitWord           Integer Int
-
-
-        -- Inference ----------------------------
-        -- | A hole used during type inference.
-        | NameHole              
-        deriving (Eq, Ord, Show)
-
-
-instance NFData Name where
- rnf nn
-  = case nn of
-        NameVar s               -> rnf s
-        NameCon s               -> rnf s
-
-        NameTyConPrim con       -> rnf con
-        NamePrimArith con       -> rnf con
-        NamePrimRef   con       -> rnf con
-
-        NameLitBool b           -> rnf b
-        NameLitNat  n           -> rnf n
-        NameLitInt  i           -> rnf i
-        NameLitWord i bits      -> rnf i `seq` rnf bits
-
-        NameHole                -> ()
 
 
 instance Pretty Name where
@@ -140,4 +83,3 @@ readName str
 
         | otherwise
         = Nothing
-
---
+++ packages/ddc-source-tetra/DDC/Source/Tetra/Name/Base.hs
@@ -0,0 +1,67 @@
+
+module DDC.Source.Tetra.Name.Base
+        (Name   (..))
+where
+import Control.DeepSeq
+import DDC.Core.Tetra    
+        ( TyConPrim     (..)
+        , PrimArith     (..)
+        , PrimRef       (..))
+
+
+-- | Names of things used in Disciple Source Tetra.
+data Name
+        -- | A user defined variable.
+        = NameVar               String
+
+        -- | A user defined constructor.
+        | NameCon               String
+
+        -- Machine primitives -------------------
+        -- | Primitive type cosntructors.
+        | NameTyConPrim         TyConPrim        
+
+        -- | Primitive arithmetic, logic and comparison.
+        | NamePrimArith         PrimArith
+
+        -- | Mutable references.
+        | NamePrimRef           PrimRef
+
+
+        -- Literals -----------------------------
+        -- | A boolean literal.
+        | NameLitBool           Bool
+
+        -- | A natural literal.
+        | NameLitNat            Integer
+
+        -- | An integer literal.
+        | NameLitInt            Integer
+
+        -- | A word literal.
+        | NameLitWord           Integer Int
+
+
+        -- Inference ----------------------------
+        -- | A hole used during type inference.
+        | NameHole              
+        deriving (Eq, Ord, Show)
+
+
+instance NFData Name where
+ rnf nn
+  = case nn of
+        NameVar s               -> rnf s
+        NameCon s               -> rnf s
+
+        NameTyConPrim con       -> rnf con
+        NamePrimArith con       -> rnf con
+        NamePrimRef   con       -> rnf con
+
+        NameLitBool b           -> rnf b
+        NameLitNat  n           -> rnf n
+        NameLitInt  i           -> rnf i
+        NameLitWord i bits      -> rnf i `seq` rnf bits
+
+        NameHole                -> ()
+
