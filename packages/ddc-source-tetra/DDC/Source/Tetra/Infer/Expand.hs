fc50155dcaf3d1e08b96ad4a0d01b36cf4ab5f73
Ben Lippmeier <benl@ouroborus.net>
2013-08-23 00:05:15 +0000

    source tetra: add expand boilerplate

--- packages/ddc-source-tetra/DDC/Source/Tetra/Infer/Expand.hs
+++ packages/ddc-source-tetra/DDC/Source/Tetra/Infer/Expand.hs
@@ -8,7 +8,7 @@ import DDC.Source.Tetra.Module
 import DDC.Source.Tetra.Name
 import DDC.Source.Tetra.Exp
 import DDC.Type.Env                     (KindEnv, TypeEnv)
--- import qualified DDC.Type.Env           as Env
+import qualified DDC.Type.Env           as Env
 
 
 -------------------------------------------------------------------------------
@@ -26,12 +26,77 @@ configDefault
 -------------------------------------------------------------------------------
 class Expand (c :: * -> * -> *) where
  expand
-        :: Config a n
+        :: Ord n 
+        => Config a n
         -> KindEnv n -> TypeEnv n
         -> c a n     -> c a n
  
 instance Expand Module where
- expand _config _kenv _tenv mm
-        = mm
+ expand config kenv tenv mm
+  = mm { moduleTops     = map (expand config kenv tenv)
+                        $ moduleTops mm }
 
+
+instance Expand Top where
+ expand config kenv tenv top
+  = case top of
+        TopBind a b x   
+         -> let tenv'   = Env.extend b tenv
+                x'      = expand config kenv tenv' x
+            in  TopBind a b x'
+
+        TopData{}
+         -> top
+
+
+instance Expand Exp where
+ expand config kenv tenv xx
+  = let down = expand config kenv tenv
+    in case xx of
+        XVar{}          -> xx
+        XCon{}          -> xx
+
+        XLAM a b x
+         -> let kenv'   = Env.extend b kenv
+                x'      = expand config kenv' tenv x
+            in  XLAM a b x'
+
+        XLam a b x
+         -> let tenv'   = Env.extend b tenv
+                x'      = expand config kenv tenv' x
+            in  XLam a b x'
+
+        XLet a (LLet b x1) x2
+         -> let tenv'   = Env.extend b tenv
+                x1'     = expand config kenv tenv' x1
+                x2'     = expand config kenv tenv' x2
+            in  XLet a (LLet b x1') x2'
+
+        XLet a (LLetRegions bts bxs) x2
+         -> let tenv'   = Env.extends bts kenv
+                kenv'   = Env.extends bxs tenv
+                x2'     = expand config kenv' tenv' x2
+            in  XLet a (LLetRegions bts bxs) x2'
+
+        XApp a x1 x2    -> XApp a    (down x1)  (down x2)
+        XCase a x alts  -> XCase a   (down x)   (map down alts)
+        XCast a c x     -> XCast a c (down x)
+        XType{}         -> xx
+        XWitness{}      -> xx
+        XDefix a xs     -> XDefix a  (map down xs)
+        XInfixOp{}      -> xx
+        XInfixVar{}     -> xx
+
+
+instance Expand Alt where
+ expand config kenv tenv alt
+  = case alt of
+        AAlt PDefault x2
+         -> let x2'     = expand config kenv tenv x2
+            in  AAlt PDefault x2'
+
+        AAlt (PData dc bs) x2
+         -> let tenv'   = Env.extends bs tenv
+                x2'     = expand config kenv tenv' x2
+            in  AAlt (PData dc bs) x2'
 
