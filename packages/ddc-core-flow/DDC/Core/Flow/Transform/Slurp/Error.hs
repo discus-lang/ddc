cedc5158b6313c18effe985778fb357e1dcfb48d
Ben Lippmeier <benl@ouroborus.net>
2014-01-13 15:48:02 +1100

    core flow: error message wibbles

--- packages/ddc-core-flow/DDC/Core/Flow/Transform/Slurp/Error.hs
+++ packages/ddc-core-flow/DDC/Core/Flow/Transform/Slurp/Error.hs
@@ -11,7 +11,7 @@ import DDC.Core.Pretty
 -- | Things that can go wrong when slurping a process spec from
 --   Disciple Core Flow code.
 data Error
-        -- | Invalid flow process definition.
+        -- | Invalid series process definition.
         = ErrorBadProcess  (Exp () Name)
 
         -- | Invalid operator definition in process.
@@ -23,11 +23,11 @@ instance Pretty Error where
  ppr err
   = case err of
         ErrorBadProcess x
-         -> vcat [ text "Bad flow process definition."
+         -> vcat [ text "Bad series process definition."
                  , empty
                  , ppr (annotate () x) ]
 
         ErrorBadOperator x
-         -> vcat [ text "Bad flow operator."
+         -> vcat [ text "Bad series operator."
                  , empty
                  , ppr (annotate () x) ]
