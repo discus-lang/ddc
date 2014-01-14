f2f0786704da5dd9d4fa080b0b87d2bbcae03043
Ben Lippmeier <benl@ouroborus.net>
2014-01-14 23:06:16 +1100

    core: kind inference where kind of tyvar is constrained as its the type of       something used as the body of a lambda abstraction

--- packages/ddc-core/DDC/Core/Check/Judge/Type/Abs.hs
+++ packages/ddc-core/DDC/Core/Check/Judge/Type/Abs.hs
@@ -11,29 +11,10 @@ import qualified Data.Set       as Set
 -- Dispatch -------------------------------------------------------------------
 checkAbs :: Checker a n
 checkAbs !table !ctx (XLAM a b1 x2) mode
- = do   checkAbsLAM table ctx a b1 x2 mode
+ = checkAbsLAM table ctx a b1 x2 mode
          
-checkAbs !table !ctx xx@(XLam a b1 x2) mode
- -- If there is no annotation then assume it should be a data binding.
- | isBot (typeOfBind b1)
- = checkAbsLamData table a ctx b1 (tBot kData) x2 mode
-
- -- Otherwise decide what to do based on the kind.
- --  TODO: merge checkAbsLamData and checkAbsLamWitness into same case.
- | otherwise
- = do   let config      = tableConfig table
-        let kenv        = tableKindEnv table
-
-        -- The rule we use depends on the kind of the binder.
-        (b1', k1, _)    <- checkBindM config kenv ctx UniverseSpec b1 Recon           -- TODO: ctx
-
-        case universeFromType2 k1 of
-         Just UniverseData
-           -> checkAbsLamData    table a ctx b1' k1 x2 mode
-         
-         Just UniverseWitness
-           -> checkAbsLamWitness table a ctx b1' k1 x2 mode
-         _ -> throw $ ErrorLamBindBadKind a xx (typeOfBind b1') k1
+checkAbs !table !ctx (XLam a b1 x2) mode
+ = checkAbsLam table a ctx b1 x2 mode
 
 checkAbs _ _ _ _
         = error "ddc-core.checkAbs: no match."
@@ -278,19 +259,26 @@ checkAbsLAM _table _ctx0 a b1 x2 (Check tExpected)
         throw $ ErrorLAMExpectedForall a xx tExpected
 
         
--- AbsLamData -----------------------------------------------------------------
+-- AbsLam -----------------------------------------------------------------
 -- When reconstructing the type of a lambda abstraction,
 --  the formal parameter must have a type annotation: eg (\v : T. x2)
-checkAbsLamData !table !a !ctx !b1 !_k1 !x2 !Recon
+checkAbsLam !table !a !ctx !b1 !x2 !Recon
  = do   let config      = tableConfig table
         let kenv        = tableKindEnv table
-        let t1          = typeOfBind b1
         let xx          = XLam a b1 x2
 
+        -- Check the parameter ------------------
+        let t1          = typeOfBind b1
+        
         -- The formal parameter must have a type annotation.
         when (isBot t1)
          $ throw $ ErrorLamParamTypeMissing a xx b1
 
+        -- Determine the kind of the parameter.
+        (t1', k1, _)    <- checkTypeM config kenv ctx UniverseSpec t1 Recon
+        let b1'         = replaceTypeOfBind t1' b1
+
+        -- Check the body -----------------------
         -- Reconstruct a type for the body, under the extended environment.
         let (ctx', pos1) = markContext ctx
         let ctx1         = pushType b1 ctx'
@@ -299,7 +287,7 @@ checkAbsLamData !table !a !ctx !b1 !_k1 !x2 !Recon
          <- tableCheckExp table table ctx1 x2 Recon
 
         -- The body of the function must produce data.
-        (_, k2, _)      <- checkTypeM config kenv ctx2 UniverseSpec t2 Recon            -- TODO: ctx
+        (_, k2, _)      <- checkTypeM config kenv ctx2 UniverseSpec t2 Recon
         when (not $ isDataKind k2)
          $ throw $ ErrorLamBodyNotData a xx b1 t2 k2 
 
@@ -309,17 +297,19 @@ checkAbsLamData !table !a !ctx !b1 !_k1 !x2 !Recon
                         $ mapMaybe (cutTaggedClosureX b1)
                         $ Set.toList c2
 
+        -- Cut the bound type and elems under it from the context.
+        let ctx_cut     = popToPos pos1 ctx2
+        
+
+        -- Build result type --------------------
         -- Build the resulting function type.
         --   The way the effect and closure term is captured depends on
         --   the configuration flags.
         (tResult, cResult)
-         <- makeFunctionType config a xx t1 t2 e2 c2_cut
-
-        -- Cut the bound type and elems under it from the context.
-        let ctx_cut     = popToPos pos1 ctx2
+         <- makeFunctionType config a xx t1 k1 t2 e2 c2_cut
         
         returnX a
-                (\z -> XLam z b1 x2')
+                (\z -> XLam z b1' x2')
                 tResult 
                 (Sum.empty kEffect)
                 cResult
@@ -328,45 +318,65 @@ checkAbsLamData !table !a !ctx !b1 !_k1 !x2 !Recon
 
 -- When synthesizing the type of a lambda abstraction
 --   we produce a type (?1 -> ?2) with new unification variables.
---
-checkAbsLamData !table !a !ctx !b1 !_k1 !x2 !Synth
+checkAbsLam !table !a !ctx !b1 !x2 !Synth
  = do   let config      = tableConfig table     
         let kenv        = tableKindEnv table
 
-        -- Check the binder type.
-        --  If there isn't an existing annotation then make an existential.
-        --  Otherwise check it's kind.
-        (b1', ctxA)
-         <- if isBot (typeOfBind b1)
+        -- Check the parameter ------------------
+        let t1          = typeOfBind b1
+
+        -- If there isn't an existing annotation then make an existential.
+        -- Otherwise check it's kind.
+        (b1', t1', ctx1)
+         <- if isBot t1
              then do 
-                iA <- newExists kData
-                let tA   = typeOfExists iA
-                let b1'  = replaceTypeOfBind tA b1
-                let ctxA = pushExists iA ctx
-                return (b1', ctxA)
+                -- There is no annotation at all, 
+                --   so make an existential.
+                -- Missing anotations are assumed to have kind Data.
+                i1      <- newExists kData
+                let t1'  = typeOfExists i1
+                let b1'  = replaceTypeOfBind t1' b1
+                let ctx1 = pushExists i1 ctx
+                return (b1', t1', ctx1)
                      
              else do
-                -- TODO: check k2 is Data. 
-                -- TODO: use expected kind
-                --       /\a. \(x : a). x  -- a has kind data.
-                (tA, _k2, ctxA)         
-                        <- checkTypeM config kenv ctx UniverseSpec 
-                                (typeOfBind b1) (Check kData)
-                let b1' = replaceTypeOfBind tA b1
-                return (b1', ctxA)
+                -- Check the existing annotation.
+                --   This also turns explit holes ? into existentials.
+                (t1', _, ctx1)
+                        <- checkTypeM config kenv ctx UniverseSpec t1 Synth
+                let b1' = replaceTypeOfBind t1' b1
+                return (b1', t1', ctx1)
+
+
+        -- Check the body -----------------------        
+        -- Make an existential for the result type.
+        i2              <- newExists kData
+        let t2          = typeOfExists i2
         
-        -- Existential for the result type.
-        iB              <- newExists kData
-        let tB          = typeOfExists iB
-        
+        -- Push the existential for the result, 
+        -- and parameter type onto the context.
+        let (ctx2, pos1) = markContext 
+                         $ pushExists i2 ctx1
+        let ctx3         = pushType   b1' ctx2
+
+
         -- Check the body against the existential for it.
-        let ctxBA        = pushExists iB ctxA
-        let (ctx', pos1) = markContext ctxBA
-        let ctx1         = pushType   b1' ctx'
+        (x2', t2', e2, c2, ctx4)
+         <- tableCheckExp table table ctx3 x2 (Check t2)
 
-        (x2', _, e2, c2, ctx2)
-         <- tableCheckExp table table ctx1 x2 (Check tB)
+        -- Force the kind of the body to be Data.
+        (_, _, ctx5)    <- checkTypeM config kenv ctx4 UniverseSpec 
+                                (applyContext ctx4 t2')
+                                (Check kData)
 
+        -- Cut the bound type and elems under it from the context.
+        let ctx_cut     = popToPos pos1 ctx5
+
+        
+        -- Build the result type -------------
+        -- Determine the kind of the parameter.
+        (_, k1, _)      <- checkTypeM config kenv ctx5 UniverseSpec t1' Recon
+        
         -- Cut closure terms due to locally bound value vars.
         -- This also lowers deBruijn indices in un-cut closure terms.
         let c2_cut      = Set.fromList
@@ -378,24 +388,20 @@ checkAbsLamData !table !a !ctx !b1 !_k1 !x2 !Synth
         --   the configuration flags.
         (tResult, cResult)
          <- makeFunctionType config a (XLam a b1' x2) 
-                (typeOfBind b1') tB e2 c2_cut
+                t1' (applyContext ctx5 k1) 
+                t2' e2 c2_cut
 
-        -- Cut the bound type and elems under it from the context.
-        let ctx_cut     = popToPos pos1 ctx2
-        
-        let b1''        = replaceTypeOfBind
-                                (applyContext ctx2 (typeOfBind b1')) b1'
-
+                        
         ctrace  $ vcat
                 [ text "* Lam Synth"
-                , indent 2 $ ppr (XLam a b1'' x2)
+                , indent 2 $ ppr (XLam a b1' x2)
                 , text "  OUT: " <> ppr tResult
                 , indent 2 $ ppr ctx
                 , indent 2 $ ppr ctx_cut 
                 , empty ]
 
         returnX a
-                (\z -> XLam z b1'' x2')
+                (\z -> XLam z b1' x2')
                 tResult
                 (Sum.empty kEffect)
                 cResult
@@ -405,30 +411,48 @@ checkAbsLamData !table !a !ctx !b1 !_k1 !x2 !Synth
 -- When checking type type of a lambda abstraction against an existing
 --   functional type we allow the formal paramter to be missing its
 --   type annotation, and in this case we replace it with the expected type.
-checkAbsLamData !table !a !ctx !b1 !_k1 !x2 !(Check tXX)
+checkAbsLam !table !a !ctx !b1 !x2 !(Check tXX)
  | Just (tX1, tX2)      <- takeTFun tXX
  = do   let config      = tableConfig table
+        let kenv        = tableKindEnv table
+        
+        -- Check the parameter ------------------
         let t1          = typeOfBind b1
 
         -- If the parameter has no type annotation then we can use the
         --   expected type we were passed down from above.
         -- If it does have an annotation, then it needs to match the
         --   expected type.
-        (b1', ctx0) 
+        (b1', t1', ctx0) 
          <- if isBot t1             
              then 
-                return  (replaceTypeOfBind tX1 b1, ctx)
+                return  (replaceTypeOfBind tX1 b1, tX1, ctx)
              else do
                 ctx0    <- makeEq a (ErrorMismatch a tX1 t1 (XLam a b1 x2))
                                 ctx t1 tX1 
-                return  (b1, ctx0)
+                return  (b1, t1, ctx0)
                         
+        -- Check the body ----------------------
         -- Check the body of the abstraction under the extended environment.
         let (ctx', pos1) = markContext ctx0
         let ctx1         = pushType b1' ctx'
 
         (x2', t2, e2, c2, ctx2)
          <- tableCheckExp table table ctx1 x2 (Check tX2)
+
+        -- Force the kind of the body to be Data.
+        (_, _, ctx3)    <- checkTypeM config kenv ctx2 UniverseSpec 
+                                (applyContext ctx2 t2) 
+                                (Check kData)
+
+        -- Cut the bound type and elems under it from the context.
+        let ctx_cut     = popToPos pos1 ctx3
+        
+
+        -- Make the result type -----------------
+        -- Determine the kind of the parameter.
+        (_, k1, _)      <- checkTypeM config kenv ctx3 UniverseSpec t1' Recon
+
 
         -- Cut closure terms due to locally bound value vars.
         -- This also lowers deBruijn indices in un-cut closure terms.
@@ -441,11 +465,10 @@ checkAbsLamData !table !a !ctx !b1 !_k1 !x2 !(Check tXX)
         --   the configuration flags.
         (tResult, cResult)
          <- makeFunctionType config a (XLam a b1' x2) 
-                (typeOfBind b1') t2 e2 c2_cut
+                t1' (applyContext ctx3 k1) 
+                t2 e2 c2_cut
 
-        -- Cut the bound type and elems under it from the context.
-        let ctx_cut     = popToPos pos1 ctx2
-        
+       
         ctrace  $ vcat 
                 [ text "* Lam Check"
                 , indent 2 $ ppr (XLam a b1' x2)
@@ -469,18 +492,29 @@ checkAbsLamData !table !a !ctx !b1 !_k1 !x2 !(Check tXX)
 -- | Construct a function type with the given effect and closure.
 --   The way the effect and closure is handled depends on the Config.
 makeFunctionType 
-        :: Ord n
+        :: (Show n, Ord n)
         => Config n              -- ^ Type checker config.
         -> a                     -- ^ Annotation for error messages.
         -> Exp a n               -- ^ Expression for error messages.
         -> Type n                -- ^ Parameter type of the function.
+        -> Kind n                -- ^ Kind of the parameter.
         -> Type n                -- ^ Result type of the function.
         -> TypeSum n             -- ^ Effect sum.
         -> Set (TaggedClosure n) -- ^ Closure terms.
         -> CheckM a n (Type n, Set (TaggedClosure n))
 
-makeFunctionType config a xx t1 t2 e2 c2
- = do   -- Trim the closure before we annotate the returned function
+makeFunctionType config a xx t1 k1 t2 e2 c2
+ | isTExists k1
+ = throw $ ErrorLamBindBadKind a xx t1 k1
+
+ | not (k1 == kData) && not (k1 == kWitness)
+ = throw $ ErrorLamBindBadKind a xx t1 k1
+
+ | otherwise
+ = do   
+        let Just uniParam    = universeFromType2 k1
+
+        -- Trim the closure before we annotate the returned function
         -- type with it. This should always succeed because trimClosure
         -- only returns Nothing if the closure is miskinded, and we've
         -- already already checked that.
@@ -498,61 +532,38 @@ makeFunctionType config a xx t1 t2 e2 c2
                 | otherwise                             = TSum e2
 
 
-        -- If the function type for the current fragment supports
-        -- latent effects and closures then just use that.
-        if      (  configFunctionalEffects  config
+        -- Data abstraction where the function type for the language fragment
+        -- supports latent effects and closures.
+        if      (  k1 == kData
+                && configFunctionalEffects  config
                 && configFunctionalClosures config)
          then   return  ( tFunEC t1 e2_captured c2_captured t2
                         , c2)
 
-        -- If the function type for the current fragment does not support
-        -- latent effects, then the body expression needs to be pure.
-        else if (  e2_captured == tBot kEffect
+        -- Data abstraction where the function constructor for the language
+        -- fragment does not suport latent effects or closures.
+        else if (  k1 == kData
+                && e2_captured == tBot kEffect
                 && c2_captured == tBot kClosure)
          then   return  ( tFun t1 t2
                         , Set.empty)
 
+        -- Witness abstractions must always be pure,
+        --  but closures are passed through.
+        else if (  k1 == kWitness
+                && e2_captured == tBot kEffect)
+         then   return  ( tImpl t1 t2
+                        , c2 )
+
         -- We don't have a way of forming a function with an impure effect.
         else if (e2_captured /= tBot kEffect)
-         then   throw $ ErrorLamNotPure  a xx UniverseData e2_captured
+         then   throw $ ErrorLamNotPure  a xx uniParam e2_captured
 
         -- We don't have a way of forming a function with an non-empty closure.
         else if (c2_captured /= tBot kClosure)
-         then   throw $ ErrorLamNotEmpty a xx UniverseData c2_captured
+         then   throw $ ErrorLamNotEmpty a xx uniParam c2_captured
 
         -- One of the above error reporting cases should have fired already.
         else    error $ "ddc-core.makeFunctionType: is broken."
 
-
--- AbsLamWitness --------------------------------------------------------------
-checkAbsLamWitness !table !a !ctx !b1 !_k1 !x2 !_dXX
- = do   let config      = tableConfig table
-        let kenv        = tableKindEnv table
-        let t1          = typeOfBind b1
-
-        -- Check the body of the abstraction.
-        let ctx1'         = pushType b1 ctx
-        let (ctx1, pos1)  = markContext ctx1'
-
-        (x2', t2, e2, c2, ctx2)                                                 -- TODO: ctx
-                        <- tableCheckExp table table ctx1 x2 Recon
-        (_, k2, _)      <- checkTypeM config kenv ctx2 UniverseSpec t2 Recon
-
-        -- The body of a witness abstraction must be pure.
-        when (e2 /= Sum.empty kEffect)
-         $ throw $ ErrorLamNotPure      a (XLam a b1 x2) UniverseWitness (TSum e2)
-
-        -- The body of a witness abstraction must produce data.
-        when (not $ isDataKind k2)
-         $ throw $ ErrorLamBodyNotData  a (XLam a b1 x2) b1 t2 k2
-
-        -- Cut the bound type and elems under it from the context.
-        let ctx'        = popToPos pos1 ctx2
-
-        returnX a
-                (\z -> XLam z b1 x2')
-                (tImpl t1 t2)
-                (Sum.empty kEffect)
-                c2
-                ctx'
 
