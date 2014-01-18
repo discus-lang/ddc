dcc391bf5f49215e8a8d1669b96f9a70ba18699a
Ben Lippmeier <benl@ouroborus.net>
2014-01-19 06:36:53 +1100

    core: split checking of private constructs into own module, and add comments

--- packages/ddc-core/DDC/Core/Check/Exp.hs
+++ packages/ddc-core/DDC/Core/Check/Exp.hs
@@ -5,10 +5,10 @@
 --    Joshua Dunfield, Neelakantan R. Krishnaswami, ICFP 2013.
 --  
 --   Extensions include:
---    * Check let bindings and case expressions.
+--    * Check let-bindings and case-expressions.
 --    * Allow type annotations on function parameters.
 --    * Allow explicit type abstraction and application.
---    * Infer the kinds of type abstration parameters.
+--    * Infer the kinds of type parameters.
 --    * Insert type applications in the checked expression, so that the 
 --      resulting program can be checked by the standard bottom-up algorithm.
 --    * Allow explicit hole '?' annotations to indicate a type or kind 
@@ -42,6 +42,7 @@ import DDC.Core.Check.Judge.Type.LamX
 import DDC.Core.Check.Judge.Type.AppT
 import DDC.Core.Check.Judge.Type.AppX
 import DDC.Core.Check.Judge.Type.Let
+import DDC.Core.Check.Judge.Type.LetPrivate
 import DDC.Core.Check.Judge.Type.Case
 import DDC.Core.Check.Judge.Type.Cast
 import DDC.Core.Check.Judge.Type.Witness
@@ -54,60 +55,61 @@ import qualified DDC.Type.Env           as Env
 -- Wrappers -------------------------------------------------------------------
 -- | Type check an expression. 
 --
---   If it's good, you get a new version with types attached to all the bound
---   variables, as well its the type, effect and closure. 
+--   If it's good, you get a new version with types attached every AST node, 
+--   as well as every binding occurrence of a variable.
 --
 --   If it's bad, you get a description of the error.
 --
---   The returned expression has types attached to all variable occurrences, 
---   so you can call `typeOfExp` on any open subterm.
---
 --   The kinds and types of primitives are added to the environments 
---   automatically, you don't need to supply these as part of the 
---   starting environments.
+--   automatically, you don't need to supply these as part of the starting 
+--   kind and type environment.
 --
 checkExp 
         :: (Ord n, Show n, Pretty n)
         => Config n                     -- ^ Static configuration.
-        -> KindEnv n                    -- ^ Starting Kind environment.
-        -> TypeEnv n                    -- ^ Starting Type environment.
+        -> KindEnv n                    -- ^ Starting kind environment.
+        -> TypeEnv n                    -- ^ Starting type environment.
         -> Exp a n                      -- ^ Expression to check.
         -> Mode  n                      -- ^ Check mode.
-        -> ( Either (Error a n)
-                    ( Exp (AnTEC a n) n
-                    , Type n
-                    , Effect n
-                    , Closure n)
-           , CheckTrace)
+        -> ( Either (Error a n)         --   Type error message. 
+                    ( Exp (AnTEC a n) n --   Expression with type annots
+                    , Type n            --   Type of expression.
+                    , Effect n          --   Effect of expression.
+                    , Closure n)        --   Closure of expression.
+           , CheckTrace)                --   Type checker debug trace.
 
 checkExp !config !kenv !tenv !xx !mode
- = let  ((ct, _, _), result)
-         = runCheck (mempty, 0, 0)
-         $ do   (xx', t, effs, clos, ctx) 
-                 <- checkExpM 
-                        (makeTable config
-                                (Env.union kenv (configPrimKinds config))
-                                (Env.union tenv (configPrimTypes config)))
-                        emptyContext xx mode
+ = (result, ct)
+ where  
+  ((ct, _, _), result)
+   = runCheck (mempty, 0, 0) 
+   $ do
+        -- Check the expression, using the monadic checking function.
+        (xx', t, effs, clos, ctx) 
+         <- checkExpM 
+                (makeTable config
+                        (Env.union kenv (configPrimKinds config))
+                        (Env.union tenv (configPrimTypes config)))
+                emptyContext xx mode
                 
-                -- Apply the final context to the annotations in expressions.
-                let applyToAnnot (AnTEC t0 e0 c0 x0)
-                        = AnTEC (applySolved ctx t0)
-                                (applySolved ctx e0)
-                                (applySolved ctx c0)
-                                x0
+        -- Apply the final context to the annotations in expressions.
+        -- This ensures that existentials are expanded to solved types.
+        let applyToAnnot (AnTEC t0 e0 c0 x0)
+                = AnTEC (applySolved ctx t0)
+                        (applySolved ctx e0)
+                        (applySolved ctx c0)
+                        x0
 
-                let xx'' = reannotate applyToAnnot 
-                         $ mapT (applySolved ctx) xx'
+        let xx'' = reannotate applyToAnnot 
+                 $ mapT (applySolved ctx) xx'
 
-                -- Apply the final context to the overall types of the expression.
-                let t'   = applySolved ctx t
-                let e'   = applySolved ctx $ TSum effs
-                let c'   = applySolved ctx $ closureOfTaggedSet clos
+        -- Also apply the final context to the overall type, 
+        -- effect and closure of the expression.
+        let t'   = applySolved ctx t
+        let e'   = applySolved ctx $ TSum effs
+        let c'   = applySolved ctx $ closureOfTaggedSet clos
 
-                return  (xx'', t', e', c')
-  
-  in    (result, ct)
+        return  (xx'', t', e', c')
 
 
 -- | Like `checkExp`, but only return the value type of an expression.
@@ -143,17 +145,19 @@ checkExpM
 -- Dispatch to the checker table based on what sort of AST node we're at.
 checkExpM !table !ctx !xx !mode
  = case xx of
-        XVar{}           -> tableCheckVarCon  table table ctx xx mode
-        XCon{}           -> tableCheckVarCon  table table ctx xx mode
-        XApp _ _ XType{} -> tableCheckAppT    table table ctx xx mode
-        XApp{}           -> tableCheckAppX    table table ctx xx mode
-        XLAM{}           -> tableCheckLamT    table table ctx xx mode
-        XLam{}           -> tableCheckLamX    table table ctx xx mode
-        XLet{}           -> tableCheckLet     table table ctx xx mode
-        XCase{}          -> tableCheckCase    table table ctx xx mode
-        XCast{}          -> tableCheckCast    table table ctx xx mode
-        XWitness{}       -> tableCheckWitness table table ctx xx mode
-        XType    a _     -> throw $ ErrorNakedType    a xx 
+    XVar{}                 -> tableCheckVarCon     table table ctx xx mode
+    XCon{}                 -> tableCheckVarCon     table table ctx xx mode
+    XApp _ _ XType{}       -> tableCheckAppT       table table ctx xx mode
+    XApp{}                 -> tableCheckAppX       table table ctx xx mode
+    XLAM{}                 -> tableCheckLamT       table table ctx xx mode
+    XLam{}                 -> tableCheckLamX       table table ctx xx mode
+    XLet _ LPrivate{} _    -> tableCheckLetPrivate table table ctx xx mode
+    XLet _ LWithRegion{} _ -> tableCheckLetPrivate table table ctx xx mode
+    XLet{}                 -> tableCheckLet        table table ctx xx mode
+    XCase{}                -> tableCheckCase       table table ctx xx mode
+    XCast{}                -> tableCheckCast       table table ctx xx mode
+    XWitness{}             -> tableCheckWitness    table table ctx xx mode
+    XType    a _           -> throw $ ErrorNakedType    a xx 
 
 
 -- Table ----------------------------------------------------------------------
@@ -170,6 +174,7 @@ makeTable config kenv tenv
         , tableCheckLamT        = checkLamT
         , tableCheckLamX        = checkLamX
         , tableCheckLet         = checkLet
+        , tableCheckLetPrivate  = checkLetPrivate
         , tableCheckCase        = checkCase
         , tableCheckCast        = checkCast 
         , tableCheckWitness     = checkWit }
--- packages/ddc-core/DDC/Core/Check/Judge/Type/Base.hs
+++ packages/ddc-core/DDC/Core/Check/Judge/Type/Base.hs
@@ -4,9 +4,7 @@ module DDC.Core.Check.Judge.Type.Base
         , Table (..)
         , returnX
 
-        -- * Modules
         , module DDC.Core.Check.Base
-
         , module DDC.Core.Check.Judge.Inst
         , module DDC.Core.Check.Judge.Sub
         , module DDC.Core.Check.Judge.Eq
@@ -24,7 +22,6 @@ module DDC.Core.Check.Judge.Type.Base
         , module DDC.Type.Transform.Trim)
 where
 import DDC.Core.Check.Base
-
 import DDC.Core.Check.Judge.Inst
 import DDC.Core.Check.Judge.Sub
 import DDC.Core.Check.Judge.Eq
@@ -45,22 +42,30 @@ import DDC.Type.Transform.Trim
 -- | Type of the function that checks some node of the core AST.
 type Checker a n
         =  (Show n, Ord n, Pretty n)
-        => Table a n
-        -> Context n
-        -> Exp a n      
-        -> Mode n
+        => Table a n                    -- ^ Static configuration.
+        -> Context n                    -- ^ Input context.
+        -> Exp a n                      -- ^ Expression to check.
+        -> Mode n                       -- ^ Type checker mode.
         -> CheckM a n
-                ( Exp (AnTEC a n) n
-                , Type n
-                , TypeSum n
-                , Set (TaggedClosure n)
-                , Context n)
+                ( Exp (AnTEC a n) n     -- Annotated, checked expression.
+                , Type n                -- Type of the expression.
+                , TypeSum n             -- Effect sum of expression.
+                , Set (TaggedClosure n) -- Closure of expression.
+                , Context n)            -- Output context.
 
 
 -- | Table of environment things that do not change during type checking
+--
 --   We've got the static config, 
 --    global kind and type environments,
 --    and a type checking function for each node of the AST.
+--
+--   We split the type checker into separate functions and dispatch them
+--   via this table so we can handle each AST node in a separate module,
+--   while avoiding the explicit mutual recursion. If the functions were
+--   explicitly mutually recursive then we'd need to write GHC boot modules,
+--   which is annoying.
+--
 data Table a n
         = Table
         { tableConfig           :: Config n
@@ -73,6 +78,7 @@ data Table a n
         , tableCheckLamT        :: Checker a n
         , tableCheckLamX        :: Checker a n
         , tableCheckLet         :: Checker a n
+        , tableCheckLetPrivate  :: Checker a n
         , tableCheckCase        :: Checker a n
         , tableCheckCast        :: Checker a n 
         , tableCheckWitness     :: Checker a n }
@@ -81,20 +87,21 @@ data Table a n
 -- | Helper function for building the return value of checkExpM'
 --   It builts the AnTEC annotation and attaches it to the new AST node,
 --   as well as returning the current effect and closure in the appropriate
---   form as part of the tuple.
+--   form as part of the tuple. 
 returnX :: Ord n 
-        => a
-        -> (AnTEC a n -> Exp (AnTEC a n) n)
-        -> Type n 
-        -> TypeSum n
-        -> Set (TaggedClosure n)
-        -> Context n
+        => a                            -- ^ Annotation for the returned expression.
+        -> (AnTEC a n 
+                -> Exp (AnTEC a n) n)   -- ^ Fn to build the returned expression.
+        -> Type n                       -- ^ Type of expression.
+        -> TypeSum n                    -- ^ Effect sum of expression.
+        -> Set (TaggedClosure n)        -- ^ Closure of expression.
+        -> Context n                    -- ^ Input context.
         -> CheckM a n 
-                ( Exp (AnTEC a n) n
-                , Type n
-                , TypeSum n
-                , Set (TaggedClosure n)
-                , Context n)
+                ( Exp (AnTEC a n) n     -- Annotated, checked expression.
+                , Type n                -- Type of expression.       (id to above)
+                , TypeSum n             -- Effect sum of expression. (id to above)
+                , Set (TaggedClosure n) -- Closure of expression.    (id to above)
+                , Context n)            -- Output context.
 
 returnX !a !f !t !es !cs !ctx
  = let  e       = TSum es
