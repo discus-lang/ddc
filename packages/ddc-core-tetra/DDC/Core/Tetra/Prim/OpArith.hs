3dbdf427ea6913615fcb27f520d74be904b9d266
Ben Lippmeier <benl@ouroborus.net>
2014-02-24 21:00:24 +1100

    core tetra: use correct function constructor in types of arithmetic functions

--- packages/ddc-core-tetra/DDC/Core/Tetra/Prim/OpArith.hs
+++ packages/ddc-core-tetra/DDC/Core/Tetra/Prim/OpArith.hs
@@ -15,48 +15,48 @@ typePrimArith op
  = case op of
         -- Arithmetic Operators.
         --  Parameterised by the type they work on.
-        PrimArithNeg    -> tForall kData $ \t -> t `tFunPE` t
-        PrimArithAdd    -> tForall kData $ \t -> t `tFunPE` t `tFunPE` t
-        PrimArithSub    -> tForall kData $ \t -> t `tFunPE` t `tFunPE` t
-        PrimArithMul    -> tForall kData $ \t -> t `tFunPE` t `tFunPE` t
-        PrimArithDiv    -> tForall kData $ \t -> t `tFunPE` t `tFunPE` t
-        PrimArithMod    -> tForall kData $ \t -> t `tFunPE` t `tFunPE` t
-        PrimArithRem    -> tForall kData $ \t -> t `tFunPE` t `tFunPE` t
+        PrimArithNeg    -> tForall kData $ \t -> t `tFun` t
+        PrimArithAdd    -> tForall kData $ \t -> t `tFun` t `tFun` t
+        PrimArithSub    -> tForall kData $ \t -> t `tFun` t `tFun` t
+        PrimArithMul    -> tForall kData $ \t -> t `tFun` t `tFun` t
+        PrimArithDiv    -> tForall kData $ \t -> t `tFun` t `tFun` t
+        PrimArithMod    -> tForall kData $ \t -> t `tFun` t `tFun` t
+        PrimArithRem    -> tForall kData $ \t -> t `tFun` t `tFun` t
 
         -- Bitwise Operators.
-        PrimArithShl    -> tForall kData $ \t -> t `tFunPE` t `tFunPE` t
-        PrimArithShr    -> tForall kData $ \t -> t `tFunPE` t `tFunPE` t
-        PrimArithBAnd   -> tForall kData $ \t -> t `tFunPE` t `tFunPE` t
-        PrimArithBOr    -> tForall kData $ \t -> t `tFunPE` t `tFunPE` t
-        PrimArithBXOr   -> tForall kData $ \t -> t `tFunPE` t `tFunPE` t
+        PrimArithShl    -> tForall kData $ \t -> t `tFun` t `tFun` t
+        PrimArithShr    -> tForall kData $ \t -> t `tFun` t `tFun` t
+        PrimArithBAnd   -> tForall kData $ \t -> t `tFun` t `tFun` t
+        PrimArithBOr    -> tForall kData $ \t -> t `tFun` t `tFun` t
+        PrimArithBXOr   -> tForall kData $ \t -> t `tFun` t `tFun` t
 
         -- Boolean Operators.
         PrimArithAnd    -> tForall kData $ \tb
-                        -> tb `tFunPE` tb `tFunPE` tb
+                        -> tb `tFun` tb `tFun` tb
         
         PrimArithOr     -> tForall kData $ \tb
-                        -> tb `tFunPE` tb `tFunPE` tb
+                        -> tb `tFun` tb `tFun` tb
 
         -- Comparison Operators.
         --  These are parameterised by the input type, as well as the boolean result, 
         --  so that we can convert between value type and unboxed type representations
         --  in the boxing transform.
         PrimArithEq     -> tForalls [kData, kData] $ \[t, tb]
-                        -> t `tFunPE` t `tFunPE` tb
+                        -> t `tFun` t `tFun` tb
         
         PrimArithNeq    -> tForalls [kData, kData] $ \[t, tb]
-                        -> t `tFunPE` t `tFunPE` tb
+                        -> t `tFun` t `tFun` tb
         
         PrimArithGt     -> tForalls [kData, kData] $ \[t, tb]
-                        -> t `tFunPE` t `tFunPE` tb
+                        -> t `tFun` t `tFun` tb
         
         PrimArithLt     -> tForalls [kData, kData] $ \[t, tb]
-                        -> t `tFunPE` t `tFunPE` tb
+                        -> t `tFun` t `tFun` tb
         
         PrimArithLe     -> tForalls [kData, kData] $ \[t, tb]
-                        -> t `tFunPE` t `tFunPE` tb
+                        -> t `tFun` t `tFun` tb
         
         PrimArithGe     -> tForalls [kData, kData] $ \[t, tb]
-                        -> t `tFunPE` t `tFunPE` tb
+                        -> t `tFun` t `tFun` tb
 
 
