6cb2c5df26d5f340e9e46fda51de719610211f13
Amos Robinson <amos.robinson@gmail.com>
2014-08-22 23:41:33 +1000

    ddc-core-tetra: add allocPtr#, readPtr# and writePtr# primitives for reading/writing vectors, and projX_Y# primitives for projecting out of tuples. (since vectors are translated to tuples of ptr and nat length)

--- packages/ddc-core-tetra/DDC/Core/Tetra/Prim/OpStore.hs
+++ packages/ddc-core-tetra/DDC/Core/Tetra/Prim/OpStore.hs
@@ -3,18 +3,22 @@ module DDC.Core.Tetra.Prim.OpStore
         ( readOpStore
         , typeOpStore)
 where
+import DDC.Core.Tetra.Prim.TyConPrim
 import DDC.Core.Tetra.Prim.TyConTetra
 import DDC.Core.Tetra.Prim.Base
 import DDC.Type.Compounds
 import DDC.Type.Exp
 import DDC.Base.Pretty
 import Control.DeepSeq
+import Data.Char
 import Data.List
 
 
 instance NFData OpStore
 
 instance Pretty OpStore where
+ ppr (OpStoreProj i j)
+  = text ("proj" ++ show i ++ "_" ++ show j ++ "#")
  ppr op
   = let Just (_, n) = find (\(p, _) -> op == p) opStoreNames
     in  (text n)
@@ -23,6 +27,19 @@ instance Pretty OpStore where
 -- | Read a primitive store operator.
 readOpStore :: String -> Maybe OpStore
 readOpStore str
+  | Just rest         <- stripPrefix "proj" str
+  , (ds, '_' : rest2) <- span isDigit rest
+  , not $ null ds
+  , arity             <- read ds
+  , arity >= 1
+  , (ds2, "#")        <- span isDigit rest2
+  , not $ null ds2
+  , ix                <- read ds2
+  , ix >= 1
+  , ix <= arity
+  = Just $ OpStoreProj arity ix
+
+  | otherwise
   =  case find (\(_, n) -> str == n) opStoreNames of
         Just (p, _)     -> Just p
         _               -> Nothing
@@ -33,7 +50,10 @@ opStoreNames :: [(OpStore, String)]
 opStoreNames
  =      [ (OpStoreAllocRef,   "allocRef#")
         , (OpStoreReadRef,    "readRef#")
-        , (OpStoreWriteRef,   "writeRef#") ]
+        , (OpStoreWriteRef,   "writeRef#")
+        , (OpStoreAllocPtr,   "allocPtr#")
+        , (OpStoreReadPtr,    "readPtr#")
+        , (OpStoreWritePtr,   "writePtr#") ]
 
 
 -- | Take the type of a primitive store operator.
@@ -54,3 +74,26 @@ typeOpStore op
          -> tForalls [kRegion, kData]
          $  \[tR, tA] -> tRef tR tA `tFun` tA
                         `tFun` tSusp (tWrite tR) tUnit
+
+
+        OpStoreAllocPtr
+         -> tForalls [kRegion, kData] 
+          $ \[tR, tA] -> tNat
+                        `tFun` tSusp (tAlloc tR) (tPtr tR tA)
+
+        OpStoreReadPtr   
+         -> tForalls [kRegion, kData]
+          $ \[tR, tA] -> tPtr tR tA `tFun` tNat
+                        `tFun` tSusp (tRead tR) tA
+
+        OpStoreWritePtr  
+         -> tForalls [kRegion, kData]
+         $  \[tR, tA] -> tPtr tR tA `tFun` tNat `tFun` tA
+                        `tFun` tSusp (tWrite tR) tUnit
+
+        -- Tuple projections --------------------
+        OpStoreProj a ix
+         -> tForalls (replicate a kData) 
+         $ \_ -> tFun   (tTupleN [TVar (UIx i) | i <- reverse [0..a-1]])
+                        (TVar (UIx (a - ix)))
+
