de2ce3b2d08f0ab35d14a59bca650369083aa29d
Ben Lippmeier <benl@ouroborus.net>
2014-12-29 11:13:58 +1100

    core: split up parser for import/export specs

---
+++ packages/ddc-core/DDC/Core/Parser/DataDef.hs
@@ -0,0 +1,84 @@
+
+module DDC.Core.Parser.DataDef
+        ( DataDef    (..)
+        , pDataDef)
+where
+import DDC.Core.Exp
+import DDC.Core.Parser.Type
+import DDC.Core.Parser.Context
+import DDC.Core.Parser.Base
+import DDC.Core.Lexer.Tokens
+import DDC.Core.Compounds
+import DDC.Type.DataDef
+import Control.Monad
+import qualified DDC.Base.Parser        as P
+
+
+pDataDef :: Ord n => Context n -> Parser n (DataDef n)
+pDataDef c
+ = do   pTokSP KData
+        nData   <- pName 
+        bsParam <- liftM concat $ P.many (pDataParam c)
+
+        P.choice
+         [ -- Data declaration with constructors that have explicit types.
+           do   pTok KWhere
+                pTok KBraceBra
+                ctors      <- P.sepEndBy1 (pDataCtor c nData bsParam) (pTok KSemiColon)
+                let ctors' = [ ctor { dataCtorTag = tag }
+                                | ctor <- ctors
+                                | tag  <- [0..] ]
+                pTok KBraceKet
+                return  $ DataDef 
+                        { dataDefTypeName       = nData
+                        , dataDefParams         = bsParam 
+                        , dataDefCtors          = Just ctors'
+                        , dataDefIsAlgebraic    = True }
+         
+           -- Data declaration with no data constructors.
+         , do   return  $ DataDef 
+                        { dataDefTypeName       = nData
+                        , dataDefParams         = bsParam
+                        , dataDefCtors          = Just []
+                        , dataDefIsAlgebraic    = True }
+
+         ]
+
+
+-- | Parse a type parameter to a data type.
+pDataParam :: Ord n => Context n -> Parser n [Bind n]
+pDataParam c 
+ = do   pTok KRoundBra
+        ns      <- P.many1 pName
+        pTokSP (KOp ":")
+        k       <- pType c
+        pTok KRoundKet
+        return  [BName n k | n <- ns]
+
+
+-- | Parse a data constructor declaration.
+pDataCtor 
+        :: Ord n 
+        => Context n
+        -> n                    -- ^ Name of data type constructor.
+        -> [Bind n]             -- ^ Type parameters of data type constructor.
+        -> Parser n (DataCtor n)
+
+pDataCtor c nData bsParam
+ = do   n       <- pName
+        pTokSP (KOp ":")
+        t       <- pType c
+        let (tsArg, tResult)    
+                = takeTFunArgResult t
+
+        return  $ DataCtor
+                { dataCtorName          = n
+
+                -- Set tag to 0 for now. We fix this up in pDataDef above.
+                , dataCtorTag           = 0
+                
+                , dataCtorFieldTypes    = tsArg
+                , dataCtorResultType    = tResult 
+                , dataCtorTypeName      = nData 
+                , dataCtorTypeParams    = bsParam }
+
---
+++ packages/ddc-core/DDC/Core/Parser/ExportSpec.hs
@@ -0,0 +1,76 @@
+
+module DDC.Core.Parser.ExportSpec
+        ( ExportSpec    (..)
+        , pExportSpecs)
+where
+import DDC.Core.Module
+import DDC.Core.Parser.Type
+import DDC.Core.Parser.Context
+import DDC.Core.Parser.Base
+import DDC.Core.Lexer.Tokens
+import DDC.Base.Pretty
+import Control.Monad
+import qualified DDC.Base.Parser        as P
+
+
+data ExportSpec n
+        = ExportValue   n (ExportSource n)
+
+
+-- | Parse some export specs.
+pExportSpecs
+        :: (Ord n, Pretty n)
+        => Context n -> Parser n [ExportSpec n]
+
+pExportSpecs c
+ = do   pTok KExport
+
+        P.choice 
+         [      -- export value { (NAME :: TYPE)+ }
+           do   P.choice [ pTok KValue, return () ]
+                pTok KBraceBra
+                specs   <- P.sepEndBy1 (pExportValue c) (pTok KSemiColon)
+                pTok KBraceKet 
+                return specs
+
+                -- export foreign X value { (NAME :: TYPE)+ }
+         , do   pTok KForeign
+                dst     <- liftM (renderIndent . ppr) pName
+                pTok KValue
+                pTok KBraceBra
+                specs   <- P.sepEndBy1 (pExportForeignValue c dst) (pTok KSemiColon)
+                pTok KBraceKet
+                return specs
+         ]
+
+
+-- | Parse an export spec.
+pExportValue
+        :: (Ord n, Pretty n)
+        => Context n -> Parser n (ExportSpec n)
+pExportValue c 
+ = do   
+        n       <- pName
+        pTokSP (KOp ":")
+        t       <- pType c
+        return  (ExportValue n (ExportSourceLocal n t))
+
+
+-- | Parse a foreign value export spec.
+pExportForeignValue    
+        :: (Ord n, Pretty n)
+        => Context n -> String -> Parser n (ExportSpec n)
+pExportForeignValue c dst
+        | "c"           <- dst
+        = do    n       <- pName
+                pTokSP (KOp ":")
+                k       <- pType c
+
+                -- ISSUE #327: Allow external symbol to be specified 
+                --             with foreign C imports and exports.
+                return  (ExportValue n (ExportSourceLocal n k))
+
+        | otherwise
+        = P.unexpected "export mode for foreign value."
+
+
