6d8bca337eb5314e2a39d712ee9782f78fa21b2f
Jacob Stanley <jacob@stanley.io>
2015-07-11 15:06:06 +1000

    make: stop clean from trashing .cabal-sandbox

--- make/targets/clean.mk
+++ make/targets/clean.mk
@@ -5,19 +5,21 @@ clean  : cleanWar cleanRuntime
 	@echo "* Cleaning leftovers"
 	@find . \
 			-name "*.o" \
-		-o      -name "*.o-boot" \
+		-o	-name "*.o-boot" \
 		-o	-name "*.so" \
-		-o  -name "*.dylib" \
+		-o	-name "*.dylib" \
 		-o	-name "*.hi" \
 		-o	-name "*.hi-boot" \
 		-o	-name "*.hcr" \
 		-o	-name "*.td" \
 		-o	-name "*.ti" \
 		-o	-name "*.deps" \
-		-o      -name "*.deps.inc" \
+		-o	-name "*.deps.inc" \
 		-o	-name "*.vo" \
 		-o	-name "*.glob" \
-		-follow | xargs -n 1 rm -f
+		-follow \
+		| grep -v "\.cabal-sandbox" \
+		| xargs -n 1 rm -f
 
 	@rm -f  doc/haddock/*
 	@rm -f  doc/haddock-core/*
