23d7c55dd03889536a9b6c3ee87e4baf83476445
Ben Lippmeier <benl@ouroborus.net>
2016-08-09 13:47:27 +1000

    inchworm: add minimal example to front haddock page

--- packages/inchworm/Text/Lexer/Inchworm.hs
+++ packages/inchworm/Text/Lexer/Inchworm.hs
@@ -12,6 +12,58 @@
 --
 --   No dependencies other than the Haskell 'base' library.
 --
+-- __ Minimal example __
+--
+-- The following code demonstrates how to perform lexical analysis
+-- of a simple LISP-like language. We use two separate name classes,
+-- one for variables that start with a lower-case letter, 
+-- and one for constructors with an upper case letter. 
+--
+-- Integers are scanned using the `scanInteger` function from the 
+-- "Text.Lexer.Inchworm.Char" module. The result contains any leftover
+-- input characters that could not be parsed. In a real lexer you should
+-- check that this is empty to ensure there has not been a lexical error.
+--
+-- @
+-- import Text.Lexer.Inchworm.Char
+-- import qualified Data.Char      as Char
+-- 
+-- -- | A source token.
+-- data Token 
+--         = KBra | KKet | KVar String | KCon String | KInt Integer
+--         deriving Show
+-- 
+-- -- | A thing with attached location information.
+-- data Located a
+--         = Located FilePath Location a
+--         deriving Show
+-- 
+-- -- | Scanner for a lispy language.
+-- scanner :: FilePath
+--         -> Scanner IO Location [Char] (Located Token)
+-- scanner fileName
+--  = skip Char.isSpace
+--  $ alts [ fmap (stamp id)   $ accept '(' KBra
+--         , fmap (stamp id)   $ accept ')' KKet
+--         , fmap (stamp KInt) $ scanInteger 
+--         , fmap (stamp KVar)
+--           $ munchWord (\\ix c  -> if ix == 0 then Char.isLower c
+--                                             else Char.isAlpha c) 
+--         , fmap (stamp KCon) 
+--           $ munchWord (\\ix  c -> if ix == 0 then Char.isUpper c
+--                                             else Char.isAlpha c)
+--         ]
+--  where  -- Stamp a token with source location information.
+--         stamp k (l, t) 
+--           = Located fileName l (k t)
+-- 
+-- main 
+--  = do   let fileName = "Source.lispy"
+--         let source   = "(some (Lispy like) 26 Program 93 (for you))"
+--         toks    <- scanStringIO source (scanner fileName)
+--         print toks
+-- @
+--
 module Text.Lexer.Inchworm
         ( -- * Basic Types
           Source
