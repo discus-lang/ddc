e5e5286594ee9919fd67fe310dc80bacb8a12240
Ben Lippmeier <benl@ouroborus.net>
2016-09-02 13:57:30 +1000

    Add tracing to EqT

--- packages/ddc-core/DDC/Core/Check/Judge/EqT.hs
+++ packages/ddc-core/DDC/Core/Check/Judge/EqT.hs
@@ -2,11 +2,7 @@
 module DDC.Core.Check.Judge.EqT
         (makeEqT)
 where
-import DDC.Core.Check.Config
-import DDC.Core.Check.Context
-import DDC.Type.Exp
-import DDC.Base.Pretty
-import qualified DDC.Core.Check.Base    as C
+import DDC.Core.Check.Base 
 
 
 -- | Make two types equivalent to each other,
@@ -16,24 +12,40 @@ makeEqT :: (Eq n, Ord n, Pretty n)
         -> Context n
         -> Type n
         -> Type n
-        -> C.Error  a n
-        -> C.CheckM a n (Context n)
+        -> Error  a n
+        -> CheckM a n (Context n)
 
 makeEqT config ctx0 tL tR err
 
- -- EqLSolve
+ -- EqT_SolveL
  | Just iL <- takeExists tL
- , not $ C.isTExists tR
- = do   let Just ctx1   = updateExists [] iL tR ctx0
+ , not $ isTExists tR
+ = do   
+        ctrace  $ vcat
+                [ text "**  EqT_SolveL"
+                , text "    tL:  " <> ppr tL
+                , text "    tR:  " <> ppr tR
+                , empty ]
+
+        let Just ctx1   = updateExists [] iL tR ctx0
+        
         return ctx1
 
- -- EqRSolve
+ -- EqT_SolveR
  | Just iR <- takeExists tR
- , not $ C.isTExists tL
- = do   let Just ctx1   = updateExists [] iR tL ctx0
+ , not $ isTExists tL
+ = do   
+        ctrace  $ vcat
+                [ text "**  EqT_SolveR"
+                , text "    tL:  " <> ppr tL
+                , text "    tR:  " <> ppr tR
+                , empty ]
+  
+        let Just ctx1   = updateExists [] iR tL ctx0
+        
         return ctx1
 
- -- EqLReach
+ -- EqT_EachL
  --  Both types are existentials, and the left is bound earlier in the stack.
  --  CAREFUL: The returned location is relative to the top of the stack,
  --           hence we need lL > lR here.
@@ -41,9 +53,18 @@ makeEqT config ctx0 tL tR err
  , Just iR <- takeExists tR,    Just lR <- locationOfExists iR ctx0
  , lL > lR
  = do   let Just ctx1   = updateExists [] iR tL ctx0
+        
+        ctrace  $ vcat
+                [ text "**  EqT_EachL"
+                , text "    tL: " <> ppr tL
+                , text "    tR: " <> ppr tR
+                , indent 4 $ ppr ctx0
+                , indent 4 $ ppr ctx1
+                , empty ]
+
         return ctx1
 
- -- EqRReach
+ -- EqT_EachR
  --  Both types are existentials, and the right is bound earlier in the stack.
  --  CAREFUL: The returned location is relative to the top of the stack,
  --           hence we need lR > lL here.
@@ -51,32 +72,73 @@ makeEqT config ctx0 tL tR err
  , Just iR <- takeExists tR,    Just lR <- locationOfExists iR ctx0
  , lR > lL
  = do   let Just ctx1   = updateExists [] iL tR ctx0
+
+        ctrace  $ vcat
+                [ text "**  EqT_EachR"
+                , text "    tL: " <> ppr tL
+                , text "    tR: " <> ppr tR
+                , indent 4 $ ppr ctx0
+                , indent 4 $ ppr ctx1
+                , empty ]
+
         return ctx1
 
  -- EqVar
  | TVar u1      <- tL
  , TVar u2      <- tR
  , u1 == u2
- =      return ctx0
+ = do   
+        -- Suppress tracing of boring rule.
+        -- ctrace  $ vcat
+        --         [ text "**  EqX_Var"
+        --         , text "    tL: " <> ppr tL
+        --         , text "    tR: " <> ppr tR
+        --         , indent 4 $ ppr ctx0
+        --         , empty ]
+
+        return ctx0
 
  -- EqCon
  | TCon tc1     <- tL
  , TCon tc2     <- tR
- , C.equivTyCon tc1 tc2
- =      return ctx0
+ , equivTyCon tc1 tc2
+ = do
+        -- Only trace rule if it's done something interesting.
+        when (not $ tc1 == tc2)
+         $ ctrace  $ vcat
+                [ text "**  EqT_Con"
+                , text "    tL: " <> ppr tL
+                , text "    tR: " <> ppr tR
+                , empty ]
+
+        return ctx0
 
  -- EqApp
  | TApp tL1 tL2 <- tL
  , TApp tR1 tR2 <- tR
  = do
+        ctrace  $ vcat
+                [ text "*>  EqT_App" 
+                , text "    tL: " <> ppr tL
+                , text "    tR: " <> ppr tR
+                , empty ]
+
         ctx1    <- makeEqT config ctx0 tL1  tR1  err
-        tL2'    <- C.applyContext ctx1 tL2
-        tR2'    <- C.applyContext ctx1 tR2
+        tL2'    <- applyContext ctx1 tL2
+        tR2'    <- applyContext ctx1 tR2
         ctx2    <- makeEqT config ctx0 tL2' tR2' err
+
+        ctrace  $ vcat
+                [ text "*<  EqT_App"
+                , text "    tL: " <> ppr tL
+                , text "    tR: " <> ppr tR
+                , indent 4 $ ppr ctx0
+                , indent 4 $ ppr ctx2
+                , empty ]
 
         return ctx2
 
  -- Error
  | otherwise
- =      C.throw err
+ =      throw err
 
