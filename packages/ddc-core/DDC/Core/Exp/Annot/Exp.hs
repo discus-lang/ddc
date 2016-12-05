e13046b0c2ec1239e9c5b930252837091a93542c
Ben Lippmeier <benl@ouroborus.net>
2016-12-05 17:23:53 +1100

    core: split NFData from core AST module

--- packages/ddc-core/DDC/Core/Exp/Annot/Exp.hs
+++ packages/ddc-core/DDC/Core/Exp/Annot/Exp.hs
@@ -2,18 +2,19 @@
 -- | Core language AST that includes an annotation on every node of
 --   an expression.
 --
---   This is the default representation for Disciple Core, and should be preferred
---   over the 'Simple' version of the AST in most cases.
+--   This is the default representation for Disciple Core, and should be
+--   preferred over the 'Simple' version of the AST in most cases.
 --
---   * Local transformations on this AST should propagate the annotations in a way that
---   would make sense if they were source position identifiers that tracked the provenance
---   of each code snippet. If the specific annotations attached to the AST would not make
---   sense after such a transformation, then the client should erase them to @()@ beforehand
---   using the `reannotate` transform.
+--   * Local transformations on this AST should propagate the annotations in a
+--     way that would make sense if they were source position identifiers that
+--     tracked the provenance of each code snippet. If the specific annotations
+--     attached to the AST would not make sense after such a transformation,
+--     then the client should erase them to @()@ beforehand using the
+--    `reannotate` transform.
 --
---   * Global transformations that drastically change the provenance of code snippets should
---     accept an AST with an arbitrary annotation type, but produce one with the annotations
---     set to @()@.
+--   * Global transformations that drastically change the provenance of code
+--     snippets should accept an AST with an arbitrary annotation type, but
+--     produce one with the annotations set to @()@.
 --
 module DDC.Core.Exp.Annot.Exp
         ( module DDC.Type.Exp
@@ -44,10 +45,8 @@ import DDC.Core.Exp.DaCon
 import DDC.Type.Exp
 import DDC.Type.Sum             ()
 import Data.Text                (Text)
-import Control.DeepSeq
 
 
--- Values ---------------------------------------------------------------------
 -- | Well-typed expressions have types of kind `Data`.
 data Exp a n
         -- | Primitive operator of the ambient calculus.
@@ -147,10 +146,10 @@ data Cast a n
         -- | Weaken the effect of an expression.
         --   The given effect is added to the effect
         --   of the body.
-        = CastWeakenEffect  !(Effect n)
+        = CastWeakenEffect !(Effect n)
 
         -- | Purify the effect (action) of an expression.
-        | CastPurify    !(Witness a n)
+        | CastPurify       !(Witness a n)
 
         -- | Box up a computation,
         --   capturing its effects in the S computation type.
@@ -164,6 +163,9 @@ data Cast a n
 
 -- | When a witness exists in the program it guarantees that a
 --   certain property of the program is true.
+--
+--   Well-typed witnesses have types of kind `Witness`.
+--
 data Witness a n
         -- | Witness variable.
         = WVar  a !(Bound n)
@@ -178,72 +180,3 @@ data Witness a n
         | WType a !(Type n)
         deriving (Show, Eq)
 
-
--- NFData ---------------------------------------------------------------------
-instance (NFData a, NFData n) => NFData (Exp a n) where
- rnf xx
-  = case xx of
-        XVar  a u       -> rnf a `seq` rnf u
-        XPrim a _       -> rnf a 
-        XCon  a dc      -> rnf a `seq` rnf dc
-        XAbs  a b x     -> rnf a `seq` rnf b   `seq` rnf x
-        XApp  a x1 x2   -> rnf a `seq` rnf x1  `seq` rnf x2
-        XLet  a lts x   -> rnf a `seq` rnf lts `seq` rnf x
-        XCase a x alts  -> rnf a `seq` rnf x   `seq` rnf alts
-        XCast a c x     -> rnf a `seq` rnf c   `seq` rnf x
-
-
-instance (NFData a, NFData n) => NFData (Arg a n) where
- rnf rr
-  = case rr of
-        RType t                 -> rnf t
-        RTerm x                 -> rnf x
-        RImplicit x             -> rnf x
-        RWitness  x             -> rnf x
-
-
-instance (NFData n)  => NFData (Param n) where
- rnf mm
-  = case mm of
-        MTerm b                 -> rnf b
-        MType b                 -> rnf b
-        MImplicit b             -> rnf b
-
-
-instance (NFData a, NFData n) => NFData (Cast a n) where
- rnf cc
-  = case cc of
-        CastWeakenEffect e      -> rnf e
-        CastPurify w            -> rnf w
-        CastBox                 -> ()
-        CastRun                 -> ()
-
-
-instance (NFData a, NFData n) => NFData (Lets a n) where
- rnf lts
-  = case lts of
-        LLet b x                -> rnf b `seq` rnf x
-        LRec bxs                -> rnf bxs
-        LPrivate bs1 u2 bs3     -> rnf bs1 `seq` rnf u2 `seq` rnf bs3
-
-
-instance (NFData a, NFData n) => NFData (Alt a n) where
- rnf aa
-  = case aa of
-        AAlt w x                -> rnf w `seq` rnf x
-
-
-instance NFData n => NFData (Pat n) where
- rnf pp
-  = case pp of
-        PDefault                -> ()
-        PData dc bs             -> rnf dc `seq` rnf bs
-
-
-instance (NFData a, NFData n) => NFData (Witness a n) where
- rnf ww
-  = case ww of
-        WVar  a u                 -> rnf a `seq` rnf u
-        WCon  a c                 -> rnf a `seq` rnf c
-        WApp  a w1 w2             -> rnf a `seq` rnf w1 `seq` rnf w2
-        WType a tt                -> rnf a `seq` rnf tt
---
+++ packages/ddc-core/DDC/Core/Exp/Annot/NFData.hs
@@ -0,0 +1,74 @@
+
+-- | `NFData` instances for Core AST types.
+module DDC.Core.Exp.Annot.NFData where
+import DDC.Core.Exp.Annot.Exp
+import Control.DeepSeq
+
+
+instance (NFData a, NFData n) => NFData (Exp a n) where
+ rnf xx
+  = case xx of
+        XVar  a u               -> rnf a `seq` rnf u
+        XPrim a _               -> rnf a 
+        XCon  a dc              -> rnf a `seq` rnf dc
+        XAbs  a b x             -> rnf a `seq` rnf b   `seq` rnf x
+        XApp  a x1 x2           -> rnf a `seq` rnf x1  `seq` rnf x2
+        XLet  a lts x           -> rnf a `seq` rnf lts `seq` rnf x
+        XCase a x alts          -> rnf a `seq` rnf x   `seq` rnf alts
+        XCast a c x             -> rnf a `seq` rnf c   `seq` rnf x
+
+
+instance (NFData a, NFData n) => NFData (Arg a n) where
+ rnf rr
+  = case rr of
+        RType t                 -> rnf t
+        RTerm x                 -> rnf x
+        RImplicit x             -> rnf x
+        RWitness  x             -> rnf x
+
+
+instance (NFData n)  => NFData (Param n) where
+ rnf mm
+  = case mm of
+        MTerm b                 -> rnf b
+        MType b                 -> rnf b
+        MImplicit b             -> rnf b
+
+
+instance (NFData a, NFData n) => NFData (Cast a n) where
+ rnf cc
+  = case cc of
+        CastWeakenEffect e      -> rnf e
+        CastPurify w            -> rnf w
+        CastBox                 -> ()
+        CastRun                 -> ()
+
+
+instance (NFData a, NFData n) => NFData (Lets a n) where
+ rnf lts
+  = case lts of
+        LLet b x                -> rnf b `seq` rnf x
+        LRec bxs                -> rnf bxs
+        LPrivate bs1 u2 bs3     -> rnf bs1 `seq` rnf u2 `seq` rnf bs3
+
+
+instance (NFData a, NFData n) => NFData (Alt a n) where
+ rnf aa
+  = case aa of
+        AAlt w x                -> rnf w `seq` rnf x
+
+
+instance NFData n => NFData (Pat n) where
+ rnf pp
+  = case pp of
+        PDefault                -> ()
+        PData dc bs             -> rnf dc `seq` rnf bs
+
+
+instance (NFData a, NFData n) => NFData (Witness a n) where
+ rnf ww
+  = case ww of
+        WVar  a u               -> rnf a `seq` rnf u
+        WCon  a c               -> rnf a `seq` rnf c
+        WApp  a w1 w2           -> rnf a `seq` rnf w1 `seq` rnf w2
+        WType a tt              -> rnf a `seq` rnf tt
