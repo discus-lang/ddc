34c09128628d3ad27adcbadf43878d2e6173082d
Chris Hall <followingthepath@gmail.com>
2017-04-23 11:54:49 +1000

    C99 requires usage of the PRI[f][nn] macros for printing uintn_t.

--- packages/ddc-code/sea/primitive/Primitive.c
+++ packages/ddc-code/sea/primitive/Primitive.c
@@ -2,6 +2,7 @@
 // Primitive operations that sea code uses.
 // In future we'll just import these with the FFI.
 #include <stdio.h>
+#include <inttypes.h>
 #include "Runtime.h"
 
 
@@ -94,7 +95,7 @@ nat_t   ddcLlvmRootIsEnd (void* p)
 Obj*    primErrorDefault(string_t* source, uint32_t line)
 {
         fprintf ( stderr
-                , "\nDDC runtime error: inexhaustive case match.\n at: %s:%d\n"
+                , "\nDDC runtime error: inexhaustive case match.\n at: %s:%" PRId32 "\n"
                 , source, line);
         exit(1);
 
@@ -128,28 +129,28 @@ string_t* primShowNat (nat_t i)
 // Show a Word64.
 string_t* primShowWord64 (uint64_t w)
 {       string_t* str = malloc(11);
-        snprintf(str, 10, "%#08llx", w);
+        snprintf(str, 10, "%#08" PRIx64, w);
         return str;
 }
 
 // Show a Word32.
 string_t* primShowWord32 (uint32_t w)
 {       string_t* str = malloc(7);
-        snprintf(str, 6, "%#04x", w);
+        snprintf(str, 6, "%#04" PRIx32, w);
         return str;
 }
 
 // Show a Word16.
 string_t* primShowWord16 (uint16_t w)
 {       string_t* str = malloc(5);
-        snprintf(str, 4, "%#02x", w);
+        snprintf(str, 4, "%#02" PRIx16, w);
         return str;
 }
 
 // Show a Word8.
 string_t* primShowWord8 (uint8_t w)
 {       string_t* str = malloc(4);
-        snprintf(str, 3, "%#01x", w);
+        snprintf(str, 3, "%#01" PRIx8, w);
         return str;
 }
 
