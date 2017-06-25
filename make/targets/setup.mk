9c3edb8a1c9afcfd005bc8cc14bccc2e4a0cffed
Ben Lippmeier <benl@ouroborus.net>
2017-06-25 20:11:01 +1000

    Add make setup target to install package deps

---
+++ make/targets/setup.mk
@@ -0,0 +1,16 @@
+
+# Print the packages needed to build DDC.
+.PHONY  : show-pkgs
+show-pkgs :
+	@echo $(DDC_PACKAGES) \
+         | sed 's/-hide-all-packages //;s/-package //g;s/base //g;s/directory //;s/array //;s/containers //'
+
+
+# Install prerequisite cabal packages.
+.PHONY 	: setup
+setup :
+	@echo "* Installing prerequisite cabal packages..."
+	@cabal update
+	@cabal install mtl parsec random stm text wl-pprint
+	@cabal install buildbox inchworm
+
