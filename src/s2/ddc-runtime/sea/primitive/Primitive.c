037fdee2c95be6e24ff8dfe5c475f2c7b4f8d3fd
Ben Lippmeier <benl@ouroborus.net>
2018-01-20 15:50:39 +1100

    base: change to stateful framework for JSON parser

--- src/s2/ddc-runtime/sea/primitive/Primitive.c
+++ src/s2/ddc-runtime/sea/primitive/Primitive.c
@@ -158,10 +158,10 @@ _DDC_MAKE_PRIM_SHOW_TYPE(Float64,float64_t, "%g",          24);
 
 
 //-----------------------------------------------------------------------------
-// Read functions.
+// Parse functions.
 //
-#define _DDC_MAKE_PRIM_READ_TYPE(typeName,typeSpec,format) \
- typeSpec primRead##typeName (Obj* pObj) \
+#define _DDC_MAKE_PRIM_PARSE_TYPE(typeName,typeSpec,format) \
+ typeSpec primParse##typeName (Obj* pObj) \
  { \
         uint8_t*  p8    = (uint8_t*)_ddcPayloadRaw(pObj); \
         uint32_t* pLen  = (uint32_t*)p8; \
@@ -172,15 +172,15 @@ _DDC_MAKE_PRIM_SHOW_TYPE(Float64,float64_t, "%g",          24);
         return x; \
  }
 
-_DDC_MAKE_PRIM_READ_TYPE(Addr,   void*,     "%p");
-_DDC_MAKE_PRIM_READ_TYPE(Int,    int,       "%d");
-_DDC_MAKE_PRIM_READ_TYPE(Nat,    nat_t,     "%zu");
-_DDC_MAKE_PRIM_READ_TYPE(Word8,  uint8_t,   "%" SCNx8);
-_DDC_MAKE_PRIM_READ_TYPE(Word16, uint16_t,  "%" SCNx16);
-_DDC_MAKE_PRIM_READ_TYPE(Word32, uint32_t,  "%" SCNx32);
-_DDC_MAKE_PRIM_READ_TYPE(Word64, uint64_t,  "%" SCNx64);
-_DDC_MAKE_PRIM_READ_TYPE(Float32,float32_t, "%f");
-_DDC_MAKE_PRIM_READ_TYPE(Float64,float64_t, "%lg");
+_DDC_MAKE_PRIM_PARSE_TYPE(Addr,   void*,     "%p");
+_DDC_MAKE_PRIM_PARSE_TYPE(Int,    int,       "%d");
+_DDC_MAKE_PRIM_PARSE_TYPE(Nat,    nat_t,     "%zu");
+_DDC_MAKE_PRIM_PARSE_TYPE(Word8,  uint8_t,   "%" SCNx8);
+_DDC_MAKE_PRIM_PARSE_TYPE(Word16, uint16_t,  "%" SCNx16);
+_DDC_MAKE_PRIM_PARSE_TYPE(Word32, uint32_t,  "%" SCNx32);
+_DDC_MAKE_PRIM_PARSE_TYPE(Word64, uint64_t,  "%" SCNx64);
+_DDC_MAKE_PRIM_PARSE_TYPE(Float32,float32_t, "%f");
+_DDC_MAKE_PRIM_PARSE_TYPE(Float64,float64_t, "%lg");
 
 
 // -- Stdin -------------------------------------------------------------------
