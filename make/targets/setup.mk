9be49f75f42ffdd8ca11b24993a5a11256acaea3
Ben Lippmeier <benl@ouroborus.net>
2018-04-15 21:59:56 +1000

    Install specific versions of packages in 'make setup'

--- make/targets/setup.mk
+++ make/targets/setup.mk
@@ -11,5 +11,10 @@ show-pkgs :
 setup :
 	@echo "* Installing prerequisite cabal packages..."
 	@$(DEPS_INSTALLER) update
-	@$(DEPS_INSTALLER) install text mtl stm parsec inchworm shimmer buildbox
+	@$(DEPS_INSTALLER) install \
+		text mtl stm \
+		parsec-3.1.13.0 \
+		inchworm-1.0.2.2 \
+		shimmer-0.1.3.2 \
+		buildbox-2.2.1.1
 
