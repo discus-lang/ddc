ba099c5ffd4dcff24fd5e7d3e04a1cf668bfbf01
Ben Lippmeier <benl@ouroborus.net>
2019-02-06 13:21:01 +1100

    build: update versions in travis file

--- make/targets/setup.mk
+++ make/targets/setup.mk
@@ -10,12 +10,12 @@ show-pkgs :
 .PHONY 	: setup
 setup :
 	@echo "* Installing prerequisite cabal packages..."
-	@$(DEPS_INSTALLER) update
-	@$(DEPS_INSTALLER) install \
+	@$(DEPS_INSTALLER) v1-update
+	@$(DEPS_INSTALLER) v1-install \
 		text mtl stm json \
 		parsec-3.1.13.0 \
-		inchworm-1.0.2.2 \
-		shimmer-0.1.3.2 \
-		buildbox-2.2.1.1 \
+		inchworm-1.1.1.2 \
+		shimmer-0.1.3.4 \
+		buildbox-2.2.1.2 \
 		cryptohash-sha256-0.11.101.0
 
